e042f3b11fed2c16d62091b3f9dd7d2a3c3f46b8
artp <dky7034@naver.com>
2025-02-20 17:19:56 +0900

    [refactor]: ArithmeticDemo2 수정

--- src/ArithmeticDemo2.java
+++ src/ArithmeticDemo2.java
@@ -5,11 +5,17 @@ public class ArithmeticDemo2 {
     Scanner in = new Scanner(System.in);
     System.out.print("정수를 입력해 주세요: ");
     int number = in.nextInt();
-    // 입력한 수가 짝수인 경우
-    if (number % 2 == 0) {
-      System.out.println("입력한 수는 짝수입니다.");
-    } else {
-      System.out.println("입력한 수는 홀수입니다.");
-    }
+
+    // 조건문 사용
+//    if (number % 2 == 0) {
+//      System.out.println("입력한 수는 짝수입니다.");
+//    } else {
+//      System.out.println("입력한 수는 홀수입니다.");
+//    }
+
+    // 삼항연산자 사용
+    String evenOdd = (number % 2 == 0) ? "짝수" : "홀수";
+    System.out.printf("입력한 수는 %d이며, %s입니다.", number, evenOdd);
+
   }
 }
--- src/TernaryOperatorDemo.java
+++ src/TernaryOperatorDemo.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class TernaryOperatorDemo {
   public static void main(String[] args) {
     // 삼항 연산자
     // 조건식 ? 연산식(true) : 연산식(false)
     boolean res = (1 != 1) ? true : false;
     System.out.println(res);
+
+    // 사용자에게 입력받기
+    Scanner in = new Scanner(System.in);
+    System.out.print("정수를 입력해 주세요");
   }
 }
