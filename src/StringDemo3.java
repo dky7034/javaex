3d9e0c2bd1b02e1629caaef3e5008eeb4f6eeada
artp <dky7034@naver.com>
2025-02-24 20:21:20 +0900

    commit

---
+++ src/StringDemo3.java
@@ -0,0 +1,25 @@
+public class StringDemo3 {
+  public static void main(String[] args) {
+    String s1 = "Hi, Java !";
+    String s2 = "Hi, Java !";
+    System.out.println("s1 == s2: " + (s1 == s2));
+    System.out.println("s1 = " + s1);
+    System.out.println("s2 = " + s2);
+
+    String s3 = new String("Hi, Java !");
+    String s4 = new String("Hi, Java !");
+    System.out.println("s3 == s4: " + (s3 == s4));
+    System.out.println("s3 = " + s3);
+    System.out.println("s4 = " + s4);
+
+    s1 = s3;
+    System.out.println("s1 == s3: " + (s1 == s3));
+    System.out.println("s1 == s4: " + (s1 == s4));
+    System.out.println(s1.equals(s4));
+
+    String a = "apple";
+    String b = "banana";
+
+    System.out.println(a.compareTo(b));
+  }
+}
---
+++ src/StringDemo4.java
@@ -0,0 +1,27 @@
+public class StringDemo4 {
+  public static void main(String[] args) {
+    String s1 = new String("hi,");
+    String s2 = new String(" java");
+
+    // 3.14를 문자열로 변환하기
+    String str = "" + 3.14;
+    String str1 = String.valueOf(3.14);
+    System.out.println("str = " + str);
+    System.out.println("str1 = " + str1);
+    String fruits = String.join(", ", "apple", "banana", "cherry", "durian");
+    System.out.println("fruits = " + fruits);
+    String str3 = String.format("%s %d", "jdk", 14);
+    System.out.println("str3 = " + str3);
+
+    // 텍스트 블록 """ ~ """
+    String strBlock = """
+        <html>
+          <body>
+            <p>hello, world</p>
+          </body>
+        <html>
+        """;
+    System.out.println("strBlock = \n" + strBlock);
+
+  }
+}
