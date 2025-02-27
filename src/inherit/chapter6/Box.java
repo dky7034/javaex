8619d264494d1b9f036bfe1754ee2e80832a0163
artp <dky7034@naver.com>
2025-02-27 16:25:49 +0900

    Auto commit: Thu Feb 27 16:25:49 KST 2025

--- src/inherit/chapter6/Box.java
+++ src/inherit/chapter6/Box.java
@@ -1,8 +1,11 @@
 package inherit.chapter6;
 
 public class Box {
-  public Box(String s) {}
-   public Box() {}
+  public Box(String s) {
+  }
+
+  public Box() {
+  }
 }
 
 class ColoredBox extends Box {
