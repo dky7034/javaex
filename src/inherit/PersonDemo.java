056cec40868cf46890f76cc5a059ef4e9f78fed3
artp <dky7034@naver.com>
2025-02-27 17:15:19 +0900

    Auto commit: Thu Feb 27 17:15:19 KST 2025

--- src/inherit/PersonDemo.java
+++ src/inherit/PersonDemo.java
@@ -3,6 +3,9 @@ package inherit;
 public class PersonDemo {
   public static void main(String[] args) {
     Student s = new Student();
-    Person p = s;
+    Person p = s; // 자손은 부모타입의 참조변수로 자동 형변환
+
+    System.out.println(p.name);
+    p.whoami();
   }
 }
