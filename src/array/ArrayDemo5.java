d5c3b68198da65310d4a12ffb7eb118cfeb4edd4
artp <dky7034@naver.com>
2025-02-27 10:08:32 +0900

    Auto commit: Thu Feb 27 10:08:32 KST 2025

--- src/array/ArrayDemo5.java
+++ src/array/ArrayDemo5.java
@@ -6,7 +6,7 @@ public class ArrayDemo5 {
   public static void main(String[] args) {
     Ball[] balls = new Ball[5];
     balls[0] = new Ball(5);
-    balls[0]
+    double volume = balls[0].getVolume();
     for (Ball ball : balls) {
       System.out.println("ball = " + ball);
     }
