f063fd3297e3c6ab9292bba65312f54f34291bc3
artp <dky7034@naver.com>
2025-02-27 16:08:56 +0900

    Auto commit: Thu Feb 27 16:08:56 KST 2025

--- src/inherit/chapter6/StaticImportDemo.java
+++ src/inherit/chapter6/StaticImportDemo.java
@@ -1,8 +1,11 @@
 package inherit.chapter6;
 
+import java.util.Arrays;
+
 public class StaticImportDemo {
   public static void main(String[] args) {
     int[] data = {3, 5, 1, 7};
+    Arrays.sort(data);
     
   }
 }
