b8af66b976ad63cac6490a135d924550f8afeee3
artp <dky7034@naver.com>
2025-02-27 11:29:22 +0900

    Auto commit: Thu Feb 27 11:29:22 KST 2025

--- src/enumType/PeopleDemo.java
+++ src/enumType/PeopleDemo.java
@@ -2,15 +2,7 @@ package enumType;
 
 public class PeopleDemo {
   public static void main(String[] args) {
-    People people = new People();
-    people.gender ==
-    System.out.println(People.FEMALE);
-    System.out.println(People.MALE);
-    System.out.println(People.ONE);
-    System.out.println(People.TWO);
+    System.out.println(People.Gender.FEMALE);
 
-    if (People.FEMALE == People.ONE) {
-      System.out.println("여성입니다.");
-    }
   }
 }
