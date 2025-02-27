7dacaec7871f3d52876f84dad2ee8423ae43ec11
artp <dky7034@naver.com>
2025-02-27 09:15:03 +0900

    Auto commit: Thu Feb 27 09:15:03 KST 2025

--- src/oop/OneToTenDemo.java
+++ src/oop/OneToTenDemo.java
@@ -1,4 +1,10 @@
 package oop;
 
 public class OneToTenDemo {
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 1; i <= 10 ; i++) {
+      sum += i;
+    }
+  }
 }
--- src/oop/UtilDemo.java
+++ src/oop/UtilDemo.java
@@ -2,6 +2,8 @@ package oop;
 
 public class UtilDemo {
   public static void main(String[] args) {
+    // 클래스 변수, 클래스 메서드는 해당 클래스 객체 생성 없이 클래스의의 이름으로 접근할 수 있습니다.
+    // 클래스명.method or 클래스명.변수
     System.out.println(Util.fourTimes(5));
   }
 }
