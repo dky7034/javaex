df556d6a077ea4571efd3fd163d5421af5bd51bd
artp <dky7034@naver.com>
2025-02-27 10:22:15 +0900

    Auto commit: Thu Feb 27 10:22:15 KST 2025

--- src/array/ArrayDemo5.java
+++ src/array/ArrayDemo5.java
@@ -7,10 +7,10 @@ public class ArrayDemo5 {
     Ball[] balls = new Ball[5];
     balls[0] = new Ball(5);
     balls[1] = new Ball(10);
-    
+
     for (Ball ball : balls) {
       if (ball != null) {
-        System.out.println(ball.radius);
+        System.out.println(ball);
       } else {
         System.out.println("null입니다.");
       }
