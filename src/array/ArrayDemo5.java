50b9737a5bb335034c7d06ccf60d12769d527dbe
artp <dky7034@naver.com>
2025-02-27 10:08:12 +0900

    Auto commit: Thu Feb 27 10:08:12 KST 2025

--- src/array/ArrayDemo5.java
+++ src/array/ArrayDemo5.java
@@ -6,6 +6,7 @@ public class ArrayDemo5 {
   public static void main(String[] args) {
     Ball[] balls = new Ball[5];
     balls[0] = new Ball(5);
+    balls[0]
     for (Ball ball : balls) {
       System.out.println("ball = " + ball);
     }
