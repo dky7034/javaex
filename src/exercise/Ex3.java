52ba90eea482b73c641b25b526f0534f0963233c
artp <dky7034@naver.com>
2025-02-27 14:06:33 +0900

    Auto commit: Thu Feb 27 14:06:33 KST 2025

--- src/exercise/Ex3.java
+++ src/exercise/Ex3.java
@@ -6,5 +6,6 @@ public class Ex3 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("URL을 입력하세요: ");
+    String url = in.nextLine();
   }
 }
