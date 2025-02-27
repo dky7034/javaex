cd980e8ca4862a3604a8cf54672119376df5d4e8
artp <dky7034@naver.com>
2025-02-27 14:07:31 +0900

    Auto commit: Thu Feb 27 14:07:31 KST 2025

--- src/exercise/Ex3.java
+++ src/exercise/Ex3.java
@@ -7,7 +7,8 @@ public class Ex3 {
     Scanner in = new Scanner(System.in);
     System.out.print("URL을 입력하세요: ");
     String url = in.nextLine();
-    if () {
+    if (url.endsWith("com")) {
+      System.out.println(url + "은 \'com\'으로 끝납니다.");
     }
   }
 }
