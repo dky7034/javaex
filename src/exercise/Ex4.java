60a943b0fa7baa5f0e7e7896ea8432f1e05d654a
artp <dky7034@naver.com>
2025-02-27 14:15:25 +0900

    Auto commit: Thu Feb 27 14:15:25 KST 2025

--- src/exercise/Ex4.java
+++ src/exercise/Ex4.java
@@ -6,5 +6,8 @@ public class Ex4 {
   public static void main(String[] args) {
     String[] name = {"가위", "바위", "보"};
     System.out.println(Arrays.toString(name));
+    for (int i = 0; i < 10; i++) {
+
+    }
   }
 }
