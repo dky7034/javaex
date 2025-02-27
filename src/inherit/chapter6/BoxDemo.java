9a0fbc0dec9936833950b707394fd8fc354c718f
artp <dky7034@naver.com>
2025-02-27 16:20:12 +0900

    Auto commit: Thu Feb 27 16:20:12 KST 2025

--- src/inherit/chapter6/BoxDemo.java
+++ src/inherit/chapter6/BoxDemo.java
@@ -2,7 +2,8 @@ package inherit.chapter6;
 
 public class BoxDemo {
   public static void main(String[] args) {
-
+    // ColoredBox 객체 생성
+    // 기본 생성자를 컴파일러가 만들어 줍니다.
     ColoredBox cb = new ColoredBox();
   }
 }
