6a12b4521336a3107944944aaefa2cd9bbe1994e
artp <dky7034@naver.com>
2025-02-27 15:22:16 +0900

    Auto commit: Thu Feb 27 15:22:16 KST 2025

--- src/inherit/chapter6/Ball.java
+++ src/inherit/chapter6/Ball.java
@@ -2,10 +2,12 @@ package inherit.chapter6;
 
 public class Ball extends Circle {
   private String color;
+
   public String findColor() {
     return color;
   }
+
   public void findVolume() {
-    return 3 / 4 * Math.PI * radius * radius * radius;
+    System.out.println(3 / 4 * Math.PI * radius * radius * radius);
   }
 }
