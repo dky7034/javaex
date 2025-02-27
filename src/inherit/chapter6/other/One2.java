9a3c31427f18a71d99c3dddef3bdc20ca7521c15
artp <dky7034@naver.com>
2025-02-27 16:41:23 +0900

    Auto commit: Thu Feb 27 16:41:23 KST 2025

--- src/inherit/chapter6/other/One2.java
+++ src/inherit/chapter6/other/One2.java
@@ -4,8 +4,8 @@ import inherit.chapter6.One;
 
 public class One2 extends One {
   void print() {
-    System.out.println(secret);
-    System.out.println(roommate);
+//    System.out.println(secret);
+//    System.out.println(roommate);
     System.out.println(child);
     System.out.println(anybody);
   }
