837b708082029681937343a6b3c1ae1368f7b1a5
artp <dky7034@naver.com>
2025-02-27 16:45:11 +0900

    Auto commit: Thu Feb 27 16:45:11 KST 2025

--- src/inherit/FinalDemo.java
+++ src/inherit/FinalDemo.java
@@ -2,3 +2,17 @@ package inherit;
 
 public class FinalDemo {
 }
+
+class Good {
+  String good;
+}
+
+class Beetter extends Good {
+  String better
+}
+
+final class Best extends Beetter {
+}
+
+class Another extends Best {
+}
