b2507bc251d2f3c1d8af8ae5bffd8c3e1ad575f8
artp <dky7034@naver.com>
2025-02-27 10:30:50 +0900

    Auto commit: Thu Feb 27 10:30:50 KST 2025

--- src/array/ArrayDemo6.java
+++ src/array/ArrayDemo6.java
@@ -3,6 +3,7 @@ package array;
 public class ArrayDemo6 {
   public static void main(String[] args) {
     // 원을 원소로 가지는 배열을 선언한 후 5개의 원을 초기화 하세요.
+    Circle[] circles = new Circle[];
     Circle circle = new Circle();
     // 원의 반지름은 1 ~ 5 까지입니다.
     // 모든 원의 반지름과 넓이를 출력
