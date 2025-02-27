489e4584be3cffe3c81bb0bbdef3626f084e61ed
artp <dky7034@naver.com>
2025-02-27 14:02:14 +0900

    Auto commit: Thu Feb 27 14:02:14 KST 2025

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -10,7 +10,7 @@ public class Test2 {
 
   // sum 메서드 생성
   // 첫 번째 매개변수를 제외한 나머지 가변개수인수로 들어온 값들만 합계를 구하는 문제
-  public static int sum(Object... objects) {
+  public static int sum(int i, int... values) {
 
     return 0;
   }
