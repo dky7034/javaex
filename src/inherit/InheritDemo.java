4c870f4f1c45d689ad2d7b1906d67db74e86d73f
artp <dky7034@naver.com>
2025-02-27 14:29:28 +0900

    Auto commit: Thu Feb 27 14:29:28 KST 2025

--- src/inherit/InheritDemo.java
+++ src/inherit/InheritDemo.java
@@ -2,3 +2,19 @@ package inherit;
 
 public class InheritDemo {
 }
+
+class Eagle {
+  String eye;
+  String mouth;
+  String wing;
+}
+
+class Tiger {
+  String eye;
+  String mouth;
+}
+
+class GoldFish {
+  String eye;
+  String mouth;
+}
