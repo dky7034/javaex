d42195bbbbb7a869220855e0b5902347989aa520
artp <dky7034@naver.com>
2025-02-27 12:13:54 +0900

    Auto commit: Thu Feb 27 12:13:54 KST 2025

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,7 +2,7 @@ package test;
 
 public class Test1 {
   public static void main(String[] args) {
-    countChar("character", c);
+    countChar("character", 'c');
   }
 
   // 문자열 s에 포함된 문자 c의 개수를 반환하는 메서드
