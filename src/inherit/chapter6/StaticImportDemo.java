0c67cd53c09346b260371d9a5a9470455520eb3b
artp <dky7034@naver.com>
2025-02-27 16:12:07 +0900

    Auto commit: Thu Feb 27 16:12:07 KST 2025

--- src/inherit/chapter6/StaticImportDemo.java
+++ src/inherit/chapter6/StaticImportDemo.java
@@ -5,6 +5,10 @@ public class StaticImportDemo {
   public static void main(String[] args) {
     int[] data = {3, 5, 1, 7};
     sort(data);
+    sort(data);
+    sort(data);
+    sort(data);
+    sort(data);
 //    System.out.println(Arrays.toString(data));
   }
 }
