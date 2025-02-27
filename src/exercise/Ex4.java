c338857a2774c09c2435130e79f6dbab680956f9
artp <dky7034@naver.com>
2025-02-27 14:19:59 +0900

    Auto commit: Thu Feb 27 14:19:59 KST 2025

--- src/exercise/Ex4.java
+++ src/exercise/Ex4.java
@@ -7,7 +7,7 @@ public class Ex4 {
     String[] name = {"가위", "바위", "보"};
     System.out.println(Arrays.toString(name));
     for (int i = 0; i < 10; i++) {
-      double r = (int)Math.floor(Math.random() * 3);
+      double r = (int) Math.floor(Math.random() * 3);
       System.out.println("r = " + r);
 
     }
