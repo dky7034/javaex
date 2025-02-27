e5212a1d748837b7084ad7bae02c7946ce724fc3
artp <dky7034@naver.com>
2025-02-27 12:29:08 +0900

    Auto commit: Thu Feb 27 12:29:08 KST 2025

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -2,11 +2,11 @@ package test;
 
 public class Test2 {
   public static void main(String[] args) {
-
+    System.out.println(sum(1,2,3,4));
   }
 
   // sum 메서드 생성
   static void sum(int... ) {
-    
+
   }
 }
