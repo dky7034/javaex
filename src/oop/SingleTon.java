12c45cf323fc90fecfe875b5dddc881449cf8fe5
artp <dky7034@naver.com>
2025-02-27 09:33:11 +0900

    Auto commit: Thu Feb 27 09:33:11 KST 2025

--- src/oop/SingleTon.java
+++ src/oop/SingleTon.java
@@ -2,7 +2,7 @@ package oop;
 
 public class SingleTon {
   // 클래스 내부에서 싱글톤 객체 생성
-  private 
+  private SingleTon singleTon
 
   // 싱글톤 생성자를 private 으로 선언하여 객체를 외부에서 생성하지 못하도록 숨김
   private SingleTon() {}
