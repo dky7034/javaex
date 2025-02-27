24463f816e0bce44a630323f31a18c5f29caafa3
artp <dky7034@naver.com>
2025-02-27 15:37:46 +0900

    Auto commit: Thu Feb 27 15:37:46 KST 2025

--- src/inherit/chapter6/InheritDemo.java
+++ src/inherit/chapter6/InheritDemo.java
@@ -2,13 +2,16 @@ package inherit.chapter6;
 
 public class InheritDemo {
   public static void main(String[] args) {
+    // 원
     Circle c = new Circle();
     c.radius = 10.0;
     c.findArea();
 
+    // 구
     Ball b = new Ball(5.0, "yellow");
     b.findColor();
     b.findVolume();
+    b.findArea();
 
   }
 }
