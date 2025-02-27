4bdf0fe0e176b880e2041d2cf4e48fbdf0aa3ddd
artp <dky7034@naver.com>
2025-02-27 11:02:00 +0900

    Auto commit: Thu Feb 27 11:02:00 KST 2025

--- src/array/CircleDemo.java
+++ src/array/CircleDemo.java
@@ -2,12 +2,12 @@ package array;
 
 public class CircleDemo {
   public static void main(String[] args) {
-    // 인수 c1이 매개변수 c에 복사된다.
-    // c1과 c는 동일한 Circle 객체를 가리킨다.
-    // zero() 메서드에서 c.radius 에 0.0을 대입한다.
+
   }
 
   static void zero() {
-    
+    // 인수 c1이 매개변수 c에 복사된다.
+    // c1과 c는 동일한 Circle 객체를 가리킨다.
+    // zero() 메서드에서 c.radius 에 0.0을 대입한다.
   }
 }
