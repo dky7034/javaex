c73365456b3e44a589b2e1a8f14ec3933c06b915
artp <dky7034@naver.com>
2025-02-27 15:35:09 +0900

    Auto commit: Thu Feb 27 15:35:09 KST 2025

--- src/inherit/chapter6/Circle.java
+++ src/inherit/chapter6/Circle.java
@@ -8,6 +8,6 @@ public class Circle {
   }
 
   public void findArea() {
-    System.out.println("넓이는 " + (Math.PI * radius * radius) + "입니다.");
+    System.out.println("원넓이는 " + (Math.PI * radius * radius) + "입니다.");
   }
 }
