6e22b420b00f992f28374f52f7ec68f5744a8877
artp <dky7034@naver.com>
2025-02-27 10:30:18 +0900

    Auto commit: Thu Feb 27 10:30:18 KST 2025

--- src/array/ArrayDemo6.java
+++ src/array/ArrayDemo6.java
@@ -1,4 +1,9 @@
 package array;
 
 public class ArrayDemo6 {
+  public static void main(String[] args) {
+    // 원을 원소로 가지는 배열을 선언한 후 5개의 원을 초기화
+    // 원의 반지름은 1 ~ 5 까지입니다.
+    // 모든 원의 반지름과 넓이를 출력
+  }
 }
