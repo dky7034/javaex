7707df19ccd4dd43298e2e3b24181d1fc89f1550
artp <dky7034@naver.com>
2025-02-27 11:26:08 +0900

    Auto commit: Thu Feb 27 11:26:08 KST 2025

--- src/enumType/PeopleDemo.java
+++ src/enumType/PeopleDemo.java
@@ -3,7 +3,7 @@ package enumType;
 public class PeopleDemo {
   public static void main(String[] args) {
     People people = new People();
-    people.gender == People;
+    people.gender == 
     System.out.println(People.FEMALE);
     System.out.println(People.MALE);
     System.out.println(People.ONE);
