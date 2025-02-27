fe1e12ab8e481eff093d7a9619b45c1ec3c23b9f
artp <dky7034@naver.com>
2025-02-27 11:24:29 +0900

    Auto commit: Thu Feb 27 11:24:29 KST 2025

--- src/enumType/People.java
+++ src/enumType/People.java
@@ -2,10 +2,7 @@ package enumType;
 
 public class People {
   Gender gender;
-  static final int FEMALE = 1;
-  static final int ONE = 1;
-  static final int TWO = 2;
-
+  Num number;
   final int kind = 0;
   final int num = 1;
 }
