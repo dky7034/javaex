ed3befa26cb88b1381878f4860d51895f95010aa
artp <dky7034@naver.com>
2025-02-27 17:23:35 +0900

    Auto commit: Thu Feb 27 17:23:35 KST 2025

--- src/inherit/PersonDemo.java
+++ src/inherit/PersonDemo.java
@@ -19,7 +19,7 @@ public class PersonDemo {
     // Person을 담는 배열을 선언하고, 여기에 student, worker를 담을 수 있다.
     Person[] persons = new Person[3];
 
-    static void method1() {
+    static void method1(Student s) {
 
     }
   }
