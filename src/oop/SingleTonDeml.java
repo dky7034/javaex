68d2660345137f7ac2df9b353d59d0075fd66a9f
artp <dky7034@naver.com>
2025-02-27 09:44:48 +0900

    Auto commit: Thu Feb 27 09:44:48 KST 2025

--- src/oop/SingleTonDeml.java
+++ src/oop/SingleTonDeml.java
@@ -3,7 +3,7 @@ package oop;
 public class SingleTonDeml {
   public static void main(String[] args) {
     SingleTon s1 = SingleTon.getInstance();
-    SingleTon s2 = SingleTon.getInstance()
+    SingleTon s2 = SingleTon.getInstance();
     // 싱글톤 객체 메서드 호출
     s1.method();
     s2.method();
