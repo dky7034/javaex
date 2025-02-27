c1e6c836616756a7ce32e034074268e89b8dafaa
artp <dky7034@naver.com>
2025-02-27 12:30:24 +0900

    Auto commit: Thu Feb 27 12:30:24 KST 2025

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -2,15 +2,15 @@ package test;
 
 public class Test2 {
   public static void main(String[] args) {
-    System.out.println(sum(1, 2, 3, 4));
+    System.out.println(sum(1, 2, 3, 4));  // 9
     int arr[] = {2, 3};
-    System.out.println(sum(1, arr));
-    System.out.println(sum(1, 2, 3, 4, 5));
+    System.out.println(sum(1, arr));  // 5
+    System.out.println(sum(1, 2, 3, 4, 5)); // 15
 
   }
 
   // sum 메서드 생성
-  static void sum(Object... objects) {
+  static  sum(Object... objects) {
 
   }
 }
