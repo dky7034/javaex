bda608350093fd6b53a566003374112c76a3bced
artp <dky7034@naver.com>
2025-02-27 15:22:37 +0900

    Auto commit: Thu Feb 27 15:22:37 KST 2025

--- src/inherit/chapter6/Circle.java
+++ src/inherit/chapter6/Circle.java
@@ -2,6 +2,10 @@ package inherit.chapter6;
 
 public class Circle {
   double radius;
+  public double findRadius() {
+    return radius;
+  }
+
   void findArea() {
     System.out.println("넓이는 " + (Math.PI * radius * radius) + "입니다.");
   }
