fc4a75406d65717d63cb1411f92e592df8e27c87
artp <dky7034@naver.com>
2025-02-27 12:22:48 +0900

    Auto commit: Thu Feb 27 12:22:48 KST 2025

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class Test1 {
   public static void main(String[] args) {
-    // 문자열 ArrayList 생성
-    ArrayList<String> temp = new ArrayList<>();
     int result = countChar("hello", 'l');
     System.out.println("result = " + result);
   }
