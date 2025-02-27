ad182950464169fc1becc6a34c48f7412faf528c
artp <dky7034@naver.com>
2025-02-27 11:37:24 +0900

    Auto commit: Thu Feb 27 11:37:24 KST 2025

--- src/enumType/PeopleDemo.java
+++ src/enumType/PeopleDemo.java
@@ -4,6 +4,8 @@ public class PeopleDemo {
   public static void main(String[] args) {
     Person person = new Person();
     person.name = "홍길동";
+    person.age = 20;
+    person.gender = Gender.FEMALE;
 
   }
 }
