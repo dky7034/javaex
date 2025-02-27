f7f44e58cc675a099218da9c1223e2d6579b997c
artp <dky7034@naver.com>
2025-02-27 14:16:23 +0900

    Auto commit: Thu Feb 27 14:16:23 KST 2025

--- src/exercise/Ex4.java
+++ src/exercise/Ex4.java
@@ -8,6 +8,7 @@ public class Ex4 {
     System.out.println(Arrays.toString(name));
     for (int i = 0; i < 10; i++) {
       double r = Math.random();
+      System.out.println("r = " + r);
 
     }
   }
