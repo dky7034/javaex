c89028093f720e929333cc9edcbe04b8bd230966
artp <dky7034@naver.com>
2025-02-27 10:06:37 +0900

    Auto commit: Thu Feb 27 10:06:37 KST 2025

--- src/array/ArrayDemo5.java
+++ src/array/ArrayDemo5.java
@@ -5,6 +5,7 @@ import oop.Ball;
 public class ArrayDemo5 {
   public static void main(String[] args) {
     Ball[] balls = new Ball[5];
+    balls[0] = new Ball(5);
     for (Ball ball : balls) {
       System.out.println("ball = " + ball);
     }
