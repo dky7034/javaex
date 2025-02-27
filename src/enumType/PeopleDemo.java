7e30fe0d2cc9c495491bdc8c070f91f2292f17d5
artp <dky7034@naver.com>
2025-02-27 11:30:09 +0900

    Auto commit: Thu Feb 27 11:30:09 KST 2025

--- src/enumType/PeopleDemo.java
+++ src/enumType/PeopleDemo.java
@@ -3,7 +3,7 @@ package enumType;
 public class PeopleDemo {
   public static void main(String[] args) {
     System.out.println(People.Gender.FEMALE);
-    System.out.println(Gender.MALE);
+    System.out.println(Gender.);
 
   }
 }
