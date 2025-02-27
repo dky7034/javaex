48168d4cdce2a6d65e007ceebe6ecd34ba1b68da
artp <dky7034@naver.com>
2025-02-27 10:27:48 +0900

    Auto commit: Thu Feb 27 10:27:48 KST 2025

--- src/array/Circle.java
+++ src/array/Circle.java
@@ -3,15 +3,17 @@ package array;
 public class Circle {
   // 생성자 (원의 반지름 초기화)
   public Circle(double radius) {
-    this.radius = radius
+    this.radius = radius;
   }
 
   // 필드 - 반지름
   private double radius;
-  
+
   // 메서드
   // 반지름을 구하는 메서드
-
+  public void showRadius() {
+    System.out.println();
+  }
   // 넓이를 구하는 메서드
 
 }
