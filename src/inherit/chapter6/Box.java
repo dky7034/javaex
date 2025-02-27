3b14631cafb60884bffc88af0b0f62a05b6a798b
artp <dky7034@naver.com>
2025-02-27 16:24:36 +0900

    Auto commit: Thu Feb 27 16:24:36 KST 2025

--- src/inherit/chapter6/Box.java
+++ src/inherit/chapter6/Box.java
@@ -9,9 +9,10 @@ public class Box {
 
 class ColoredBox extends Box {
   // 객체 생성 시 부모 생성자도 만들어줌
-   public ColoredBox(String color) {
-     this.color = color;
-   }
+  public ColoredBox(String color) {
+    this.color = color;
+  }
+
   // 필드
   String color;
 
