ced995f3650c35267a92c59480f3835c617ce9a9
artp <dky7034@naver.com>
2025-02-27 10:32:03 +0900

    Auto commit: Thu Feb 27 10:32:03 KST 2025

--- src/array/ArrayDemo6.java
+++ src/array/ArrayDemo6.java
@@ -5,7 +5,7 @@ public class ArrayDemo6 {
     // 원을 원소로 가지는 배열을 선언한 후 5개의 원을 초기화 하세요.
     Circle[] circles = new Circle[5];
     for (int i = 0; i < 5; i++) {
-      
+      circles[i] = new Circle();
     }
 
 
