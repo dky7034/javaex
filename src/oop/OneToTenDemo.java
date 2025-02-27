d94f5802423167dd47b00eb11dc9ca3ad2167a6d
artp <dky7034@naver.com>
2025-02-27 09:20:59 +0900

    Auto commit: Thu Feb 27 09:20:59 KST 2025

--- src/oop/OneToTenDemo.java
+++ src/oop/OneToTenDemo.java
@@ -17,7 +17,7 @@ public class OneToTenDemo {
   }
   // 메서드 (정적 메서드)
   public static void main(String args[]) {
-    System.out.println(sumOneToTen);
+    System.out.println("sumOneToTen = " + sumOneToTen);
   }
 
 }
