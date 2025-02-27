3513649eb401921a7a629e3db5fab1394a327516
artp <dky7034@naver.com>
2025-02-27 09:19:55 +0900

    Auto commit: Thu Feb 27 09:19:55 KST 2025

--- src/oop/OneToTenDemo.java
+++ src/oop/OneToTenDemo.java
@@ -7,6 +7,10 @@ public class OneToTenDemo {
   // 멤버
   // 변수 (정적 변수)
   static int sumOneToTen;
+  // static 블록
+  static {
+    int sum = 0;
+  }
   // 메서드 (정적 메서드)
   public static void main(String args[]) {
     System.out.println(sumOneToTen);
