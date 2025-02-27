db671bb7e3a6a789ac1e40c127ce012c2a132d42
artp <dky7034@naver.com>
2025-02-27 12:37:30 +0900

    Auto commit: Thu Feb 27 12:37:30 KST 2025

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -6,12 +6,11 @@ public class Test2 {
     int arr[] = {2, 3};
     System.out.println(sum(1, arr));  // 5
     System.out.println(sum(1, 2, 3, 4, 5)); // 14
-
   }
 
   // sum 메서드 생성
   static int sum(Object... objects) {
-    
+
     return 0;
   }
 }
