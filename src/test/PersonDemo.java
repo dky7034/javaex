0b15d99090d91a538ce900ae67661733042ba790
artp <dky7034@naver.com>
2025-02-28 11:22:45 +0900

    Auto commit: Fri Feb 28 11:22:45 KST 2025

--- src/test/PersonDemo.java
+++ src/test/PersonDemo.java
@@ -3,8 +3,8 @@ package test;
 public class PersonDemo {
   public static void main(String[] args) {
     Person[] people = new Person[3];
-    people[0] =
-        people[0] =
-            people[0] =
+    people[0] = new Person();
+    people[0] = new Student();
+    people[0] = new ForeignStudent();
   }
 }
