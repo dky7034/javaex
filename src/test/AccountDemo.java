c1e1603e0f3bcea9ac9173252fd37c11ac77a7b0
artp <dky7034@naver.com>
2025-02-28 15:27:23 +0900

    Auto commit: Fri Feb 28 15:27:23 KST 2025

--- src/test/AccountDemo.java
+++ src/test/AccountDemo.java
@@ -3,6 +3,6 @@ package test;
 public class AccountDemo {
   public static void main(String[] args) {
     Account[] accounts = new Account[100];
-    account[0] = new Account("111-111", "홍길동");
+    accounts[0] = new Account("111-111", "홍길동");
   }
 }
