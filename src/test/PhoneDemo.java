2194b2e621334310ced56142925c7f6c2523fa00
artp <dky7034@naver.com>
2025-02-28 12:31:00 +0900

    Auto commit: Fri Feb 28 12:31:00 KST 2025

--- src/test/PhoneDemo.java
+++ src/test/PhoneDemo.java
@@ -5,8 +5,8 @@ public class PhoneDemo {
     Phone[] phones = {new Phone("황진이"), new TelePhone("길동이", "내일"), new SmartPhone("민국이", "갤러그")};
 
     System.out.println(phones[0] instanceof Phone);
-    System.out.println(phones[0] instanceof Phone);
-    System.out.println(phones[0] instanceof Phone);
+    System.out.println(phones[1] instanceof TelePhone);
+    System.out.println(phones[2] instanceof SmartPhone);
 
     for (Phone p : phones) {
       if (p instanceof Phone) {
