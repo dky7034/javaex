a44bc0a34958b6b88f6dfb6819a4efa9107ba894
artp <dky7034@naver.com>
2025-02-28 10:38:21 +0900

    Auto commit: Fri Feb 28 10:38:21 KST 2025

--- src/test/CircleDemo.java
+++ src/test/CircleDemo.java
@@ -4,6 +4,7 @@ public class CircleDemo {
   public static void main(String[] args) {
     Circle[] circles = new Circle[2];
     circles[0] = new Circle(5);
-    circles[1] = new ColorCircle(10);
+    circles[1] = new ColorCircle(10, "빨간색");
+    
   }
 }
