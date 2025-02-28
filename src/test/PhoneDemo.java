e0a8913a08ad894c6c9173619b031d8213de540d
artp <dky7034@naver.com>
2025-02-28 12:20:06 +0900

    Auto commit: Fri Feb 28 12:20:05 KST 2025

--- src/test/PhoneDemo.java
+++ src/test/PhoneDemo.java
@@ -1,4 +1,7 @@
 package test;
 
 public class PhoneDemo {
+  public static void main(String[] args) {
+    Phone[] phones = { new Phone("황진이"), new TelePhone("길동이", "내일"), new SmartPhone("민국이, "갤러그") }
+  }
 }
