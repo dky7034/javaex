bffdbcbe1f85af3c45939f814059570c91a7adc3
artp <dky7034@naver.com>
2025-02-28 10:56:01 +0900

    Auto commit: Fri Feb 28 10:56:01 KST 2025

--- src/test/PointDemo.java
+++ src/test/PointDemo.java
@@ -2,7 +2,7 @@ package test;
 
 public class PointDemo {
   public static void main(String[] args) {
-    Point point = new Point();
+    Point point = new Point(3, 5);
     PointDemo pointDemo = new PointDemo();
 
   }
