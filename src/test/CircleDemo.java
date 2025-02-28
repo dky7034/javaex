e26bcf11d48ef37be0e10fa5e0920cab90c7e619
artp <dky7034@naver.com>
2025-02-28 10:37:29 +0900

    Auto commit: Fri Feb 28 10:37:29 KST 2025

--- src/test/CircleDemo.java
+++ src/test/CircleDemo.java
@@ -5,6 +5,6 @@ import java.awt.*;
 public class CircleDemo {
   public static void main(String[] args) {
     Color[] colors = new Color[2];
-    colors[0] = new Color();
+    colors[0] = new 
   }
 }
