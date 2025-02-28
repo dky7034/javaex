f8f0477a21e8caddbdb3d8b38950fc0eff0a4c5f
artp <dky7034@naver.com>
2025-02-28 11:29:09 +0900

    Auto commit: Fri Feb 28 11:29:09 KST 2025

--- src/test/PersonDemo.java
+++ src/test/PersonDemo.java
@@ -4,8 +4,8 @@ public class PersonDemo {
   public static void main(String[] args) {
     Person[] people = new Person[3];
     people[0] = new Person("길동이", 22);
-    people[0] = new Student("황진이", 23, 100);
-    people[0] = new ForeignStudent("Amy", 30, 200, "U.S.A");
+    people[1] = new Student("황진이", 23, 100);
+    people[2] = new ForeignStudent("Amy", 30, 200, "U.S.A");
     for (Person p : people) {
       p.show();
     }
