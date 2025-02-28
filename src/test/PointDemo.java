a6d19387362b72233f7efdef1d2e4b482388d31e
artp <dky7034@naver.com>
2025-02-28 10:58:02 +0900

    Auto commit: Fri Feb 28 10:58:02 KST 2025

--- src/test/PointDemo.java
+++ src/test/PointDemo.java
@@ -4,6 +4,8 @@ public class PointDemo {
   public static void main(String[] args) {
     Point point = new Point(3, 5);
     MovablePoint movablePoint = new MovablePoint(point.getX(), point.getY(), 30, 60);
-    
+
+    System.out.println(point);
+    System.out.println(movablePoint);
   }
 }
