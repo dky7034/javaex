aa4c9cbf66bac172681aaada2b090ff4e1d304b8
artp <dky7034@naver.com>
2025-02-28 10:36:58 +0900

    Auto commit: Fri Feb 28 10:36:58 KST 2025

--- src/test/CircleDemo.java
+++ src/test/CircleDemo.java
@@ -2,7 +2,7 @@ package test;
 
 public class CircleDemo {
   public static void main(String[] args) {
-    Circle circle = new ColorCircle();
+    Circle circle = new ColorCircle("5.0", );
 
   }
 }
