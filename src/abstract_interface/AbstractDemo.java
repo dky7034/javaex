dfac54ba6fe8dbecd94887698b82aadcc38b8bc8
artp <dky7034@naver.com>
2025-02-28 17:51:00 +0900

    Auto commit: Fri Feb 28 17:51:00 KST 2025

--- src/abstract_interface/AbstractDemo.java
+++ src/abstract_interface/AbstractDemo.java
@@ -10,7 +10,7 @@ public class AbstractDemo {
 
     TestAbstract testAbstract = new extendsTestAbstract();
     int numParent = testAbstract.numParent;
-    testAbstract.stringParent
+    String stringParent = testAbstract.stringParent;
   }
 }
 
