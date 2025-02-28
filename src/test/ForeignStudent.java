6662e31e2b629f3def9c54d59dcfd42b530a1c0f
artp <dky7034@naver.com>
2025-02-28 11:17:13 +0900

    Auto commit: Fri Feb 28 11:17:13 KST 2025

--- src/test/ForeignStudent.java
+++ src/test/ForeignStudent.java
@@ -1,6 +1,10 @@
 package test;
 
 public class ForeignStudent {
+  // 필드
   private String nationality;
 
+  // 생성자
+
+  // 메서드
 }
--- src/test/Student.java
+++ src/test/Student.java
@@ -3,4 +3,8 @@ package test;
 public class Student {
   // 필드
   private int stuNum;
+
+  // 생성자
+
+  // 메서드
 }
