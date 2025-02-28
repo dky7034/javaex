680ec896636e789852c60c38e0fb8c0595b56766
artp <dky7034@naver.com>
2025-02-28 14:26:32 +0900

    Auto commit: Fri Feb 28 14:26:32 KST 2025

--- src/test/Account.java
+++ src/test/Account.java
@@ -6,5 +6,14 @@ public class Account {
   // 생성자
 
   // getter and setter
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public void setBalance(int balance) {
+    this.balance = balance;
+  }
+
   // 메서드
 }
