083a155f2d985e7a49bee60655543d271c523fea
artp <dky7034@naver.com>
2025-02-28 10:50:04 +0900

    Auto commit: Fri Feb 28 10:50:04 KST 2025

--- src/test/Point.java
+++ src/test/Point.java
@@ -3,22 +3,29 @@ package test;
 public class Point {
   // 필드
   private int x, y;
+
   // 생성자
-  public Point() {}
+  public Point() {
+  }
+
   // 메서드
   // getter and setter
   public int getX() {
     return x;
   }
+
   public int getY() {
     return y;
   }
+
   public void setX(int x) {
     this.x = x;
   }
+
   public void setY(int y) {
     this.y = y;
   }
+
   // 다른 메서드
   void method() {
 
@@ -43,5 +50,7 @@ class MovablePoint extends Point {
 
   // 메서드
   // getter and setter
+
+  // 다른 메서드
 
 }
