d479de505b5e17184cedc57dcdcb5f30e4005860
artp <dky7034@naver.com>
2025-02-28 14:39:59 +0900

    Auto commit: Fri Feb 28 14:39:59 KST 2025

--- src/test/AccountDemo.java
+++ src/test/AccountDemo.java
@@ -1,4 +1,13 @@
 package test;
 
 public class AccountDemo {
+  public static void main(String[] args) {
+    Account account = new Account();
+    account.setBalance(1000);
+    System.out.println("잔액: " + account.getBalance());
+    account.setBalance();
+    account.setBalance();
+    account.setBalance();
+
+  }
 }
