d2ff9101b5a41021bc96bc3a9137cfe1dc34c357
artp <dky7034@naver.com>
2025-02-28 12:25:21 +0900

    Auto commit: Fri Feb 28 12:25:21 KST 2025

--- src/test/PhoneDemo.java
+++ src/test/PhoneDemo.java
@@ -5,7 +5,7 @@ public class PhoneDemo {
     Phone[] phones = {new Phone("황진이"), new TelePhone("길동이", "내일"), new SmartPhone("민국이", "갤러그")};
 
     for (int i = 0; i < phones.length; i++) {
-      if (phones[i] instanceof ) {
+      if (phones[i] instanceof Phone) {
       }
     }
     
