4bbf529f4d9d6f483785c2432a78d4d6027f6f5c
artp <dky7034@naver.com>
2025-02-28 11:17:30 +0900

    Auto commit: Fri Feb 28 11:17:30 KST 2025

--- src/test/Person.java
+++ src/test/Person.java
@@ -6,6 +6,22 @@ public class Person {
   private int age;
 
   // 생성자
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
 
   // 메서드
 
