da155e67faa84b07b7c51f5773350015f15b47d3
artp <dky7034@naver.com>
2025-02-28 12:15:15 +0900

    Auto commit: Fri Feb 28 12:15:15 KST 2025

--- src/test/Phone.java
+++ src/test/Phone.java
@@ -1,13 +1,34 @@
 package test;
 
 public class Phone {
+  // 필드
+  
+  // 생성자
+
+  // getter and setter
+
+  // 메서드
   protected String owner;
 }
 
 class TelePhone extends Phone {
+  // 필드
+
+  // 생성자
+
+  // getter and setter
+
+  // 메서드
 
 }
 
 class SmartPhone extends Phone {
+  // 필드
+
+  // 생성자
+
+  // getter and setter
+
+  // 메서드
 
 }
