ee1bcca9153fbe2f93b3486a29a7082d697ed582
artp <dky7034@naver.com>
2025-02-28 12:28:17 +0900

    Auto commit: Fri Feb 28 12:28:17 KST 2025

--- src/test/PhoneDemo.java
+++ src/test/PhoneDemo.java
@@ -3,18 +3,6 @@ package test;
 public class PhoneDemo {
   public static void main(String[] args) {
     Phone[] phones = {new Phone("황진이"), new TelePhone("길동이", "내일"), new SmartPhone("민국이", "갤러그")};
-
-    for (int i = 0; i < phones.length; i++) {
-      if (phones[i] instanceof Phone) {
-        phones[i].talk();
-      } else if (phones[i] instanceof TelePhone) {
-        TelePhone telePhone = (TelePhone) phones[i];
-        telePhone.autoAnswering();
-      } else if (phones[i] instanceof SmartPhone) {
-        SmartPhone smartPhone = (SmartPhone) phones[i];
-        smartPhone.playGame();
-      }
-    }
     
     for (Phone p : phones) {
       if (p instanceof Phone) {
