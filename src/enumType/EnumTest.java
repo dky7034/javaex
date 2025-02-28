d5e404a95f591f018f7ab18d88bdf5dc21a349c2
artp <dky7034@naver.com>
2025-02-28 19:03:31 +0900

    Auto commit: Fri Feb 28 19:03:31 KST 2025

--- src/enumType/EnumTest.java
+++ src/enumType/EnumTest.java
@@ -3,6 +3,8 @@ package enumType;
 public class EnumTest {
   public static void main(String[] args) {
     OrderStatus status = OrderStatus.SHIPPED;
+
+    OrderStatus wrongStatus = "SHIPPING";
   }
 
 }
