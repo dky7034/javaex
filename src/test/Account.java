85b5f1dd84def7d9b7f66308a0c14239bbcbf246
artp <dky7034@naver.com>
2025-02-28 14:50:48 +0900

    Auto commit: Fri Feb 28 14:50:48 KST 2025

--- src/test/Account.java
+++ src/test/Account.java
@@ -5,6 +5,7 @@ public class Account {
   private int balance;
   // 필드 추가 - 계좌번호, 계좌주
   private String accountNo;
+  private String accountOwner;
   // 상수 선언
   final static int MIN_BALANCE = 0;
   final static int MAX_BALANCE = 1_000_000;
