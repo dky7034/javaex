83068dad2ef7737fcb963cc962a18d7373a4c58e
artp <dky7034@naver.com>
2025-02-28 15:15:25 +0900

    Auto commit: Fri Feb 28 15:15:25 KST 2025

--- src/test/AccountDemo.java
+++ src/test/AccountDemo.java
@@ -3,14 +3,6 @@ package test;
 public class AccountDemo {
   public static void main(String[] args) {
     Account account = new Account(10_000, "10010011111", "홍길동");
-    account.setBalance(1000);
-    System.out.println("잔액: " + account.getBalance());
-    account.setBalance(-100);
-    System.out.println("잔액: " + account.getBalance());
-    account.setBalance(12_000_000);
-    System.out.println("잔액: " + account.getBalance());
-    account.setBalance(0);
-    System.out.println("잔액: " + account.getBalance());
-
+    
   }
 }
