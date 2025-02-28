1c21eec4d53c03da099a087995cb09db8522fdb8
artp <dky7034@naver.com>
2025-02-28 10:22:17 +0900

    Auto commit: Fri Feb 28 10:22:17 KST 2025

--- src/inherit/PolymorDemo.java
+++ src/inherit/PolymorDemo.java
@@ -4,5 +4,7 @@ public class PolymorDemo {
   public static void main(String[] args) {
     Vehicle[] vehicles = new Vehicle[3];
     vehicles[0] = new Vehicle();
+    vehicles[1] = new Car();
+    vehicles[2] = new SportsCar();
   }
 }
