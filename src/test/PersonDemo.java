0d70d4c597626172a639566c06e11de345f20fb8
artp <dky7034@naver.com>
2025-02-28 11:24:48 +0900

    Auto commit: Fri Feb 28 11:24:48 KST 2025

--- src/test/PersonDemo.java
+++ src/test/PersonDemo.java
@@ -6,5 +6,8 @@ public class PersonDemo {
     people[0] = new Person("길동이", 22);
     people[0] = new Student("황진이", 23, 100);
     people[0] = new ForeignStudent("Amy", 30, 200, "U.S.A");
+    for (Person person : people) {
+      
+    }
   }
 }
