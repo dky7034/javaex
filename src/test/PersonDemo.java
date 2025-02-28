d4b1a8bafce0666f6da5464fa4e340e9eb52a311
artp <dky7034@naver.com>
2025-02-28 11:23:20 +0900

    Auto commit: Fri Feb 28 11:23:20 KST 2025

--- src/test/PersonDemo.java
+++ src/test/PersonDemo.java
@@ -4,7 +4,7 @@ public class PersonDemo {
   public static void main(String[] args) {
     Person[] people = new Person[3];
     people[0] = new Person("길동이", 22);
-    people[0] = new Student();
+    people[0] = new Student("황진이", 23, 100);
     people[0] = new ForeignStudent();
   }
 }
