17eb004226073c428d556d956a571a1c3ffddfef
artp <dky7034@naver.com>
2025-02-28 14:12:54 +0900

    Auto commit: Fri Feb 28 14:12:54 KST 2025

--- src/test/VehicleDemo.java
+++ src/test/VehicleDemo.java
@@ -3,5 +3,10 @@ package test;
 public class VehicleDemo {
   public static void main(String[] args) {
     Car c = new Car("파랑", 200, 1000, 5);
+    c.show();
+
+    System.out.println();
+    Vehicle v = c;
+    v.show();
   }
 }
