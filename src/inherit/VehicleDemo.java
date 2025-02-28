2467345bc07d3acf17e3d122ada0800198c210c6
artp <dky7034@naver.com>
2025-02-28 10:13:51 +0900

    Auto commit: Fri Feb 28 10:13:51 KST 2025

--- src/inherit/VehicleDemo.java
+++ src/inherit/VehicleDemo.java
@@ -4,6 +4,7 @@ public class VehicleDemo {
   public static void main(String[] args) {
     Vehicle v1 = new Vehicle();
     System.out.println(v1.name);
+    v1.whoami();
 
     Vehicle v = new Car();
     System.out.println(v.name); // 부모 클래스의 멤버 호출
