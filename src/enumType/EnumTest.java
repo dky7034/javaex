254bac2a31712b1543f871a51b71ba0814d6c9e2
artp <dky7034@naver.com>
2025-02-28 19:09:32 +0900

    Auto commit: Fri Feb 28 19:09:32 KST 2025

--- src/enumType/EnumTest.java
+++ src/enumType/EnumTest.java
@@ -4,7 +4,10 @@ public class EnumTest {
   public static void main(String[] args) {
     OrderStatus status = OrderStatus.SHIPPED;
 
-    OrderStatus wrongStatus = OrderStatus.SHIPPING;
+    // 잘못된 enum 상수 사용 시도
+    // ❌ 컴파일 에러 - SHIPPING이라는 상수가 OrderStatus에 정의되어 있지 않음
+    // enum 상수 자체는 변경되거나 새로 생성될 수 없습니다.
+    // OrderStatus wrongStatus = OrderStatus.SHIPPING;
   }
 
 }
