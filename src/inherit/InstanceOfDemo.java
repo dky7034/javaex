d3e3515837eb8f7af714b5744c0136980548fd83
artp <dky7034@naver.com>
2025-02-28 09:33:14 +0900

    Auto commit: Fri Feb 28 09:33:14 KST 2025

--- src/inherit/InstanceOfDemo.java
+++ src/inherit/InstanceOfDemo.java
@@ -1,4 +1,13 @@
 package inherit;
 
 public class InstanceOfDemo {
+  public static void main(String[] args) {
+    Person p = new Person();
+    Student s = new Student();
+    Worker w = new Worker();
+
+    System.out.println(p instanceof Person);
+    System.out.println(s instanceof Person);
+    System.out.println(w instanceof Person);
+  }
 }
