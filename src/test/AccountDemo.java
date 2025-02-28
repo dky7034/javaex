c605e24cb0577dfef0b10abbec9f86c71f3a4847
artp <dky7034@naver.com>
2025-02-28 15:30:02 +0900

    Auto commit: Fri Feb 28 15:30:01 KST 2025

--- src/test/AccountDemo.java
+++ src/test/AccountDemo.java
@@ -6,6 +6,6 @@ public class AccountDemo {
     accounts[0] = new Account("111-111", "홍길동");
     accounts[0].setBalance(10000);
 
-    System.out.println("accounts = " + accounts);
+    System.out.println("accounts = " + accounts[0]);
   }
 }
