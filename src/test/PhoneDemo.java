cd5fdfb073707bd713da29c6701282d7df3c95c4
artp <dky7034@naver.com>
2025-02-28 12:24:54 +0900

    Auto commit: Fri Feb 28 12:24:54 KST 2025

--- src/test/PhoneDemo.java
+++ src/test/PhoneDemo.java
@@ -4,7 +4,7 @@ public class PhoneDemo {
   public static void main(String[] args) {
     Phone[] phones = {new Phone("황진이"), new TelePhone("길동이", "내일"), new SmartPhone("민국이", "갤러그")};
 
-    for (int i = 0; i < ; i++) {
+    for (int i = 0; i < phones.length; i++) {
       
     }
     
