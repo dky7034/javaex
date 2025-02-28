9916f62f8806ae6a7258326b7baa4004c7e3e305
artp <dky7034@naver.com>
2025-02-28 10:44:24 +0900

    Auto commit: Fri Feb 28 10:44:24 KST 2025

--- src/test/Point.java
+++ src/test/Point.java
@@ -1,7 +1,9 @@
 package test;
 
 public class Point {
-  
+  // 필드
+  // 생성자
+  // 메서드
 }
 
 class MovablePoint extends Point {
--- src/test/PointDemo.java
+++ src/test/PointDemo.java
@@ -2,6 +2,6 @@ package test;
 
 public class PointDemo {
   public static void main(String[] args) {
-    
+
   }
 }
