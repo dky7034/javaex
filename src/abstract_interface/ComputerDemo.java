627ddc914d746a042dee92d2770e1e318085306c
artp <dky7034@naver.com>
2025-03-04 11:20:04 +0900

    Auto commit: Tue Mar  4 11:20:04 KST 2025

--- src/abstract_interface/ComputerDemo.java
+++ src/abstract_interface/ComputerDemo.java
@@ -4,5 +4,12 @@ public class ComputerDemo {
   public static void main(String[] args) {
     Computer c = new Computer();
     Computer c1 = new Computer();
+
+    c.turnOn();
+    c.turnOff();
+
+    NoteBook nb = new NoteBook();
+    nb.turnOn();
+    nb.turnOff();
   }
 }
