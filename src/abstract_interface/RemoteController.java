e7f4d3ced237d45ed80da529548b0e2a99ca8a25
artp <dky7034@naver.com>
2025-03-04 10:49:34 +0900

    Auto commit: Tue Mar  4 10:49:34 KST 2025

--- src/abstract_interface/RemoteController.java
+++ src/abstract_interface/RemoteController.java
@@ -1,7 +1,7 @@
 package abstract_interface;
 
-public class RemoteController {
+public class RemoteController implements RemoteControllable {
   public static void main(String[] args) {
-    
+
   }
 }
