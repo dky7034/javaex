01fcd8f47a808891aaca152f88d97de9f8951ed9
artp <dky7034@naver.com>
2025-03-04 15:25:21 +0900

    Auto commit: Tue Mar  4 15:25:21 KST 2025

--- src/test/Abstract.java
+++ src/test/Abstract.java
@@ -2,9 +2,29 @@ package test;
 
 public abstract class Abstract {
   public int i;
+
   public Abstract(int i) {
     this.i = i;
   }
+
   public abstract void show();
+}
+
+class Concrete extends Abstract {
+  // 필드
+  int i;
+
+  // 생성자
+  public Concrete(int i, int i1) {
+    super(i);
+    this.i = i1;
+  }
+  
+  // 메서드
+  // 부모 메서드 오버라이드
+  @Override
+  public void show() {
+    System.out.println("Concrete 가 오버라이딩한 메서드입니다.");
+  }
 }
 
