69b92579bd9bba3a9810a8c335896d33f88e2d9a
artp <dky7034@naver.com>
2025-03-04 15:17:39 +0900

    Auto commit: Tue Mar  4 15:17:39 KST 2025

--- src/test/ConcreteDemo.java
+++ src/test/ConcreteDemo.java
@@ -2,8 +2,7 @@ package test;
 
 public class ConcreteDemo {
   public static void main(String[] args) {
-    Concrete c = new Concrete(1, 2) {}
-
-
+    Concrete c = new Concrete(1, 2);
+    c.show();
   }
 }
