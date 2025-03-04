fc17378f03066ff9b330d5d04b1dedad5196e65b
artp <dky7034@naver.com>
2025-03-04 16:04:49 +0900

    Auto commit: Tue Mar  4 16:04:49 KST 2025

--- src/test/LineDemo.java
+++ src/test/LineDemo.java
@@ -4,7 +4,7 @@ public class LineDemo {
   public static void main(String[] args) {
     Line l1 = new Line(30);
     Line l2 = new Line(90);
-    if (l1.length == -999) {
+    if (l1.compareTo(l2) == -999) {
       System.out.println("비교할 수 없습니다.");
     } else if (l1.compareTo(l2) == 0) {
       System.out.println("두 라인의 길이가 같습니다.");
