5eb52b6138ee3d561ca7b6170e4f22a121ce863d
artp <dky7034@naver.com>
2025-03-04 10:38:32 +0900

    Auto commit: Tue Mar  4 10:38:32 KST 2025

--- src/abstract_interface/TV.java
+++ src/abstract_interface/TV.java
@@ -3,11 +3,11 @@ package abstract_interface;
 public class TV implements Controllable {
   @Override
   public void turnOn() {
-
+    System.out.println("TV를 켜다");
   }
 
   @Override
   public void turnOff() {
-
+    System.out.println("TV를 끄다");
   }
 }
