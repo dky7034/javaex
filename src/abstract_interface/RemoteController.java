7d239cf1157ab67b014ff3906983641237ba24e5
artp <dky7034@naver.com>
2025-03-04 10:49:42 +0900

    Auto commit: Tue Mar  4 10:49:42 KST 2025

--- src/abstract_interface/RemoteController.java
+++ src/abstract_interface/RemoteController.java
@@ -4,4 +4,23 @@ public class RemoteController implements RemoteControllable {
   public static void main(String[] args) {
 
   }
+  @Override
+  public void remoteOn() {
+
+  }
+
+  @Override
+  public void remoteOff() {
+
+  }
+
+  @Override
+  public void turnOn() {
+
+  }
+
+  @Override
+  public void turnOff() {
+
+  }
 }
