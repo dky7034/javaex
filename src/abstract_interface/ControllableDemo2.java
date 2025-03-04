1d8530705a5375321f1e3282d75dc8eb467afa77
artp <dky7034@naver.com>
2025-03-04 11:45:31 +0900

    Auto commit: Tue Mar  4 11:45:31 KST 2025

--- src/abstract_interface/ControllableDemo2.java
+++ src/abstract_interface/ControllableDemo2.java
@@ -9,7 +9,7 @@ public class ControllableDemo2 {
   // 형변환과 다형성을 테스트해 보세요.
   public static void test(Controllable controllable) {
     Controllable[] controllables = new Controllable[]{new TV(), new Computer(), new NoteBook()};
-    for (Controllable controllable1 : controllables) {
+    for (Controllable controllable : controllables) {
 
     }
 
