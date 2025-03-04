c86262e55a66a52046350a01bd73045eb80d198e
artp <dky7034@naver.com>
2025-03-04 15:06:57 +0900

    Auto commit: Tue Mar  4 15:06:57 KST 2025

--- src/abstract_interface/LocalAnonymousDemo.java
+++ src/abstract_interface/LocalAnonymousDemo.java
@@ -1,4 +1,12 @@
 package abstract_interface;
 
 public class LocalAnonymousDemo {
+  public static void main(String[] args) {
+    Bird b = new Bird() {
+      @Override
+      void move() {
+        System.out.println("독수리가 난다.");
+      }
+    };
+  }
 }
