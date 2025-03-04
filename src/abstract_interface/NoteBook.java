4d018737e9388c2b27d3bca2ea8f5c1baaee9a13
artp <dky7034@naver.com>
2025-03-04 11:18:23 +0900

    Auto commit: Tue Mar  4 11:18:23 KST 2025

--- src/abstract_interface/NoteBook.java
+++ src/abstract_interface/NoteBook.java
@@ -5,6 +5,7 @@ public class NoteBook extends Computer implements Portable {
   public void inMyBag() {
     System.out.println("노트북은 내 가방 안에 있다.");
   }
+  
   @Override
   public void turnOn() {
     System.out.println("노트북을 켠다.");
