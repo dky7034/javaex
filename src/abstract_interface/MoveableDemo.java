7dd611030396d034b7995b1a9002f9d660e59c5a
artp <dky7034@naver.com>
2025-03-04 12:20:13 +0900

    Auto commit: Tue Mar  4 12:20:13 KST 2025

--- src/abstract_interface/MoveableDemo.java
+++ src/abstract_interface/MoveableDemo.java
@@ -3,5 +3,9 @@ package abstract_interface;
 public class MoveableDemo {
   public static void main(String[] args) {
     Movable m = new Car();
+    m.move();
+    Car c = (Car) m;
+    c.move();
+    c.show();
   }
 }
