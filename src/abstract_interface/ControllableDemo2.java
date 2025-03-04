07a42fafe8272eaeb8c80a924bdcd964d02f672a
artp <dky7034@naver.com>
2025-03-04 11:46:45 +0900

    Auto commit: Tue Mar  4 11:46:45 KST 2025

--- src/abstract_interface/ControllableDemo2.java
+++ src/abstract_interface/ControllableDemo2.java
@@ -3,6 +3,8 @@ package abstract_interface;
 public class ControllableDemo2 {
   public static void main(String[] args) {
     test(new TV());
+    test(new Computer());
+    test(new NoteBook());
   }
 
   // 매개변수를 인터페이스로 주고, (tv, computer, notebook 객체를 이용)
