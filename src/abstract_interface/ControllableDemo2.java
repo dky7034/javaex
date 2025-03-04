074f1f2481730b9cab87998f8bbf03b274bc9858
artp <dky7034@naver.com>
2025-03-04 11:45:27 +0900

    Auto commit: Tue Mar  4 11:45:27 KST 2025

--- src/abstract_interface/ControllableDemo2.java
+++ src/abstract_interface/ControllableDemo2.java
@@ -2,16 +2,16 @@ package abstract_interface;
 
 public class ControllableDemo2 {
   public static void main(String[] args) {
-
+    test();
   }
 
   // 매개변수를 인터페이스로 주고, (tv, computer, notebook 객체를 이용)
   // 형변환과 다형성을 테스트해 보세요.
   public static void test(Controllable controllable) {
     Controllable[] controllables = new Controllable[]{new TV(), new Computer(), new NoteBook()};
-    controllable.turnOn();
-    controllable.turnOff();
-    controllable.repair();
-    
+    for (Controllable controllable1 : controllables) {
+
+    }
+
   }
 }
