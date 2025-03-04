5f720e31635f15742f9d52ecc85be59a81cb0429
artp <dky7034@naver.com>
2025-03-04 15:44:14 +0900

    Auto commit: Tue Mar  4 15:44:14 KST 2025

--- src/test/Line.java
+++ src/test/Line.java
@@ -4,8 +4,8 @@ public class Line {
   public static void main(String[] args) {
     A a = new A();
     B b = new B();
-    if (a.compareTo(b)) {
-      
+    if (a.compareTo(b) == 0) {
+
     }
   }
 
