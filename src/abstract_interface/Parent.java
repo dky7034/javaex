ca153f708288c3af05b253a3edf0d48b76b9f792
artp <dky7034@naver.com>
2025-03-04 14:17:49 +0900

    Auto commit: Tue Mar  4 14:17:49 KST 2025

--- src/abstract_interface/Parent.java
+++ src/abstract_interface/Parent.java
@@ -2,10 +2,14 @@ package abstract_interface;
 
 public class Parent {
   void method1() {
-    
+    System.out.println("부모입니다.");
   }
 }
 
 class OnlyOnce extends Parent {
+  @Override
+  void method1() {
+    super.method1();
+  }
 }
 
