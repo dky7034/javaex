12c3dc82a4472407e4c9377c92ec2bf580e81b4e
artp <dky7034@naver.com>
2025-03-04 10:22:33 +0900

    Auto commit: Tue Mar  4 10:22:33 KST 2025

--- src/abstract_interface/CoinDemo1.java
+++ src/abstract_interface/CoinDemo1.java
@@ -3,5 +3,6 @@ package abstract_interface;
 public class CoinDemo1 {
   public static void main(String[] args) {
     System.out.println("Dime은 " + Coin.DIME + "센트입니다.");
+    System.out.println("Quarter는 " + Coin.QUARTER + "센트입니다.");
   }
 }
