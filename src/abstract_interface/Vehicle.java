c1034e8cd11c303fabb70da6efcf7d7e6c8cfe35
artp <dky7034@naver.com>
2025-03-04 12:15:23 +0900

    Auto commit: Tue Mar  4 12:15:23 KST 2025

--- src/abstract_interface/Vehicle.java
+++ src/abstract_interface/Vehicle.java
@@ -3,12 +3,12 @@ package abstract_interface;
 public interface Vehicle {
   // 반드시 가져야 하는 기능
   void move();
-
+  
 }
 
 class Car implements Vehicle {
   @Override
   public void move() {
-    
+
   }
 }
