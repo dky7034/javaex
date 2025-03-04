53dc8af6f45fdc87a6283bc9ad149524d5292e2b
artp <dky7034@naver.com>
2025-03-04 15:02:39 +0900

    Auto commit: Tue Mar  4 15:02:39 KST 2025

--- src/abstract_interface/LocalClassDemo.java
+++ src/abstract_interface/LocalClassDemo.java
@@ -1,4 +1,12 @@
 package abstract_interface;
 
 public class LocalClassDemo {
+  public static void main(String[] args) {
+    class Eagle extends Bird {
+      @Override
+      void move() {
+        System.out.println("독수리가 난다.");
+      }
+    }
+  }
 }
