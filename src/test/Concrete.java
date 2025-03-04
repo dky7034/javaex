46a5f5a3bcefb6563ded8eb8044c684da5294945
artp <dky7034@naver.com>
2025-03-04 15:15:27 +0900

    Auto commit: Tue Mar  4 15:15:27 KST 2025

--- src/test/Concrete.java
+++ src/test/Concrete.java
@@ -14,6 +14,8 @@ public class Concrete extends Abstract {
   @Override
   public void show() {
     System.out.println("Concrete 가 오버라이딩한 메서드입니다.");
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
     System.out.println("i = " + i);
   }
 }
