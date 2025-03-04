a1a4b27b9cfaa380c9e74866763f6cb753adf439
artp <dky7034@naver.com>
2025-03-04 14:24:35 +0900

    Auto commit: Tue Mar  4 14:24:35 KST 2025

--- src/abstract_interface/ParentDemo.java
+++ src/abstract_interface/ParentDemo.java
@@ -9,7 +9,7 @@ public class ParentDemo {
         System.out.println("한 번만 호출됩니다.");
       }
     };
-    p.method1();
+    p.method1(); // 한 번만 호출됩니다.
 
     Parent parent = new Parent();
     parent.method1(); // 부모입니다.
