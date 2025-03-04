e2a83601757947f2c2b2ecb3cfa1ff584eef290d
artp <dky7034@naver.com>
2025-03-04 11:48:24 +0900

    Auto commit: Tue Mar  4 11:48:24 KST 2025

--- src/abstract_interface/ControllableDemo2.java
+++ src/abstract_interface/ControllableDemo2.java
@@ -10,13 +10,8 @@ public class ControllableDemo2 {
   // 매개변수를 인터페이스로 주고, (tv, computer, notebook 객체를 이용)
   // 형변환과 다형성을 테스트해 보세요.
   public static void test(Controllable c) {
-    Controllable[] controllables = new Controllable[]{new TV(), new Computer(), new NoteBook()};
-    System.out.println("-------------------------");
-    for (Controllable controllable : controllables) {
-      controllable.turnOn();
-      controllable.turnOff();
-      controllable.repair();
-    }
-
+    c.turnOn();
+    c.turnOff();
+    c.repair();
   }
 }
