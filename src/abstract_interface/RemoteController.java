26b18d5ba7c54922e130af72c51f146cedeb9eb7
artp <dky7034@naver.com>
2025-03-04 10:50:25 +0900

    Auto commit: Tue Mar  4 10:50:25 KST 2025

--- src/abstract_interface/RemoteController.java
+++ src/abstract_interface/RemoteController.java
@@ -6,17 +6,17 @@ public class RemoteController implements RemoteControllable {
   }
   @Override
   public void remoteOn() {
-
+    System.out.println("리모컨를 키다");
   }
 
   @Override
   public void remoteOff() {
-
+    System.out.println("리모컨을 끄다");
   }
 
   @Override
   public void turnOn() {
-
+    System.out.println("TV를 키다");
   }
 
   @Override
