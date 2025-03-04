2cfce617c480681b36766ef55718bd8b0afe3275
artp <dky7034@naver.com>
2025-03-04 14:21:23 +0900

    Auto commit: Tue Mar  4 14:21:23 KST 2025

--- src/abstract_interface/ParentDemo.java
+++ src/abstract_interface/ParentDemo.java
@@ -3,7 +3,7 @@ package abstract_interface;
 public class ParentDemo {
   public static void main(String[] args) {
     Parent p = new Parent() {
-      // Parent
+      // Parent Class를 상속받은 익명 클래스 
     }
   }
 }
