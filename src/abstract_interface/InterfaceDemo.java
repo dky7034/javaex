107d8580233085b302bc0306b640ad8919392dd8
artp <dky7034@naver.com>
2025-03-04 10:39:34 +0900

    Auto commit: Tue Mar  4 10:39:34 KST 2025

--- src/abstract_interface/InterfaceDemo.java
+++ src/abstract_interface/InterfaceDemo.java
@@ -3,5 +3,9 @@ package abstract_interface;
 public class InterfaceDemo {
   public static void main(String[] args) {
     TV tv = new TV();
+    tv.turnOn();
+    tv.turnOff();
+    tv.repair();
+    
   }
 }
