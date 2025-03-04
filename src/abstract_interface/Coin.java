973ed6dde9666674dbcb9f805f094014758ef313
artp <dky7034@naver.com>
2025-03-04 10:27:22 +0900

    Auto commit: Tue Mar  4 10:27:22 KST 2025

--- src/abstract_interface/Coin.java
+++ src/abstract_interface/Coin.java
@@ -1,5 +1,6 @@
 package abstract_interface;
 
 public interface Coin {
+  // 인터페이스에서 상수 앞에는 public static final이 생략되어 있고, 컴파일러가 나중에 추가해 줍니다.
   int PENNY = 1, NICKEL = 5, DIME = 10, QUARTER = 25;
 }
