41b5e3bf36bc87de60ff41bf496fbc318aee400f
artp <dky7034@naver.com>
2025-03-04 15:50:26 +0900

    Auto commit: Tue Mar  4 15:50:26 KST 2025

--- src/test/LineDemo.java
+++ src/test/LineDemo.java
@@ -5,9 +5,9 @@ public class LineDemo {
     Line l1 = new Line(10);
     Line l2 = new Line(20);
     if (l1.compareTo(l2) == 0) {
-      System.out.println("두 객체가 동일합니다.");
+      System.out.println("두 라인의 길이가 같습니다.");
     } else if (l1.compareTo(l2) > 0) {
-      System.out.println("a 가 b 객체보다 더 큽니다.");
+      System.out.println("l1 라인이 l2 라인보다 깁니다.");
     } else {
       System.out.println("b 가 a 객체보다 더 큽니다.");
     }
