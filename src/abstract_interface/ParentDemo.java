a81d2164b7367add98fedfc699038260962a5d37
artp <dky7034@naver.com>
2025-03-04 14:21:46 +0900

    Auto commit: Tue Mar  4 14:21:46 KST 2025

--- src/abstract_interface/ParentDemo.java
+++ src/abstract_interface/ParentDemo.java
@@ -3,7 +3,11 @@ package abstract_interface;
 public class ParentDemo {
   public static void main(String[] args) {
     Parent p = new Parent() {
-      // Parent Class를 상속받은 익명 클래스 
+      // Parent Class를 상속받은 익명 클래스 → 오버라이딩한 메서드
+      @Override
+      void method1() {
+        super.method1();
+      }
     }
   }
 }
