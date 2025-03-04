467a55550d470d52f60495230b68b4610d32aba0
artp <dky7034@naver.com>
2025-03-04 18:44:58 +0900

    Auto commit: Tue Mar  4 18:44:58 KST 2025

--- src/test/abc.java
+++ src/test/abc.java
@@ -3,6 +3,8 @@ package test;
 public class abc implements C {
   public static void main(String[] args) {
     System.out.println(A.value);
+    System.out.println(B.value);
+    System.out.println(abc.value);
   }
 }
 
