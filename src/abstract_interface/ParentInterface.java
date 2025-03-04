68540e0fdf09df335711266997a8d2b46cf3d848
artp <dky7034@naver.com>
2025-03-04 14:30:51 +0900

    Auto commit: Tue Mar  4 14:30:51 KST 2025

--- src/abstract_interface/ParentInterface.java
+++ src/abstract_interface/ParentInterface.java
@@ -3,3 +3,8 @@ package abstract_interface;
 public interface ParentInterface {
   void method1();
 }
+
+//  interface OnlyOnce implements ParentInterface {
+//    @Override
+//    void method1() {}
+//  }
