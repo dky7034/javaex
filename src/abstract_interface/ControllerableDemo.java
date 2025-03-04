f0445e04cff35e2b7df0656bdb3ff50df24dcc1a
artp <dky7034@naver.com>
2025-03-04 11:36:36 +0900

    Auto commit: Tue Mar  4 11:36:36 KST 2025

--- src/abstract_interface/ControllerableDemo.java
+++ src/abstract_interface/ControllerableDemo.java
@@ -3,5 +3,13 @@ package abstract_interface;
 public class ControllerableDemo {
   public static void main(String[] args) {
     Controllable computer = new Computer;
+    computer.turnOn();
+    computer.repair();
+    computer.repair();
+    Controllable n = new NoteBook();
+    n.turnOn();
+    n.turnOff();
+    n.repair();
+    Controllable tv = new Controllable();
   }
 }
