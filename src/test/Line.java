b1c7738018cc62c9bf281170586d03670105a81b
artp <dky7034@naver.com>
2025-03-04 15:47:41 +0900

    Auto commit: Tue Mar  4 15:47:41 KST 2025

--- src/test/Line.java
+++ src/test/Line.java
@@ -3,7 +3,6 @@ package test;
 public class Line {
   public static void main(String[] args) {
     A a = new A();
-    B b = new B();
     if (a.compareTo(b) == 0) {
       System.out.println("두 객체가 동일합니다.");
     } else if (a.compareTo(b) > 0) {
