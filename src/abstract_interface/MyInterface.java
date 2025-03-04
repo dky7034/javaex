7048c6ca8d719a3637c0557a5e9a70ea3d704323
artp <dky7034@naver.com>
2025-03-04 09:28:34 +0900

    Auto commit: Tue Mar  4 09:28:34 KST 2025

--- src/abstract_interface/MyInterface.java
+++ src/abstract_interface/MyInterface.java
@@ -16,6 +16,13 @@ interface YourInterface extends MyInterface {
 
 // 클래스는 인터페이스를 구현
 class MyClass implements YourInterface {
-  
+  @Override
+  public void sayHello2() {
 
+  }
+
+  @Override
+  public void sayHello() {
+
+  }
 }
