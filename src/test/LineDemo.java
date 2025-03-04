74f3f9b87caabc89b9c5994986724b765169e0ab
artp <dky7034@naver.com>
2025-03-04 16:18:09 +0900

    Auto commit: Tue Mar  4 16:18:09 KST 2025

--- src/test/LineDemo.java
+++ src/test/LineDemo.java
@@ -2,7 +2,7 @@ package test;
 
 public class LineDemo {
   public static void main(String[] args) {
-    Line l1 = new Line(30);
+    Line l1 = new Line(130);
     Line l2 = new Line(90);
     if (l1.compareTo(l2) == -999) {
       System.out.println("비교할 수 없습니다.");
