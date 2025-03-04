1c4767426029c02f994f563d457ac1660a6ad12e
artp <dky7034@naver.com>
2025-03-04 12:20:25 +0900

    Auto commit: Tue Mar  4 12:20:25 KST 2025

--- src/abstract_interface/MoveableDemo.java
+++ src/abstract_interface/MoveableDemo.java
@@ -4,6 +4,9 @@ public class MoveableDemo {
   public static void main(String[] args) {
     Movable m = new Car();
     m.move();
+
+    System.out.println("----------");
+
     Car c = (Car) m;
     c.move();
     c.show();
