2095a658c1a4e051ae15a7fa68a282b1657ef410
artp <dky7034@naver.com>
2025-03-04 16:31:47 +0900

    Auto commit: Tue Mar  4 16:31:47 KST 2025

--- src/test/PersonDemo.java
+++ src/test/PersonDemo.java
@@ -4,8 +4,8 @@ public class PersonDemo {
   public static void main(String[] args) {
     Person[] people = new Person[3];
     people[0] = new Person("홍길동", 19);
-    people[1] = new Person(20);
-    people[2] = new Person(17);
+    people[1] = new Person("유관순", 23);
+    people[2] = new Person("손흥민", 21);
 
     for (int i = 0; i < people.length; i++) {
       if (people[i].compareTo(people[i+1]) == 0) {
