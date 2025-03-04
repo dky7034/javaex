fbe09075bb48f6f127e6781b7cf591987c4fbd06
artp <dky7034@naver.com>
2025-03-04 09:37:01 +0900

    Auto commit: Tue Mar  4 09:37:00 KST 2025

--- src/abstract_interface/MyInterfaceDemo.java
+++ src/abstract_interface/MyInterfaceDemo.java
@@ -3,6 +3,8 @@ package abstract_interface;
 public class MyInterfaceDemo extends MyClass {
   public static void main(String[] args) {
     MyClass m = new MyClass();
+    System.out.println(m.myClassInt);
+    System.out.println(m.yourClassInt);
   }
 
 }
