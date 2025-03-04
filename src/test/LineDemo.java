6c0140fc2d2a4e15c4e6263407a6d8f9643e5090
artp <dky7034@naver.com>
2025-03-04 15:57:26 +0900

    Auto commit: Tue Mar  4 15:57:26 KST 2025

--- src/test/LineDemo.java
+++ src/test/LineDemo.java
@@ -4,11 +4,12 @@ public class LineDemo {
   public static void main(String[] args) {
     Line l1 = new Line(10);
     Line l2 = new Line(20);
-    if (l1.compareTo(l2) == 0) {
+    if () {
+    else if (l1.compareTo(l2) == 0) {
       System.out.println("두 라인의 길이가 같습니다.");
     } else if (l1.compareTo(l2) > 0) {
       System.out.println("l1 라인이 l2 라인보다 깁니다.");
-    } else {
+    } else if (l1.com) {
       System.out.println("l2 라인이 l1 라인보다 깁니다.");
     }
   }
