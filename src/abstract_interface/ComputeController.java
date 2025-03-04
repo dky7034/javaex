8db3dd6f9bc38b65c5a96808bbdf6290e5ebc9ee
artp <dky7034@naver.com>
2025-03-04 11:07:06 +0900

    Auto commit: Tue Mar  4 11:07:06 KST 2025

--- src/abstract_interface/ComputeController.java
+++ src/abstract_interface/ComputeController.java
@@ -1,5 +1,18 @@
 package abstract_interface;
 
 public class ComputeController extends Controllable {
-  
+  @Override
+  public void turnOn() {
+    
+  }
+
+  @Override
+  public void turnOff() {
+
+  }
+
+  @Override
+  public void repair() {
+    Controllable.super.repair();
+  }
 }
