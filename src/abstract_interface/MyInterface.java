ab82b36731b57f12ad260c2c04ae322f2d2ec736
artp <dky7034@naver.com>
2025-03-04 10:08:11 +0900

    Auto commit: Tue Mar  4 10:08:11 KST 2025

--- src/abstract_interface/MyInterface.java
+++ src/abstract_interface/MyInterface.java
@@ -6,7 +6,9 @@ public interface MyInterface {
   // 추상메서드
   public abstract void sayHello();
 
-  public abstract void myNameIs();
+  default void myNameIs() {
+    
+  }
 }
 
 class NewClass implements MyInterface {
