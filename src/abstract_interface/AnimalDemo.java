bcc0b51b09b0b7184ef365a586968dbb5aacda55
artp <dky7034@naver.com>
2025-03-04 12:11:04 +0900

    Auto commit: Tue Mar  4 12:11:04 KST 2025

--- src/abstract_interface/AnimalDemo.java
+++ src/abstract_interface/AnimalDemo.java
@@ -10,13 +10,13 @@ public class AnimalDemo {
     for (Animal animal : animals) {
       animal.sound();
     }
-
+    
     makeSound(new Dog()); // 강아지가 소리를 낸다.
     makeSoune(new Cuckoo()); // 뻐꾸기가 소리를 낸다.
   }
 
   static void makeSound(Animal animal) {
-    
+
 
 
   }
