410e66357ec6b37a01d63e99e80b824419b071ec
artp <dky7034@naver.com>
2025-03-04 15:45:30 +0900

    Auto commit: Tue Mar  4 15:45:30 KST 2025

--- src/test/Line.java
+++ src/test/Line.java
@@ -8,6 +8,8 @@ public class Line {
       System.out.println("두 객체가 동일합니다.");
     } else if (a.compareTo(b) > 0) {
       System.out.println("a 가 b 객체보다 더 큽니다.");
+    } else {
+      System.out.println("b 가 a 객체보다 더 큽니다.");
     }
   }
 
