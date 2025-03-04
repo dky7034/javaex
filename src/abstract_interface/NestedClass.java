7d9b7e5774c14d45ae3003c94701dcc6a168c35d
artp <dky7034@naver.com>
2025-03-04 14:14:14 +0900

    Auto commit: Tue Mar  4 14:14:14 KST 2025

--- src/abstract_interface/NestedClass.java
+++ src/abstract_interface/NestedClass.java
@@ -2,11 +2,8 @@ package abstract_interface;
 
 public class NestedClass {
   class InnerClass {
-
   }
 }
 
 class Parent {}
-class OnlyOnce extends Parent {
-  
-}
+class OnlyOnce extends Parent {}
