3e95dafd9fefa43aba58910e629606e85b06e111
artp <dky7034@naver.com>
2025-03-04 11:42:53 +0900

    Auto commit: Tue Mar  4 11:42:53 KST 2025

--- src/abstract_interface/ControllableDemo2.java
+++ src/abstract_interface/ControllableDemo2.java
@@ -7,7 +7,7 @@ public class ControllableDemo2 {
 
   // 매개변수를 인터페이스로 주고, (tv, computer, notebook 객체를 이용)
   // 형변환과 다형성을 테스트해 보세요.
-  public static void test() {
+  public static void test(Controllable controllable) {
 
   }
 }
