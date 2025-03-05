fb1da0f663a5e472ac7abed88fcd95869a35b130
artp <dky7034@naver.com>
2025-03-05 17:28:16 +0900

    Auto commit: Wed Mar  5 17:28:16 KST 2025

--- src/exercise/chapter7/Ex6.java
+++ src/exercise/chapter7/Ex6.java
@@ -1,4 +1,7 @@
 package exercise.chapter7;
 
 public class Ex6 {
+  public static void main(String[] args) {
+    
+  }
 }
--- src/exercise/chapter8/Ex1_2.java
+++ src/exercise/chapter8/Ex1_2.java
@@ -2,6 +2,6 @@ package exercise.chapter8;
 
 public class Ex1_2 {
   public static void main(String[] args) {
-    
+
   }
 }
