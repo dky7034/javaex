c64e104022420030e1dc1cd836ada6c041f3bc30
artp <dky7034@naver.com>
2025-03-05 23:14:17 +0900

    Auto commit: Wed Mar  5 23:14:17 KST 2025

--- src/exercise/chapter7/Ex6.java
+++ src/exercise/chapter7/Ex6.java
@@ -30,9 +30,5 @@ class Worker implements Human {
   public void eat() {
     System.out.println("빵을 먹습니다.");
   }
-
-  @Override
-  public void print() {
-    System.out.println("인간입니다.");
-  }
+  
 }
