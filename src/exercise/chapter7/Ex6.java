ab07adf045411db1698617fc0a45b3948b88b78b
artp <dky7034@naver.com>
2025-03-05 17:51:08 +0900

    Auto commit: Wed Mar  5 17:51:08 KST 2025

--- src/exercise/chapter7/Ex6.java
+++ src/exercise/chapter7/Ex6.java
@@ -1,41 +1,41 @@
-package exercise.chapter7;
-
-public class Ex6 {
-  public static void main(String[] args) {
-    Human.echo(); // 야호!!
-    Student s = new Student(20);
-    s.print(); // 20세의 학생입니다.
-    s.eat(); // 도시락을 먹습니다.
-
-    Human p = new Worker();
-    p.print(); // 인간입니다.
-    p.eat(); // 빵을 먹습니다.
-  }
-}
-
-interface Human {
-  void eat();
-  void print();
-}
-
-class Worker implements Human {
-  @Override
-  public void eat() {
-    System.out.println("빵을 먹습니다.");
-  }
-
-  @Override
-  public void print() {
-  }
-}
-
-class Student extends Worker {
-  // 필드
-  int age;
-
-  // 생성자
-  public Student(int age) {
-    this.age = age;
-  }
-
-}
+//package exercise.chapter7;
+//
+//public class Ex6 {
+//  public static void main(String[] args) {
+//    Human.echo(); // 야호!!
+//    Student s = new Student(20);
+//    s.print(); // 20세의 학생입니다.
+//    s.eat(); // 도시락을 먹습니다.
+//
+//    Human p = new Worker();
+//    p.print(); // 인간입니다.
+//    p.eat(); // 빵을 먹습니다.
+//  }
+//}
+//
+//interface Human {
+//  void eat();
+//  void print();
+//}
+//
+//class Worker implements Human {
+//  @Override
+//  public void eat() {
+//    System.out.println("빵을 먹습니다.");
+//  }
+//
+//  @Override
+//  public void print() {
+//  }
+//}
+//
+//class Student extends Worker {
+//  // 필드
+//  int age;
+//
+//  // 생성자
+//  public Student(int age) {
+//    this.age = age;
+//  }
+//
+//}
