b95734b0854d56e0d20390b506a8b967b9630406
artp <dky7034@naver.com>
2025-03-05 10:30:38 +0900

    Auto commit: Wed Mar  5 10:30:38 KST 2025

--- src/default_package/ObjectDemo.java
+++ src/default_package/ObjectDemo.java
@@ -3,5 +3,6 @@ package default_package;
 public class ObjectDemo {
   public static void main(String[] args) {
     Keyboard keyboard = new Keyboard("키보드");
+    Mouse mouse = new Mouse("마우스");
   }
 }
