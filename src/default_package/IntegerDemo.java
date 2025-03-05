dc691eca5e2c71006bd663787b85e239ba1519b1
artp <dky7034@naver.com>
2025-03-05 12:05:32 +0900

    Auto commit: Wed Mar  5 12:05:32 KST 2025

--- src/default_package/IntegerDemo.java
+++ src/default_package/IntegerDemo.java
@@ -2,7 +2,8 @@ package default_package;
 
 public class IntegerDemo {
   public static void main(String[] args) {
-    Integer b = new Integer(10);
-    
+    Integer b = null;
+    System.out.println(b.equals(10));
+
   }
 }
