6bd82c2326798cadcf8ccc49cedd6101c437c766
artp <dky7034@naver.com>
2025-03-05 12:29:48 +0900

    Auto commit: Wed Mar  5 12:29:48 KST 2025

--- src/default_package/CalendarDemo.java
+++ src/default_package/CalendarDemo.java
@@ -5,5 +5,6 @@ import java.util.Calendar;
 public class CalendarDemo {
   public static void main(String[] args) {
     Calendar cal = Calendar.getInstance();
+    System.out.println(cal.getTime());
   }
 }
