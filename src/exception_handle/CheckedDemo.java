22b144b589f4ad05c2e6c9fd62a9b1d11ff26a17
artp <dky7034@naver.com>
2025-03-05 16:36:54 +0900

    Auto commit: Wed Mar  5 16:36:54 KST 2025

--- src/exception_handle/CheckedDemo.java
+++ src/exception_handle/CheckedDemo.java
@@ -2,14 +2,10 @@ package exception_handle;
 
 public class CheckedDemo {
   public static void main(String[] args) {
-
+    method1();
   }
-  static void method1() {
-    try {
-      method2();
-    } catch (ClassNotFoundException e) {
-      System.out.println(e.getMessage());
-    }
+  static void method1() throws ClassNotFoundException {
+    method2();
   }
   static void method2() throws ClassNotFoundException {
     Class clazz = Class.forName("java.lang.String");
