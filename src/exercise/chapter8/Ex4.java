373688f7129deb249107097457cfc46a465e6fa9
artp <dky7034@naver.com>
2025-03-05 15:30:29 +0900

    Auto commit: Wed Mar  5 15:30:29 KST 2025

--- src/exercise/chapter8/Ex4.java
+++ src/exercise/chapter8/Ex4.java
@@ -2,7 +2,9 @@ package exercise.chapter8;
 
 public class Ex4 {
   public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
 
+    }
   }
 }
 
@@ -13,7 +15,7 @@ class Dice {
 
   // 메서드
   void roll() {
-    
+
   }
 
 
