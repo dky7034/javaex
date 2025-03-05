b4bbcba8195815a7954a17fc4882c95dc15d1da6
artp <dky7034@naver.com>
2025-03-05 14:01:55 +0900

    Auto commit: Wed Mar  5 14:01:55 KST 2025

--- src/default_package/StringTokenizerDemo.java
+++ src/default_package/StringTokenizerDemo.java
@@ -7,5 +7,6 @@ public class StringTokenizerDemo {
     String s = "of the people, by the people, for the people";
 
     StringTokenizer st = new StringTokenizer(s, ",");
+    st.countTokens();
   }
 }
