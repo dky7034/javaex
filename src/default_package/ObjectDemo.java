23d1df1d0a724cc3e7a80489f471d2f4e2a49557
artp <dky7034@naver.com>
2025-03-05 11:26:17 +0900

    Auto commit: Wed Mar  5 11:26:17 KST 2025

--- src/default_package/ObjectDemo.java
+++ src/default_package/ObjectDemo.java
@@ -5,8 +5,9 @@ public class ObjectDemo {
     Keyboard keyboard = new Keyboard("키보드");
 
     Class c  = keyboard.getClass();
+    c.getName();
 
-        Keyboard keyboard1 = new Keyboard("키보드");
+    Keyboard keyboard1 = new Keyboard("키보드");
     Mouse mouse = new Mouse("마우스");
     Mouse mouse1 = new Mouse("마우스");
 
