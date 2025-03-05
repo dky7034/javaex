8fae06bb46f8682589afeed7a4ea9ac9b81be89b
artp <dky7034@naver.com>
2025-03-05 14:37:42 +0900

    Auto commit: Wed Mar  5 14:37:42 KST 2025

--- src/example/StudentTest.java
+++ src/example/StudentTest.java
@@ -3,6 +3,7 @@ package example;
 public class StudentTest {
   public static void main(String[] args) {
     System.out.println(new Student("김삿갓"));
+    System.out.println(new Student("홍길동"));
   }
 }
 
