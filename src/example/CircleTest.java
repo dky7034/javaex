96f96fee16ca68f7cbbe27dee3f265202f3ee5c0
artp <dky7034@naver.com>
2025-03-05 14:20:30 +0900

    Auto commit: Wed Mar  5 14:20:30 KST 2025

--- src/example/CircleTest.java
+++ src/example/CircleTest.java
@@ -2,6 +2,11 @@ package example;
 
 public class CircleTest {
   public static void main(String[] args) {
+    Circle c1 = new Circle(3);
+    Circle c2 = new Circle(3);
 
+    if (c1.equals(c2)) {
+      
+    }
   }
 }
