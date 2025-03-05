8b78b964fd3bbab6a9f20e3acd36fc88b80f367a
artp <dky7034@naver.com>
2025-03-05 12:07:23 +0900

    Auto commit: Wed Mar  5 12:07:23 KST 2025

--- src/default_package/IntegerDemo.java
+++ src/default_package/IntegerDemo.java
@@ -5,7 +5,7 @@ public class IntegerDemo {
     int a = 0; // a 기초타입
 
     Integer b = 10; // b 참조타입
-    Integer c = 10;
+    Integer c = 10; // c 참조타입
     System.out.println(b.equals(10));
     System.out.println(b.equals(c));
 
