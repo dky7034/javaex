3303b110147ad34f8b1c59b7b8c576990182d558
artp <dky7034@naver.com>
2025-03-05 16:52:42 +0900

    Auto commit: Wed Mar  5 16:52:42 KST 2025

--- src/exception_handle/UncheckedDemo2.java
+++ src/exception_handle/UncheckedDemo2.java
@@ -16,5 +16,6 @@ public class UncheckedDemo2 {
   static void method2() throws ArithmeticException, IndexOutOfBoundsException, NullPointerException, IllegalArgumentException {
     int i = 1 / 0; // ArithmeticException
     System.out.println(i);
+    throw new NullPointerException();
   }
 }
