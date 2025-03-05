5e255b782e31214a97a0a07dd44f39a7c20d6510
artp <dky7034@naver.com>
2025-03-05 17:31:28 +0900

    Auto commit: Wed Mar  5 17:31:28 KST 2025

--- src/exercise/chapter7/Ex6.java
+++ src/exercise/chapter7/Ex6.java
@@ -2,10 +2,10 @@ package exercise.chapter7;
 
 public class Ex6 {
   public static void main(String[] args) {
-    Human.echo();
+    Human.echo(); // 야호!!
     Student s = new Student(20);
-    s.print();
-    s.eat();
+    s.print(); // 20세의 학생입니다.
+    s.eat(); //
 
     Human p = new Worker();
     p.print();
