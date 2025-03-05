8e11ec1f22c42a4a5ff71f9d5c9136490907164f
artp <dky7034@naver.com>
2025-03-05 12:23:23 +0900

    Auto commit: Wed Mar  5 12:23:23 KST 2025

--- src/default_package/ArraysDemo.java
+++ src/default_package/ArraysDemo.java
@@ -6,6 +6,8 @@ public class ArraysDemo {
   public static void main(String[] args) {
     char[] a1 = {'j', 'a', 'v', 'a'};
     char[] a2 = Arrays.copyOf(a1, a1.length);
+
+    
     for (char c : a2) {
       System.out.println(c);
     }
