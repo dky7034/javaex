c392e16d4a9a1e96b609a10eec654f8b40f893df
artp <dky7034@naver.com>
2025-03-05 12:24:15 +0900

    Auto commit: Wed Mar  5 12:24:15 KST 2025

--- src/default_package/ArraysDemo.java
+++ src/default_package/ArraysDemo.java
@@ -6,8 +6,7 @@ public class ArraysDemo {
   public static void main(String[] args) {
     char[] a1 = {'j', 'a', 'v', 'a'};
     char[] a2 = Arrays.copyOf(a1, 2);
-
-    System.out.println("a1 = " + a1);
-    System.out.println("a2 = " + a2);
+    System.out.println(a1);
+    System.out.println(a2);
   }
 }
