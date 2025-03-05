f5cf9ef9187af30954660fb1d791a070731b4f63
artp <dky7034@naver.com>
2025-03-05 10:29:56 +0900

    Auto commit: Wed Mar  5 10:29:56 KST 2025

--- src/default_package/ObjectDemo.java
+++ src/default_package/ObjectDemo.java
@@ -2,6 +2,6 @@ package default_package;
 
 public class ObjectDemo {
   public static void main(String[] args) {
-    Keyboard keyboard = new Keyboard();
+    Keyboard keyboard = new Keyboard("키볻,");
   }
 }
