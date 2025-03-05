e73482608a2b26495cb8763b096307ad4ce1d210
artp <dky7034@naver.com>
2025-03-05 14:21:33 +0900

    Auto commit: Wed Mar  5 14:21:33 KST 2025

--- src/example/CircleTest.java
+++ src/example/CircleTest.java
@@ -3,7 +3,7 @@ package example;
 public class CircleTest {
   public static void main(String[] args) {
     Circle c1 = new Circle(3);
-    Circle c2 = new Circle(3);
+    Circle c2 = new Circle(4);
 
     if (c1.equals(c2)) {
       System.out.println("c1과 c2는 같다.");
