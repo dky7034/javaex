6bd2ed153cae6d38c5d5dcd78ae739a67fe1c21a
artp <dky7034@naver.com>
2025-03-05 12:39:30 +0900

    Auto commit: Wed Mar  5 12:39:30 KST 2025

--- src/default_package/RandomDemo.java
+++ src/default_package/RandomDemo.java
@@ -6,7 +6,7 @@ public class RandomDemo {
   public static void main(String[] args) {
     Random random = new Random();
     for (int i = 0; i < 5; i++) {
-
+      random.nextInt();
     }
     random.nextInt();
     random.nextInt();
