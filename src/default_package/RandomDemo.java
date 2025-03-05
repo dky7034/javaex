73550c1f4afb8ad6995d466617a4bcc9b486f55d
artp <dky7034@naver.com>
2025-03-05 12:42:42 +0900

    Auto commit: Wed Mar  5 12:42:42 KST 2025

--- src/default_package/RandomDemo.java
+++ src/default_package/RandomDemo.java
@@ -6,7 +6,7 @@ public class RandomDemo {
   public static void main(String[] args) {
     Random random = new Random();
     for (int i = 0; i < 5; i++) {
-      System.out.println(random.nextInt(3));
+      System.out.println(random.nextInt(3)); // 0부터 (3-1) 범위의 무작위 정수 생성
     }
   }
 }
