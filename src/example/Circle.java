080c6ff5697aee4f650621c2b810d4d4e4ec6101
artp <dky7034@naver.com>
2025-03-05 14:27:23 +0900

    Auto commit: Wed Mar  5 14:27:23 KST 2025

--- src/example/Circle.java
+++ src/example/Circle.java
@@ -5,21 +5,19 @@ import java.util.Objects;
 public class Circle {
   // 필드
   int radius;
-
   // 생성자
   public Circle(int radius) {
     this.radius = radius;
   }
-
   // 메서드
   @Override
   public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) {
+    if (o == null || getClass() != o.getClass()) return false;{
 
     }
 
   }
-}
+
 
   @Override
   public int hashCode() {
