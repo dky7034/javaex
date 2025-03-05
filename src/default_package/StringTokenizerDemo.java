300812be77da069b4bfe9bcd56360730347ab363
artp <dky7034@naver.com>
2025-03-05 14:00:05 +0900

    Auto commit: Wed Mar  5 14:00:05 KST 2025

--- src/default_package/StringTokenizerDemo.java
+++ src/default_package/StringTokenizerDemo.java
@@ -6,6 +6,6 @@ public class StringTokenizerDemo {
   public static void main(String[] args) {
     String s = "of the people, by the people, for the people";
 
-    StringTokenizer st = new StringTokenizer(,);
+    StringTokenizer st = new StringTokenizer();
   }
 }
