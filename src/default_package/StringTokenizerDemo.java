18a193ba271f9714d33745c5fe8503c96d3e68bb
artp <dky7034@naver.com>
2025-03-05 14:07:20 +0900

    Auto commit: Wed Mar  5 14:07:20 KST 2025

--- src/default_package/StringTokenizerDemo.java
+++ src/default_package/StringTokenizerDemo.java
@@ -6,7 +6,7 @@ public class StringTokenizerDemo {
   public static void main(String[] args) {
     String s = "of the people, by the people, for the people";
 
-    StringTokenizer st = new StringTokenizer(s, ", ");
+    StringTokenizer st = new StringTokenizer(s, ", ", true);
     System.out.println(st.countTokens());
     while (st.hasMoreTokens()) {
       System.out.println(st.nextToken());
