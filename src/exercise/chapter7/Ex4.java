2f41496ab1d9613486925e72123b3e8f522b606b
artp <dky7034@naver.com>
2025-03-05 10:10:45 +0900

    Auto commit: Wed Mar  5 10:10:45 KST 2025

--- src/exercise/chapter7/Ex4.java
+++ src/exercise/chapter7/Ex4.java
@@ -13,12 +13,17 @@ public class Ex4 {
 }
 
 class Korean implements Talkable {
-
-
+  @Override
+  public void talk() {
+    System.out.println("안녕하세요.");
+  }
 }
 
 class American implements Talkable {
+  @Override
+  public void talk() {
 
+  }
 }
 
 interface Talkable {
