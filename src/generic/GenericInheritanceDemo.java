910c5ae81f508d8893882c4a3883af78a2938908
artp <dky7034@naver.com>
2025-03-06 14:41:58 +0900

    Auto commit: Thu Mar  6 14:41:58 KST 2025

--- src/generic/GenericInheritanceDemo.java
+++ src/generic/GenericInheritanceDemo.java
@@ -1,4 +1,9 @@
 package generic;
 
+import java.util.ArrayList;
+
 public class GenericInheritanceDemo {
+  public static void main(String[] args) {
+    ArrayList<Beverage> list1 = new ArrayList<>();
+  }
 }
