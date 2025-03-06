ea1d678811662813769f3b70096a616916dcaefa
artp <dky7034@naver.com>
2025-03-06 09:18:44 +0900

    Auto commit: Thu Mar  6 09:18:44 KST 2025

--- src/exception_handle/ExceptionDemo2.java
+++ src/exception_handle/ExceptionDemo2.java
@@ -1,4 +1,9 @@
 package exception_handle;
 
+import java.io.FileInputStream;
+
 public class ExceptionDemo2 {
+  public static void main(String[] args) {
+    FileInputStream is = new FileInputStream("test.txt");
+  }
 }
