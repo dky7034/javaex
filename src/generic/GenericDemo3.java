0903d0c5ce7c3abe32476092257c6220da9e02d9
artp <dky7034@naver.com>
2025-03-06 11:19:02 +0900

    Auto commit: Thu Mar  6 11:19:02 KST 2025

--- src/generic/GenericDemo3.java
+++ src/generic/GenericDemo3.java
@@ -4,13 +4,11 @@ public class GenericDemo3 {
   public static void main(String[] args) {
     Cup<Boricha> borichaCup = new Cup<>();
     Boricha boricha = new Boricha();
-
     borichaCup.setBeverage(boricha);
     borichaCup.getBeverage().drinkBoricha();
 
     Cup<Beer> beerCup = new Cup<>();
     Beer beer = new Beer();
-
     beerCup.setBeverage(beer);
     beerCup.getBeverage().drinkBeer();
   }
