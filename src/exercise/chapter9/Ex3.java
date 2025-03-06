680be50b902effd699ceca6ef298d337655197a5
artp <dky7034@naver.com>
2025-03-06 12:33:06 +0900

    Auto commit: Thu Mar  6 12:33:06 KST 2025

--- src/exercise/chapter9/Ex3.java
+++ src/exercise/chapter9/Ex3.java
@@ -9,6 +9,7 @@ public class Ex3 {
   public static void main(String[] args) {
     // 숫자(정수)를 담을 수 있는 ArrayList 선언
     ArrayList<Integer> ali = new ArrayList<>();
+    a1.add(10);
 
     // 문자열을 담을 수 있는 ArrayList 선언
     ArrayList<String> als = new ArrayList<>();
