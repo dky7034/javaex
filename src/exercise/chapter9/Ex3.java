cd2a37403cdc71c330191ce0047402c318dfd953
artp <dky7034@naver.com>
2025-03-06 12:27:45 +0900

    Auto commit: Thu Mar  6 12:27:45 KST 2025

--- src/exercise/chapter9/Ex3.java
+++ src/exercise/chapter9/Ex3.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Ex3 {
   public static void main(String[] args) {
     // 숫자(정수)를 담을 수 있는 ArrayList 선언
-    ArrayList<Integer> al = new ArrayList<>();
+    ArrayList<Integer> ali = new ArrayList<>();
 
     // 문자열을 담을 수 있는 ArrayList 선언
-    ArrayList<String> sl = new ArrayList<>();
+    ArrayList<String> als = new ArrayList<>();
   }
 }
