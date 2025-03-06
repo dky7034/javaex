04d2f692966c01f137ad16156cd0df6cdd7042ee
artp <dky7034@naver.com>
2025-03-06 10:15:26 +0900

    Auto commit: Thu Mar  6 10:15:26 KST 2025

--- src/exception_handle/UncheckedDemo4.java
+++ src/exception_handle/UncheckedDemo4.java
@@ -9,4 +9,8 @@ public class UncheckedDemo4 {
     String input = in.nextLine();
     System.out.println(square(input));
   }
+
+  private static Object square(String input) {
+    return square(input);
+  }
 }
