49532fd7938f172bc0f345889a801defb7dfad6c
artp <dky7034@naver.com>
2025-03-06 12:26:56 +0900

    Auto commit: Thu Mar  6 12:26:56 KST 2025

--- src/exercise/chapter9/Ex3.java
+++ src/exercise/chapter9/Ex3.java
@@ -1,4 +1,10 @@
 package exercise.chapter9;
 
+import java.util.ArrayList;
+
 public class Ex3 {
+  public static void main(String[] args) {
+    // 숫자(정수)를 담을 수 있는 ArrayList 선언
+    ArrayList<Integer> al = new ArrayList<>();
+  }
 }
