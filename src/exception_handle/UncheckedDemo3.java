4ee6279d530dcb08ecdc64866bacbb3c8ce1eb46
artp <dky7034@naver.com>
2025-03-06 09:47:09 +0900

    Auto commit: Thu Mar  6 09:47:09 KST 2025

--- src/exception_handle/UncheckedDemo3.java
+++ src/exception_handle/UncheckedDemo3.java
@@ -7,6 +7,7 @@ public class UncheckedDemo3 {
       System.out.println(array[5]);
     } catch (ArrayIndexOutOfBoundsException e) {
       System.out.println(e.getMessage());
+      System.out.println("배열에 해당 원소가 존재하지 않습니다.");
     }
 
   }
