3413bfb7d9aa2ad74877bc9fe66d57430ee6bcdd
artp <dky7034@naver.com>
2025-03-06 10:22:37 +0900

    Auto commit: Thu Mar  6 10:22:37 KST 2025

--- src/exception_handle/UncheckedDemo4.java
+++ src/exception_handle/UncheckedDemo4.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class UncheckedDemo4 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-
     System.out.print("숫자를 입력하세요: ");
     String input = in.nextLine();
     try {
