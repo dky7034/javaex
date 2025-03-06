0a99729ac06ed9b92551fbc4618c0a58dd3c3aa4
artp <dky7034@naver.com>
2025-03-06 11:45:39 +0900

    Auto commit: Thu Mar  6 11:45:39 KST 2025

--- src/exercise/chapter9/Ex1.java
+++ src/exercise/chapter9/Ex1.java
@@ -4,6 +4,7 @@ public class Ex1 {
   public static void main(String[] args) {
     Box<Integer> i = new Box<>();
     i.setItem(Integer.valueOf(100));
+    System.out.println(i.getItem());
   }
 }
 
