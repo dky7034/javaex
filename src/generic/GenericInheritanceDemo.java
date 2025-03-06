119c30331a7b59ddf450a5edd5248dbd43132b40
artp <dky7034@naver.com>
2025-03-06 14:49:06 +0900

    Auto commit: Thu Mar  6 14:49:06 KST 2025

--- src/generic/GenericInheritanceDemo.java
+++ src/generic/GenericInheritanceDemo.java
@@ -20,6 +20,10 @@ public class GenericInheritanceDemo {
 //    list2.add(new Boricha());   // 강한 타입 체크 (컴파일 에러 / 타입 안정성)
 //    list2.add(new Beverage());  // 강한 타입 체크 (컴파일 에러 / 타입 안정성)
     list2.add(new Beer());
+
+  }
+  static void beverageTest() {
     
   }
+
 }
