5ed17d72dfcb9e4611dbd0077edbccf0a9908f29
artp <dky7034@naver.com>
2025-03-06 15:26:44 +0900

    Auto commit: Thu Mar  6 15:26:44 KST 2025

--- src/generic/GenericMethodDemo.java
+++ src/generic/GenericMethodDemo.java
@@ -3,7 +3,11 @@ package generic;
 public class GenericMethodDemo {
   static class Utils {
     // 제너릭 메서드 - <T> 반환타입 메서드명() {}
-    public static <T> void method(T[] a) {}
+    public static <T> void method(T[] a) {
+      for (T t : a) {
+        
+      }
+    }
   }
 
 
