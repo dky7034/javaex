ef2d16a6c3943c3962642ed0b026cb0c7c9533de
artp <dky7034@naver.com>
2025-03-06 09:22:23 +0900

    Auto commit: Thu Mar  6 09:22:22 KST 2025

--- src/exception_handle/ExceptionDemo2.java
+++ src/exception_handle/ExceptionDemo2.java
@@ -2,7 +2,6 @@ package exception_handle;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class ExceptionDemo2 {
   public static void main(String[] args) {
@@ -10,13 +9,10 @@ public class ExceptionDemo2 {
     try {
       is = new FileInputStream("test.txt");
     } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
+      System.out.println(e.getMessage());
     } finally {
-      try {
-        is.close();
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
+      is.close();
+
     }
   }
 }
