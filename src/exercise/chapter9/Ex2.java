f081fcd836abca932048fc6dca1812ade205509c
artp <dky7034@naver.com>
2025-03-06 12:12:21 +0900

    Auto commit: Thu Mar  6 12:12:21 KST 2025

--- src/exercise/chapter9/Ex2.java
+++ src/exercise/chapter9/Ex2.java
@@ -5,7 +5,7 @@ public class Ex2 {
     // Person (이름과 나이)
     Person<String, Integer> p1 = new Person<>("홍길동", 20);
     // Person (주민번호, 이름)
-    Person<String, String> p2 = new Person<>();
+    Person<String, String> p2 = new Person<>("111111", "손흥민");
 
     System.out.println(p1.getK());
     System.out.println(p1.getV());
