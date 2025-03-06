fdaae11fdc27537955f89b6541548e2d89262ef1
artp <dky7034@naver.com>
2025-03-06 11:18:38 +0900

    Auto commit: Thu Mar  6 11:18:38 KST 2025

--- src/generic/GenericDemo3.java
+++ src/generic/GenericDemo3.java
@@ -8,9 +8,10 @@ public class GenericDemo3 {
     borichaCup.setBeverage(boricha);
     borichaCup.getBeverage().drinkBoricha();
 
+    Cup<Beer> beerCup = new Cup<>();
     Beer beer = new Beer();
 //    c.setBeverage(beer); // Boricha 가 아닌 객체는 담을 수 없습니다.
-    Cup<Beer> beerCup = new Cup<>();
+
     beerCup.setBeverage(beer);
   }
 }
