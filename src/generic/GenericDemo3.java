7f195043428bccd622385d9c7db5d4a7093e2f05
artp <dky7034@naver.com>
2025-03-06 11:17:28 +0900

    Auto commit: Thu Mar  6 11:17:28 KST 2025

--- src/generic/GenericDemo3.java
+++ src/generic/GenericDemo3.java
@@ -2,13 +2,15 @@ package generic;
 
 public class GenericDemo3 {
   public static void main(String[] args) {
-    Cup<Boricha> c = new Cup<>();
+    Cup<Boricha> borichaCup = new Cup<>();
     Boricha b = new Boricha();
 
-    c.setBeverage(b);
-    Boricha boricha = c.getBeverage();
+    borichaCup.setBeverage(b);
+    Boricha boricha = borichaCup.getBeverage();
 
     Beer beer = new Beer();
 //    c.setBeverage(beer); // Boricha 가 아닌 객체는 담을 수 없습니다.
+
+    Cup<Beer> beerCup = new Cup<>();
   }
 }
