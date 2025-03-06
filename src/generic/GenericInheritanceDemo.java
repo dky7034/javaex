cd7f869c62395f7a7dabfa5c45d9c5eab39cc888
artp <dky7034@naver.com>
2025-03-06 14:42:39 +0900

    Auto commit: Thu Mar  6 14:42:39 KST 2025

--- src/generic/GenericInheritanceDemo.java
+++ src/generic/GenericInheritanceDemo.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 public class GenericInheritanceDemo {
   public static void main(String[] args) {
     ArrayList<Beverage> list1 = new ArrayList<>();
-    list1.add(new Beverage());
-    list1.add(new Beer());
-    list1.add(new Boricha());
+    list1.add(new Beverage()); // 인덱스 0
+    list1.add(new Beer());    // 인덱스 1
+    list1.add(new Boricha()); // 인덱스 2
+
+    li
   }
 }
