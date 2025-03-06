66a4e8d607283e3fab851253c3df5634fbb0058f
artp <dky7034@naver.com>
2025-03-06 12:35:11 +0900

    Auto commit: Thu Mar  6 12:35:10 KST 2025

--- src/exercise/chapter9/Ex3.java
+++ src/exercise/chapter9/Ex3.java
@@ -19,6 +19,7 @@ public class Ex3 {
 
     // 숫자(정수)를 담을 수 있는 Stack 선언
     Stack<Integer> sti = new Stack<>();
+    sti.add("1");
 
     // 문자열을 담을 수 있는 Stack 선언
     Stack<String> sts = new Stack<>();
