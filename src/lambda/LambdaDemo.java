89db660dff8c60796a1e6b21295017e14f905529
artp <dky7034@naver.com>
2025-03-06 16:45:27 +0900

    Auto commit: Thu Mar  6 16:45:27 KST 2025

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -11,10 +11,11 @@ public class LambdaDemo {
 }
 
 // (a, b) -> a > b ? a : b;
-class AnyObject {
-  class Anonymous {
-    int max(int a, int b) {
-      return a > b ? a : b;
-    }
-  }
-}
+//class AnyObject {
+//  class Anonymous {
+//    int max(int a, int b) {
+//      return a > b ? a : b;
+//    }
+//  }
+//}
+
