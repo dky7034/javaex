a20dcf10fb9f1911833c43c6c9e6f3ae458d90a3
artp <dky7034@naver.com>
2025-03-06 11:24:37 +0900

    Auto commit: Thu Mar  6 11:24:37 KST 2025

--- src/generic/GenericDemo4.java
+++ src/generic/GenericDemo4.java
@@ -5,5 +5,9 @@ import java.util.ArrayList;
 public class GenericDemo4 {
   public static void main(String[] args) {
     ArrayList arrayList = new ArrayList();
+    arrayList.add(10);
+    arrayList.add("str");
+    arrayList.add('c');
+
   }
 }
