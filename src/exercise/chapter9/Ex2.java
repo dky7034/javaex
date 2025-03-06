445768736ea6c7fa2587a9a37eed3323d060e72c
artp <dky7034@naver.com>
2025-03-06 12:11:28 +0900

    Auto commit: Thu Mar  6 12:11:28 KST 2025

--- src/exercise/chapter9/Ex2.java
+++ src/exercise/chapter9/Ex2.java
@@ -3,6 +3,7 @@ package exercise.chapter9;
 public class Ex2 {
   public static void main(String[] args) {
     // Person (이름과 나이)
+    Person<String, Integer> p1 = new Person<>();
     // Person (주민번호, 이름)
 
   }
