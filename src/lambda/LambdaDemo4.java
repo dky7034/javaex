802356f734731135dfadb873c21a34b2b7845dfe
artp <dky7034@naver.com>
2025-03-06 17:22:41 +0900

    Auto commit: Thu Mar  6 17:22:41 KST 2025

--- src/lambda/LambdaDemo4.java
+++ src/lambda/LambdaDemo4.java
@@ -3,7 +3,7 @@ package lambda;
 public class LambdaDemo4 {
   public static void main(String[] args) {
     DiceFunction d1 = () -> {
-      return Math.random() * 6 + 1;
+      return (int)Math.random() * 6 + 1;
     };
   }
 }
