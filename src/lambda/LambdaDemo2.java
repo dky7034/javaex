bc98f4011f79943b2ab9224abac653d3bfa30e9b
artp <dky7034@naver.com>
2025-03-06 17:15:39 +0900

    Auto commit: Thu Mar  6 17:15:39 KST 2025

--- src/lambda/LambdaDemo2.java
+++ src/lambda/LambdaDemo2.java
@@ -2,13 +2,15 @@ package lambda;
 
 public class LambdaDemo2 {
   public static void main(String[] args) {
-    PrintFunction printFunction = new PrintFunction() {
+    PrintFunction p = new PrintFunction() {
       @Override
       public void printVar(String name, int i) {
         System.out.println(name + "=" + i);
       }
     };
-    printFunction.printVar("홍길동", 3);
+    p.printVar("홍길동", 3);
+
+    PrintFunction p2 = (name, i) -> System.out.println(name + "=" + i);
   }
 }
 
