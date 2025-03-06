52baa8a131fc973359a999b0a839a44adeb92557
artp <dky7034@naver.com>
2025-03-06 12:11:05 +0900

    Auto commit: Thu Mar  6 12:11:05 KST 2025

--- src/exercise/chapter9/Ex2.java
+++ src/exercise/chapter9/Ex2.java
@@ -12,8 +12,10 @@ class Person<K, V> {
   private K k;
   private V v;
 
-  public Person<K, V> () {}
-
+  public Person(K k, V v) {
+    this.k = k;
+    this.v = v;
+  }
 
   public K getK() {
     return k;
