6e2c5784a0ee8d3042e7d33d869f062275309654
artp <dky7034@naver.com>
2025-03-06 09:19:19 +0900

    Auto commit: Thu Mar  6 09:19:19 KST 2025

--- src/exception_handle/ExceptionDemo2.java
+++ src/exception_handle/ExceptionDemo2.java
@@ -1,9 +1,14 @@
 package exception_handle;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class ExceptionDemo2 {
   public static void main(String[] args) {
-    FileInputStream is = new FileInputStream("test.txt");
+    try {
+      FileInputStream is = new FileInputStream("test.txt");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
