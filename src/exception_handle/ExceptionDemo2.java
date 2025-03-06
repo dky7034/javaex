1c1d63ecb1b23d5abc1eae6a979079b813b63e99
artp <dky7034@naver.com>
2025-03-06 09:26:29 +0900

    Auto commit: Thu Mar  6 09:26:29 KST 2025

--- src/exception_handle/ExceptionDemo2.java
+++ src/exception_handle/ExceptionDemo2.java
@@ -1,11 +1,10 @@
 package exception_handle;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ExceptionDemo2 {
   public static void main(String[] args) throws IOException, NullPointerException {
+    // 
 //    FileInputStream is = null;
 //    try {
 //      is = new FileInputStream("test.txt");
