fe561abf3080bfdc07058127c647c24038376031
artp <dky7034@naver.com>
2025-03-06 17:12:43 +0900

    Auto commit: Thu Mar  6 17:12:43 KST 2025

--- src/lambda/LambdaDemo2.java
+++ src/lambda/LambdaDemo2.java
@@ -2,7 +2,12 @@ package lambda;
 
 public class LambdaDemo2 {
   public static void main(String[] args) {
-    
+    PrintFunction printFunction = new PrintFunction() {
+      @Override
+      public void printVar(String name, int i) {
+        
+      }
+    };
   }
 }
 
