e6ca3fc68a4f0d9a067b213aa262f60afc2c5fb2
artp <dky7034@naver.com>
2025-03-06 15:40:47 +0900

    Auto commit: Thu Mar  6 15:40:47 KST 2025

--- src/generic/GenericMethodDemo2.java
+++ src/generic/GenericMethodDemo2.java
@@ -4,6 +4,9 @@ public class GenericMethodDemo2 {
   public static void main(String[] args) {
     Ticket[] tickets = new Ticket[3];
     // Ticket: 가격 10_000, 20_000, 15_000
+    tickets[0] = new Ticket(10000);
+    tickets[1] = new Ticket(20000);
+    tickets[2] = new Ticket(30000);
   }
 
   // 제네릭 메서드
