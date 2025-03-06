023ef5b38b794d6c2665493b5e25910a2de583e2
artp <dky7034@naver.com>
2025-03-06 15:29:02 +0900

    Auto commit: Thu Mar  6 15:29:02 KST 2025

--- src/generic/GenericMethodDemo.java
+++ src/generic/GenericMethodDemo.java
@@ -15,5 +15,7 @@ public class GenericMethodDemo {
     Integer[] a = {1, 2, 3, 4, 5};
     Double[] d = {1.0, 2.0, 3.0, 4.0, 5.0};
     Character[] c = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+    Utils.method(a);
   }
 }
