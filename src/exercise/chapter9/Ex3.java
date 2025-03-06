62dc100171ec290f413ecdb7c12d94fae2f7e358
artp <dky7034@naver.com>
2025-03-06 12:36:49 +0900

    Auto commit: Thu Mar  6 12:36:49 KST 2025

--- src/exercise/chapter9/Ex3.java
+++ src/exercise/chapter9/Ex3.java
@@ -24,6 +24,8 @@ public class Ex3 {
 
     // 문자열을 담을 수 있는 Stack 선언
     Stack<String> sts = new Stack<>();
+    sts.add("str");
+    System.out.println(sts.pop());
 
     // 숫자(정수)를 담을 수 있는 Queue 선언
     Queue<Integer> qi = new LinkedList<>();
