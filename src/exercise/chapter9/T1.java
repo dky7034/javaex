0f598ee5e00a10638a4165018610c6aa5ba8fca2
artp <dky7034@naver.com>
2025-03-06 14:18:48 +0900

    Auto commit: Thu Mar  6 14:18:48 KST 2025

--- src/exercise/chapter9/T1.java
+++ src/exercise/chapter9/T1.java
@@ -2,7 +2,13 @@ package exercise.chapter9;
 
 public class T1 {
   public static void main(String[] args) {
+    Container<String> container1 = new Container<>();
+    container1.setT("홍길동");
+    String str = container1.getT();
 
+    Container<Integer> container2 = new Container<>();
+    container2.setT(6);
+    int value = container2.getT();
   }
 }
 
