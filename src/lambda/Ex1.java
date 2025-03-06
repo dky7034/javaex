2f3270e83f2e1c7bfc03e3bcfb479e43187b0279
artp <dky7034@naver.com>
2025-03-06 16:30:16 +0900

    Auto commit: Thu Mar  6 16:30:16 KST 2025

--- src/lambda/Ex1.java
+++ src/lambda/Ex1.java
@@ -1,6 +1,9 @@
 package lambda;
 
 public class Ex1 {
-
+  public static void main(String[] args) {
+    (a, b) -> a > b ? a : b
+  }
 }
-(a, b) -> a > b ? a : b
+
+    (String name, int i) -> System.out.println("name")
