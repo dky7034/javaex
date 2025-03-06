75fbe3d60c94efaf81d85684e8ed1b0de129d7f4
artp <dky7034@naver.com>
2025-03-06 09:09:46 +0900

    Auto commit: Thu Mar  6 09:09:46 KST 2025

--- src/exception_handle/UncheckedDemo.java
+++ src/exception_handle/UncheckedDemo.java
@@ -2,10 +2,14 @@ package exception_handle;
 
 public class UncheckedDemo {
   public static void main(String[] args) {
-    try {
-      int i = 1 / 0;
-    } catch (ArithmeticException e) {
-      System.out.println(e.getMessage());
+//    try {
+//      int i = 1 / 0;
+//    } catch (ArithmeticException e) {
+//      System.out.println(e.getMessage());
+//    }
+    int number = 0;
+    if (number == 0) {
+      System.out.println("0으로 나눌 수 없습니다.");
     }
   }
 
