c473604815f3a80495acd567c13a5d87a27989f4
artp <dky7034@naver.com>
2025-03-06 09:22:52 +0900

    Auto commit: Thu Mar  6 09:22:52 KST 2025

--- src/exception_handle/ExceptionDemo2.java
+++ src/exception_handle/ExceptionDemo2.java
@@ -2,9 +2,10 @@ package exception_handle;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ExceptionDemo2 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     FileInputStream is = null;
     try {
       is = new FileInputStream("test.txt");
