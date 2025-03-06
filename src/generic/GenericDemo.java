4372e2401d86d6a96295eebfbb50721d70cca184
artp <dky7034@naver.com>
2025-03-06 10:30:47 +0900

    Auto commit: Thu Mar  6 10:30:47 KST 2025

--- src/generic/GenericDemo.java
+++ src/generic/GenericDemo.java
@@ -5,5 +5,8 @@ import java.util.ArrayList;
 public class GenericDemo {
   public static void main(String[] args) {
     ArrayList<Integer> il = new ArrayList<>();
+    il.add(10);
+    il.add(20);
+    il.add(30);
   }
 }
