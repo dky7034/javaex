5e78f03f7a9c979c3e06fba1934aefebef88bd55
artp <dky7034@naver.com>
2025-03-06 15:34:49 +0900

    Auto commit: Thu Mar  6 15:34:49 KST 2025

--- src/generic/GenericMethodDemo2.java
+++ src/generic/GenericMethodDemo2.java
@@ -3,6 +3,6 @@ package generic;
 public class GenericMethodDemo2 {
   public static void main(String[] args) {
     Ticket[] tickets = new Ticket[3];
-    // Ticket: 가격
+    // Ticket: 가격 10_000, 20_000, 15_000
   }
 }
