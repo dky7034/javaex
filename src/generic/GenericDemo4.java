480eac821c0c2c323a129138ce3934f9dbd91a65
artp <dky7034@naver.com>
2025-03-06 11:29:47 +0900

    Auto commit: Thu Mar  6 11:29:47 KST 2025

--- src/generic/GenericDemo4.java
+++ src/generic/GenericDemo4.java
@@ -6,7 +6,7 @@ public class GenericDemo4 {
   public static void main(String[] args) {
     ArrayList<Integer> arrayList = new ArrayList();
     arrayList.add(10);
-//    arrayList.add("str");
+//    arrayList.add("str"); // 타입 안전성 보장
 //    arrayList.add('c');
 
     int i = (Integer) arrayList.get(1);
