4dd3baf25752dd17ff303f4b50a78d1fbf5003f2
artp <dky7034@naver.com>
2025-03-06 11:07:44 +0900

    Auto commit: Thu Mar  6 11:07:44 KST 2025

--- src/generic/GenericDemo2.java
+++ src/generic/GenericDemo2.java
@@ -13,7 +13,7 @@ public class GenericDemo2 {
 
     Boricha boricha = new Boricha();
     c.setBeverage(boricha);
-    Object o = (Beer) c.getBeverage(); // 다운캐스팅은 컴파일이 걸러주지 않는 실행 오류
+    Object o = c.getBeverage(); // 다운캐스팅은 컴파일이 걸러주지 않는 실행 오류
     beer1.drinkBeer();                   // 컴파일할 때 걸러질 수 있는 오류로 만들기 위해 제네릭 클래스를 사용
 
     Beverage beverage = new Beverage();
