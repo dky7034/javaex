935b38b423f11aa973259b1dc11126ad51130f16
artp <dky7034@naver.com>
2025-03-06 17:17:51 +0900

    Auto commit: Thu Mar  6 17:17:51 KST 2025

--- src/lambda/LambdaDemo3.java
+++ src/lambda/LambdaDemo3.java
@@ -2,6 +2,12 @@ package lambda;
 
 public class LambdaDemo3 {
   public static void main(String[] args) {
-    
+
   }
+}
+
+// 함수형 인터페이스
+@FunctionalInterface
+interface SquareFunction {
+  int square(int x);
 }
