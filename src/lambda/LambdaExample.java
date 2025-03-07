ed0811f948e602838c21a9268d7a337d43c0ce75
artp <dky7034@naver.com>
2025-03-07 09:22:15 +0900

    Auto commit: Fri Mar  7 09:22:15 KST 2025

--- src/lambda/LambdaExample.java
+++ src/lambda/LambdaExample.java
@@ -1,8 +1,13 @@
 package lambda;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class LambdaExample {
   public static void main(String[] args) {
-
+    List<String> list = Arrays.asList("abc", "aaa", "bbb", "ddd", "aaa");
+    Collections.sort(list);
   }
 }
 
