8854b6f588900b525962b93a45f492b62a58fb43
artp <dky7034@naver.com>
2025-03-07 09:29:51 +0900

    Auto commit: Fri Mar  7 09:29:51 KST 2025

--- src/lambda/LambdaExample.java
+++ src/lambda/LambdaExample.java
@@ -8,14 +8,15 @@ public class LambdaExample {
   public static void main(String[] args) {
     List<String> list = Arrays.asList("abc", "aaa", "bbb", "ddd", "aaa");
 
-    Comparator<String> c = new Comparator<String>() {
-      @Override
-      public int compare(String o1, String o2) {
-        return o1.compareTo(o2); // o1 > o2: 양수, o1 < o2: 음수, o1 == o2: 0
-//        return o2.compareTo(o1); // o1 > o2: 음수, o1 < o2: 양수, o1 == o2: 0
-      }
-    };
-    (o1, o2) -> o1.compareTo(o2);
+//    Comparator<String> c = new Comparator<String>() {
+//      @Override
+//      public int compare(String o1, String o2) {
+//        return o1.compareTo(o2); // o1 > o2: 양수, o1 < o2: 음수, o1 == o2: 0
+////        return o2.compareTo(o1); // o1 > o2: 음수, o1 < o2: 양수, o1 == o2: 0
+//      }
+//    };
+
+    Comparator<String> c = (o1, o2) -> o1.compareTo(o2);
     Collections.sort(list, c);
   }
 }
