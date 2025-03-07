fd85b5053df567f94ca328743af8f0723edcd749
artp <dky7034@naver.com>
2025-03-07 17:14:42 +0900

    Auto commit: Fri Mar  7 17:14:42 KST 2025

--- src/exercise/chapter11/Ex1.java
+++ src/exercise/chapter11/Ex1.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Ex1 {
   public static void main(String[] args) {
+    // 부모 참조 타입 변수로 객체를 생성해야 나중에 유지보수에 유리함
     List<String> al = new LinkedList<>();
 //        new ArrayList<>();
     al.add("갈매기");
