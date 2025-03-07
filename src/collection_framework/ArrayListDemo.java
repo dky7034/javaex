9d5c3293c7112d0ccec25517d355f90f9109b3b7
artp <dky7034@naver.com>
2025-03-07 15:15:44 +0900

    Auto commit: Fri Mar  7 15:15:44 KST 2025

--- src/collection_framework/ArrayListDemo.java
+++ src/collection_framework/ArrayListDemo.java
@@ -1,7 +1,10 @@
 package collection_framework;
 
+import java.util.List;
+
 public class ArrayListDemo {
   public static void main(String[] args) {
+    List<String> list = List.of("그랜저", "소나타", "아반떼", "제네시스", "소울");
     
   }
 }
