c4299ec2dea9238dcab90e5d6d8ec5840c5c8b0e
artp <dky7034@naver.com>
2025-03-07 09:39:13 +0900

    Auto commit: Fri Mar  7 09:39:13 KST 2025

--- src/exercise/chapter7/Ex3_3.java
+++ src/exercise/chapter7/Ex3_3.java
@@ -44,6 +44,10 @@ public class Ex3_3 {
         return o2.compareTo(o1);
       }
     });
+
+    Arrays.sort(o1, o2);
+
+
     System.out.println("내림차순 정렬");
     System.out.println(Arrays.toString(books));
   }
