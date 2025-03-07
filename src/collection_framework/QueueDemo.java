16d2479ed02faa08821bae7513a1ba30fa303e2d
artp <dky7034@naver.com>
2025-03-07 16:26:15 +0900

    Auto commit: Fri Mar  7 16:26:15 KST 2025

--- src/collection_framework/QueueDemo.java
+++ src/collection_framework/QueueDemo.java
@@ -1,4 +1,10 @@
 package collection_framework;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueDemo {
+  public static void main(String[] args) {
+    Queue<String> q = new LinkedList<>();
+  }
 }
