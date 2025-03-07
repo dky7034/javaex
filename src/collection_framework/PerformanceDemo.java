1394d78d41b7102b3fd33bbabde568695cd9e1d6
artp <dky7034@naver.com>
2025-03-07 16:15:30 +0900

    Auto commit: Fri Mar  7 16:15:30 KST 2025

--- src/collection_framework/PerformanceDemo.java
+++ src/collection_framework/PerformanceDemo.java
@@ -16,12 +16,30 @@ public class PerformanceDemo {
     long endTime = System.currentTimeMillis();
     System.out.println("배열의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
 
-    // 시간 체크
+    // 시간 체크 (nanoTime)
     startTime = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
       ll.addLast(i);
     }
     endTime = System.currentTimeMillis();
     System.out.println("연결 리스트의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
+
+    // 시간 체크 (nanoTime)
+    startTime = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      al.addLast(i);
+    }
+    endTime = System.currentTimeMillis();
+    System.out.println("배열의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
+
+    // 시간 체크 (nanoTime)
+    startTime = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      ll.addLast(i);
+    }
+    endTime = System.currentTimeMillis();
+    System.out.println("연결 리스트의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
+
+
   }
 }
