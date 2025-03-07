71434d067aa998dd292d093ff7421dee6f9c60f4
artp <dky7034@naver.com>
2025-03-07 09:24:46 +0900

    Auto commit: Fri Mar  7 09:24:46 KST 2025

--- src/lambda/LambdaExample.java
+++ src/lambda/LambdaExample.java
@@ -7,13 +7,14 @@ import java.util.List;
 public class LambdaExample {
   public static void main(String[] args) {
     List<String> list = Arrays.asList("abc", "aaa", "bbb", "ddd", "aaa");
-    Collections.sort(list);
+
     Comparator c = new Comparator() {
       @Override
       public int compare(Object o1, Object o2) {
         return 0;
       }
     };
+    Collections.sort(list);
   }
 }
 
