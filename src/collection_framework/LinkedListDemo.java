a948207a2af85ad03ffae94cefb6de353955fea3
artp <dky7034@naver.com>
2025-03-07 16:01:13 +0900

    Auto commit: Fri Mar  7 16:01:13 KST 2025

--- src/collection_framework/LinkedListDemo.java
+++ src/collection_framework/LinkedListDemo.java
@@ -1,7 +1,10 @@
 package collection_framework;
 
+import java.util.LinkedList;
+
 public class LinkedListDemo {
   public static void main(String[] args) {
+    LinkedList<Integer> list = new LinkedList<>();
     
   }
 }
