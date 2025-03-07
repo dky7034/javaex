ef972f0721414c8c441b957ee7170122b5fbbeac
artp <dky7034@naver.com>
2025-03-07 15:38:08 +0900

    Auto commit: Fri Mar  7 15:38:08 KST 2025

--- src/collection_framework/StackDemo.java
+++ src/collection_framework/StackDemo.java
@@ -6,9 +6,9 @@ import java.util.Stack;
 public class StackDemo {
   public static void main(String[] args) {
     Stack<String> stack = new Stack<>();
-//    stack.push("10");
-//    stack.push("20");
-//    stack.push("30");
+    stack.push("10");
+    stack.push("20");
+    stack.push("30");
 
 //    if (!stack.isEmpty()) {
 //      System.out.println(stack.peek());
