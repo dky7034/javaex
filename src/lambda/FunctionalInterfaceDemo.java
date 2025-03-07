f872f57b5ba0596233c8178a7bb5b897430501ba
artp <dky7034@naver.com>
2025-03-07 10:47:33 +0900

    Auto commit: Fri Mar  7 10:47:33 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -29,6 +29,6 @@ public class FunctionalInterfaceDemo {
     f = Integer::parseInt; // static method 참조
 
     String s1 = "hello", s2 = "world";
-    BiFunction<String, String, Boolean> f = (s1, s2) -> s1.equals(s2);
+    BiFunction<String, String, Boolean> f1 = (s1, s2) -> s1.equals(s2);
   }
 }
