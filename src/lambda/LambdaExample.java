0043c61b44ce88e606f9054254fca4415810cc97
artp <dky7034@naver.com>
2025-03-07 09:29:01 +0900

    Auto commit: Fri Mar  7 09:29:01 KST 2025

--- src/lambda/LambdaExample.java
+++ src/lambda/LambdaExample.java
@@ -15,6 +15,7 @@ public class LambdaExample {
 //        return o2.compareTo(o1); // o1 > o2: 음수, o1 < o2: 양수, o1 == o2: 0
       }
     };
+    (o1, o2) -> o1.compareTo(o2);
     Collections.sort(list, c);
   }
 }
