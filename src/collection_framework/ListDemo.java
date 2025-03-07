bd9c01c1f221da39b0ee005a5c7aec0aff887b0a
artp <dky7034@naver.com>
2025-03-07 15:01:32 +0900

    Auto commit: Fri Mar  7 15:01:32 KST 2025

--- src/collection_framework/ListDemo.java
+++ src/collection_framework/ListDemo.java
@@ -7,7 +7,5 @@ public class ListDemo {
   public static void main(String[] args) {
     String[] animals = {"a", "b", "c", "d"};
     List<String> list = Arrays.asList(animals);
-    list.
-    System.out.println(list);
   }
 }
