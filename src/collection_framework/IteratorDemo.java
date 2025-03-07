6ae3057173cbc4c7a2779662b152f9f6df94927f
artp <dky7034@naver.com>
2025-03-07 12:10:05 +0900

    Auto commit: Fri Mar  7 12:10:05 KST 2025

--- src/collection_framework/IteratorDemo.java
+++ src/collection_framework/IteratorDemo.java
@@ -1,4 +1,9 @@
 package collection_framework;
 
+import java.util.Collection;
+
 public class IteratorDemo {
+  public static void main(String[] args) {
+    Collection<String> collection = new
+  }
 }
