d316a647116862d8da09e141f5783694c6b9f440
artp <dky7034@naver.com>
2025-03-07 15:16:51 +0900

    Auto commit: Fri Mar  7 15:16:51 KST 2025

--- src/collection_framework/ArrayListDemo.java
+++ src/collection_framework/ArrayListDemo.java
@@ -1,12 +1,13 @@
 package collection_framework;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListDemo {
   public static void main(String[] args) {
     List<String> list = List.of("그랜저", "소나타", "아반떼", "제네시스", "소울");
 //    list.add("k3"); // List.of로 만들어진 객체는 Array를 기반으로 만들어졌으므로 크기가 고정되어있어 요소를 추가할 수 없습니다.
-    
+    List<String> list2 = new ArrayList<>();
 
   }
 }
