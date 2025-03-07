985bc6c75fdae592f54f40bd850eecae7c49f05d
artp <dky7034@naver.com>
2025-03-07 10:20:04 +0900

    Auto commit: Fri Mar  7 10:20:04 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -1,7 +1,10 @@
 package lambda;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
-    
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
   }
 }
