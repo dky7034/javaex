86875e1f4e223b7a78df1c6c98032e28c991542e
artp <dky7034@naver.com>
2025-03-07 16:19:35 +0900

    Auto commit: Fri Mar  7 16:19:35 KST 2025

--- src/collection_framework/PerformanceDemo.java
+++ src/collection_framework/PerformanceDemo.java
@@ -14,16 +14,16 @@ public class PerformanceDemo {
     for (int i = 0; i < 100_000; i++) {
       al.add(i);
     }
-    long endTime = System.currentTimeMillis();
-    System.out.println("배열의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
+    long endTime = System.nanoTime();
+    System.out.println("배열의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime) / 1_000_000 + "ms");
 
     // 시간 체크 (nanoTime)
     startTime = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
       ll.addLast(i);
     }
-    endTime = System.currentTimeMillis();
-    System.out.println("연결 리스트의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
+    endTime = System.nanoTime();
+    System.out.println("연결 리스트의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime) / 1_000_000 + "ms");
 
     // 맨 앞에 원소를 추가
     // 시간 체크 (nanoTime)
@@ -31,16 +31,15 @@ public class PerformanceDemo {
     for (int i = 0; i < 100_000; i++) {
       al.add(0, i);
     }
-    endTime = System.currentTimeMillis();
-    System.out.println("배열의 맨 앞에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
+    endTime = System.nanoTime();
+    System.out.println("배열의 맨 앞에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime) / 1_000_000 + "ms");
 
     // 시간 체크 (nanoTime)
     startTime = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
       ll.addFirst(i);
     }
-    endTime = System.currentTimeMillis();
-    System.out.println("연결 리스트의 맨 앞에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
-
+    endTime = System.nanoTime();
+    System.out.println("연결 리스트의 맨 앞에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime) / 1_000_000 + "ms");
   }
 }
