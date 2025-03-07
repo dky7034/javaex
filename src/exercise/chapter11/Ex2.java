4f7ef244ce75a51fe343dab85d58331d0eaa53c8
artp <dky7034@naver.com>
2025-03-07 22:50:45 +0900

    Auto commit: Fri Mar  7 22:50:45 KST 2025

--- src/exercise/chapter11/Ex2.java
+++ src/exercise/chapter11/Ex2.java
@@ -42,13 +42,14 @@ class Person {
   // equals 오버라이딩
   @Override
   public boolean equals(Object obj) {
-    // obj 가 현재 객체와 같은 타입인지 판별하는 조건문
-    // null 체크 필수
+    // obj 가 현재 객체와 같은 타입인지 판별하는 조건문 + null 체크
     if (obj == null || getClass() != obj.getClass()) {
       return false;
     }
+    // obj 가 null 도 아니고 Person 과 같은 타입이라면,
+    // 다운캐스팅
     Person p = (Person) obj;
-    return (this.name.equals(p.name)); // 이름이 같으면 true 리턴, 다르면 false 리턴
+//    return (this.name.equals(p.name)); // 이름이 같으면 true 리턴, 다르면 false 리턴
 
 
   }
