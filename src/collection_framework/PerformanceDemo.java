d36cb12e5162c01f32c62dfb317291811f344153
artp <dky7034@naver.com>
2025-03-07 16:11:32 +0900

    Auto commit: Fri Mar  7 16:11:32 KST 2025

--- src/collection_framework/PerformanceDemo.java
+++ src/collection_framework/PerformanceDemo.java
@@ -8,7 +8,7 @@ public class PerformanceDemo {
     ArrayList<Integer> al = new ArrayList<>();
     LinkedList<Integer> ll = new LinkedList<>();
 
-    // 시간 체크
+    // 시간 체크 (currentTimeMillis)
     long startTime = System.currentTimeMillis();
     for (int i = 0; i < 100_000; i++) {
       al.add(i);
