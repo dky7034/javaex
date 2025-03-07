26f85867d665906116a320df63b803ed7bd0a3b4
artp <dky7034@naver.com>
2025-03-07 11:09:36 +0900

    Auto commit: Fri Mar  7 11:09:36 KST 2025

--- src/lambda/PredicateDemo.java
+++ src/lambda/PredicateDemo.java
@@ -4,16 +4,16 @@ import java.util.function.Predicate;
 
 public class PredicateDemo {
   public static void main(String[] args) {
-    Predicate<String> isEmpty = new Predicate<String>() {
-      @Override
-      public boolean test(String s) {
-        return s.length() == 0;
-      }
-    };
-    String s = "";
-    if (s != null && isEmpty.test(s)) {
-      System.out.println("빈 문자열입니다.");
-    }
+//    Predicate<String> isEmpty = new Predicate<String>() {
+//      @Override
+//      public boolean test(String s) {
+//        return s.length() == 0;
+//      }
+//    };
+//    String s = "";
+//    if (s != null && isEmpty.test(s)) {
+//      System.out.println("빈 문자열입니다.");
+//    }
 
     // 함수형 인터페이스 - 람다식 활용
     String str = "";
