de6a51e5d9b0b87059793fc72a5168c1bef7f09d
artp <dky7034@naver.com>
2025-03-07 16:13:52 +0900

    Auto commit: Fri Mar  7 16:13:52 KST 2025

--- src/collection_framework/PerformanceDemo.java
+++ src/collection_framework/PerformanceDemo.java
@@ -19,8 +19,9 @@ public class PerformanceDemo {
     // 시간 체크 (
     startTime = System.currentTimeMillis();
     for (int i = 0; i < 100_000; i++) {
-
+      ll.addLast(i);
     }
-    System.out.println();
+    endTime = System.currentTimeMillis();
+    System.out.println("연결 리스트의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
   }
 }
