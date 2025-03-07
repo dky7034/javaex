0d17f3028f79c750412d65168cd38b657a99f57e
artp <dky7034@naver.com>
2025-03-07 15:35:44 +0900

    Auto commit: Fri Mar  7 15:35:44 KST 2025

--- src/collection_framework/StackDemo.java
+++ src/collection_framework/StackDemo.java
@@ -9,6 +9,9 @@ public class StackDemo {
 //    stack.push("20");
 //    stack.push("30");
 
+    if (!stack.isEmpty()) {
+      
+    }
     System.out.println(stack.peek()); // 30
     System.out.println(stack.pop()); // 30 삭제
     System.out.println(stack.peek()); // 20
