06d0ea07535e989869d2462c52a83c23de58d0e3
artp <dky7034@naver.com>
2025-03-07 16:17:04 +0900

    Auto commit: Fri Mar  7 16:17:04 KST 2025

--- src/collection_framework/PerformanceDemo.java
+++ src/collection_framework/PerformanceDemo.java
@@ -8,6 +8,7 @@ public class PerformanceDemo {
     ArrayList<Integer> al = new ArrayList<>();
     LinkedList<Integer> ll = new LinkedList<>();
 
+    // 마지막에 원소를 추가
     // 시간 체크 (nanoTime)
     long startTime = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
@@ -24,6 +25,7 @@ public class PerformanceDemo {
     endTime = System.currentTimeMillis();
     System.out.println("연결 리스트의 마지막에 원소를 추가하는 데 걸리는 시간: " + (endTime - startTime));
 
+    // 맨 앞에 원소를 추가
     // 시간 체크 (nanoTime)
     startTime = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
