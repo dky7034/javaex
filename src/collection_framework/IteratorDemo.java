9e18cea764eedf9160dd022554c4ae32c6d2d78a
artp <dky7034@naver.com>
2025-03-07 12:10:35 +0900

    Auto commit: Fri Mar  7 12:10:35 KST 2025

--- src/collection_framework/IteratorDemo.java
+++ src/collection_framework/IteratorDemo.java
@@ -1,9 +1,10 @@
 package collection_framework;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class IteratorDemo {
   public static void main(String[] args) {
-    Collection<String> collection = new
+    Collection<String> collection = Arrays.asList("다람쥐", "개구리", "나비");
   }
 }
