8705b64a62aaf01e33ca80761d755bc01d5f946f
artp <dky7034@naver.com>
2025-03-07 11:20:08 +0900

    Auto commit: Fri Mar  7 11:20:08 KST 2025

--- src/lambda/LambdaDemo5.java
+++ src/lambda/LambdaDemo5.java
@@ -2,6 +2,9 @@ package lambda;
 
 public class LambdaDemo5 {
   public static void main(String[] args) {
-    String[] sArr = {"", "", ""};
+    String[] sArr = {
+        "로마에 가면 로마법을 따르라",
+        "시간은 금이다",
+        "펜은 칼보다 강하다"};
   }
 }
