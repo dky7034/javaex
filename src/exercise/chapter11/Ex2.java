907f21069d1f44f7d51b67a3ac75518ae0e0e353
artp <dky7034@naver.com>
2025-03-07 17:25:12 +0900

    Auto commit: Fri Mar  7 17:25:12 KST 2025

--- src/exercise/chapter11/Ex2.java
+++ src/exercise/chapter11/Ex2.java
@@ -11,7 +11,7 @@ public class Ex2 {
     set.add(new Person("최고봉", 56));
     set.add(new Person("우등생", 16));
     set.add(new Person("나자바", 35));
-
+    
     for (Person person : set) {
       System.out.print(person);
     }
