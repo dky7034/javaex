28f159189dfc196a917f2108dca3957462e89263
artp <dky7034@naver.com>
2025-03-07 17:20:37 +0900

    Auto commit: Fri Mar  7 17:20:37 KST 2025

--- src/exercise/chapter11/Ex2.java
+++ src/exercise/chapter11/Ex2.java
@@ -2,6 +2,16 @@ package exercise.chapter11;
 
 public class Ex2 {
   public static void main(String[] args) {
-    
+
   }
+}
+
+// Person 클래스
+class Person {
+  // 필드
+
+  // 생성자
+
+  // 메서드
+  
 }
