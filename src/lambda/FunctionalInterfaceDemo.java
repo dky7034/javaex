1a37106483054137e06359d1eea03561dccc6f91
artp <dky7034@naver.com>
2025-03-07 10:30:01 +0900

    Auto commit: Fri Mar  7 10:30:01 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -2,20 +2,13 @@ package lambda;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
     list.forEach(i -> System.out.print(i + " : "));
 //    list.removeIf(j -> j % 2 == 0); // list의 모든 요소를 읽어서 짝수를 삭제
-    list.removeIf(new Predicate<Integer>() {
-      @Override
-      public boolean test(Integer integer) {
-        return integer % 2 == 0;
-      }
-    });
-
+    
     list.forEach(integer -> System.out.println(integer + " : "));
   }
 }
