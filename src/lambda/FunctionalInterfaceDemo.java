89f2be15d60c59f7382127ee07ed645640452b29
artp <dky7034@naver.com>
2025-03-07 10:39:07 +0900

    Auto commit: Fri Mar  7 10:39:07 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-    list.forEach(i -> System.out.print());
+    list.forEach(i -> System.out.print(i));
 
     System.out.println();
     list.replaceAll(i -> i * 10);
