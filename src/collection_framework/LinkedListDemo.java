5b9a2cba61b92515bfc5fec93fbb7901d6473e15
artp <dky7034@naver.com>
2025-03-07 16:02:07 +0900

    Auto commit: Fri Mar  7 16:02:07 KST 2025

--- src/collection_framework/LinkedListDemo.java
+++ src/collection_framework/LinkedListDemo.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 public class LinkedListDemo {
   public static void main(String[] args) {
     LinkedList<Integer> list = new LinkedList<>();
+    list.addFirst(10);
     
   }
 }
