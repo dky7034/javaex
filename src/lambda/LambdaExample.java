6be6ff3cc88aaa803e57161c49f1db2ddd558f10
artp <dky7034@naver.com>
2025-03-07 09:30:42 +0900

    Auto commit: Fri Mar  7 09:30:42 KST 2025

--- src/lambda/LambdaExample.java
+++ src/lambda/LambdaExample.java
@@ -8,6 +8,7 @@ public class LambdaExample {
   public static void main(String[] args) {
     List<String> list = Arrays.asList("abc", "aaa", "bbb", "ddd", "aaa");
 
+    System.out.println(list);
     Comparator<String> c = new Comparator<String>() {
       @Override
       public int compare(String o1, String o2) {
