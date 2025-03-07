99f1a6639d08445c46c863fb2f1a4545f1c22000
artp <dky7034@naver.com>
2025-03-07 16:02:26 +0900

    Auto commit: Fri Mar  7 16:02:26 KST 2025

--- src/collection_framework/LinkedListDemo.java
+++ src/collection_framework/LinkedListDemo.java
@@ -6,6 +6,8 @@ public class LinkedListDemo {
   public static void main(String[] args) {
     LinkedList<Integer> list = new LinkedList<>();
     list.addFirst(10);
+    list.addFirst(20);
+    list.addFirst(30);
     
   }
 }
