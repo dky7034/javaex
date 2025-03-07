1b29bb263cbc0b3847d24e2b314aeee76028982a
artp <dky7034@naver.com>
2025-03-07 16:41:40 +0900

    Auto commit: Fri Mar  7 16:41:40 KST 2025

--- src/collection_framework/SetDemo.java
+++ src/collection_framework/SetDemo.java
@@ -6,5 +6,9 @@ import java.util.Set;
 public class SetDemo {
   public static void main(String[] args) {
     Set<Integer> set = new HashSet<>();
+    set.add(10);
+    System.out.println(set);
+
+    set.add(10);
   }
 }
