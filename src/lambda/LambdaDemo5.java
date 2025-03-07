6da621c9e7bd86f3470a003a218f30468563520c
artp <dky7034@naver.com>
2025-03-07 11:21:45 +0900

    Auto commit: Fri Mar  7 11:21:45 KST 2025

--- src/lambda/LambdaDemo5.java
+++ src/lambda/LambdaDemo5.java
@@ -1,5 +1,7 @@
 package lambda;
 
+import java.util.Arrays;
+
 public class LambdaDemo5 {
   public static void main(String[] args) {
     String[] sArr = {
@@ -7,8 +9,8 @@ public class LambdaDemo5 {
         "시간은 금이다",
         "펜은 칼보다 강하다"
     };
-    
 
-
+    Arrays.sort(sArr);
+    System.out.println(Arrays.toString());
   }
 }
