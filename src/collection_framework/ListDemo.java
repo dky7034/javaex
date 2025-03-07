203c0f8ffc2f6e75b4d905f0708f263ea952cbd8
artp <dky7034@naver.com>
2025-03-07 15:08:04 +0900

    Auto commit: Fri Mar  7 15:08:04 KST 2025

--- src/collection_framework/ListDemo.java
+++ src/collection_framework/ListDemo.java
@@ -1,5 +1,6 @@
 package collection_framework;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,7 +8,8 @@ public class ListDemo {
   public static void main(String[] args) {
     String[] animals = {"a", "b", "c", "d"};
     List<String> list = Arrays.asList(animals);
-    
+
+    List<Integer> listl = new ArrayList<>();
 
   }
 }
