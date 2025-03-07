c5c4ee6efe1d338648c9dfed9639006dc704e6b6
artp <dky7034@naver.com>
2025-03-07 23:28:51 +0900

    Auto commit: Fri Mar  7 23:28:51 KST 2025

--- src/exercise/chapter11/Ex2.java
+++ src/exercise/chapter11/Ex2.java
@@ -1,6 +1,7 @@
 package exercise.chapter11;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Ex2 {
@@ -47,7 +48,7 @@ class Person {
     // ull 체크 + obj 가 현재 객체와 같은 타입인지 판별하여 하나라도 맞으면 false 반환
     if (obj == null || getClass() != obj.getClass()) return false;
     Person p = (Person) obj;
-    return (this.name.equals(p.name)); // 이름이 같으면 true 리턴, 다르면 false 리턴
+    return Objects.equals(name, p.name); // 이름이 같으면 true 리턴, 다르면 false 리턴
   }
 
 //  // hashCode 오버라이딩
