a8a5528fde43dcbac9c655be8c3b37b07c398b9e
artp <dky7034@naver.com>
2025-03-07 17:05:19 +0900

    Auto commit: Fri Mar  7 17:05:19 KST 2025

--- src/exercise/chapter11/Ex1.java
+++ src/exercise/chapter11/Ex1.java
@@ -5,5 +5,10 @@ import java.util.ArrayList;
 public class Ex1 {
   public static void main(String[] args) {
     ArrayList<String> al = new ArrayList<>();
+    al.add("갈매기");
+    al.add("다람쥐");
+    al.add("라마");
+
+    
   }
 }
