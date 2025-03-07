3d614a2c160c25d7394eacb556d07c36808fee25
artp <dky7034@naver.com>
2025-03-07 10:33:53 +0900

    Auto commit: Fri Mar  7 10:33:53 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -8,12 +8,12 @@ public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
     list.forEach(i -> System.out.print(i + " : "));
-//    list.removeIf(j -> j % 2 == 0); // list의 모든 요소를 읽어서 짝수를 삭제
+
     System.out.println();
-//    list.forEach(integer -> System.out.println(integer + " : "));
     list.replaceAll(i -> i * 10);
     list.forEach(i -> System.out.print(i + " : "));
 
+    System.out.println();
     HashMap<Integer, String> map = new HashMap<>();
     map.put(1, "사과");
     map.put(2, "딸기");
