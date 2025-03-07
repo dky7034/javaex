8ee953f5ddb0163a51becadbb3da350f41169381
artp <dky7034@naver.com>
2025-03-07 14:36:07 +0900

    Auto commit: Fri Mar  7 14:36:07 KST 2025

--- src/collection_framework/ListDemo.java
+++ src/collection_framework/ListDemo.java
@@ -2,6 +2,6 @@ package collection_framework;
 
 public class ListDemo {
   public static void main(String[] args) {
-    String[] animals = {"a", "b", "c", "d"}
+    String[] animals = {"a", "b", "c", "d"};
   }
 }
