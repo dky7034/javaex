5feaa0c450776cd3d0c9483a4ea90fbf88d37d58
artp <dky7034@naver.com>
2025-03-07 16:09:49 +0900

    Auto commit: Fri Mar  7 16:09:49 KST 2025

--- src/collection_framework/PerformanceDemo.java
+++ src/collection_framework/PerformanceDemo.java
@@ -1,7 +1,11 @@
 package collection_framework;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class PerformanceDemo {
   public static void main(String[] args) {
-    
+    ArrayList<Integer> al = new ArrayList<>();
+    LinkedList<Integer> ll = new LinkedList<>();
   }
 }
