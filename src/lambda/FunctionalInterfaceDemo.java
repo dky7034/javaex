5f3aafc565326c1b1998906b799b145881825ba3
artp <dky7034@naver.com>
2025-03-07 10:39:46 +0900

    Auto commit: Fri Mar  7 10:39:46 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -8,6 +8,7 @@ public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
     list.forEach(i -> System.out.print(i));
+    System.out.println();
     list.forEach(System.out::println);
 
     System.out.println();
