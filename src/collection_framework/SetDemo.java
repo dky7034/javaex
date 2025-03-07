dad07b84e80ab876d1bf220cb9404f084d90ed1b
artp <dky7034@naver.com>
2025-03-07 16:39:56 +0900

    Auto commit: Fri Mar  7 16:39:56 KST 2025

--- src/collection_framework/SetDemo.java
+++ src/collection_framework/SetDemo.java
@@ -1,4 +1,10 @@
 package collection_framework;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo {
+  public static void main(String[] args) {
+    Set<Integer> set = new HashSet<>();
+  }
 }
