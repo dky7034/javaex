93a4574d0ba705d41cbf192b3b63446ad189043a
artp <dky7034@naver.com>
2025-03-07 10:05:56 +0900

    Auto commit: Fri Mar  7 10:05:56 KST 2025

--- src/lambda/PredicateDemo.java
+++ src/lambda/PredicateDemo.java
@@ -4,12 +4,12 @@ import java.util.function.Predicate;
 
 public class PredicateDemo {
   public static void main(String[] args) {
-    Predicate<String> isEmpty = new Predicate<String>() {
-      @Override
-      public boolean test(String s) {
-        return s.length() == 0;
-      }
-    };
+//    Predicate<String> isEmpty = new Predicate<String>() {
+//      @Override
+//      public boolean test(String s) {
+//        return s.length() == 0;
+//      }
+//    };
     Predicate<String> isEmpty2 = s -> s.length()==0;
 
     String s = "";
