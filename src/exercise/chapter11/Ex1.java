76a4dad74c40672894a6d256f4343d3459e8f44f
artp <dky7034@naver.com>
2025-03-07 17:04:52 +0900

    Auto commit: Fri Mar  7 17:04:52 KST 2025

--- src/exercise/chapter11/Ex1.java
+++ src/exercise/chapter11/Ex1.java
@@ -1,7 +1,9 @@
 package exercise.chapter11;
 
+import java.util.ArrayList;
+
 public class Ex1 {
   public static void main(String[] args) {
-    
+    ArrayList<String> al = new ArrayList<>();
   }
 }
