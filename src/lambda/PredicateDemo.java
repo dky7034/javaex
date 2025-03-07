e4b9ae721fbd921c6238c5bd81bfeadf9c02989f
artp <dky7034@naver.com>
2025-03-07 10:03:14 +0900

    Auto commit: Fri Mar  7 10:03:14 KST 2025

--- src/lambda/PredicateDemo.java
+++ src/lambda/PredicateDemo.java
@@ -1,7 +1,15 @@
 package lambda;
 
+import java.util.function.Predicate;
+
 public class PredicateDemo {
   public static void main(String[] args) {
-
+    Predicate<String> isEmpty = new Predicate<String>() {
+      @Override
+      public boolean test(String s) {
+        return false;
+      }
+    }
   }
 }
+
