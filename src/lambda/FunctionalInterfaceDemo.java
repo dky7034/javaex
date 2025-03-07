088c25fc81f16dc675f0b8975bae7dae6fec05fc
artp <dky7034@naver.com>
2025-03-07 10:43:31 +0900

    Auto commit: Fri Mar  7 10:43:31 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -1,12 +1,13 @@
 package lambda;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
-    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+    List<Integer> list = new ArrayList<>1, 2, 3, 4, 5);
     list.removeIf(j -> j % 2 == 0);
     list.forEach(i -> System.out.print(i));
     System.out.println();
