24dbff4c6f2f0714108bd323c71d2e957cabbfb5
artp <dky7034@naver.com>
2025-03-07 10:27:04 +0900

    Auto commit: Fri Mar  7 10:27:04 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -8,5 +8,6 @@ public class FunctionalInterfaceDemo {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
     list.forEach(i -> System.out.print(i + " : "));
     list.removeIf(j -> j % 2 == 0); // list의 모든 요소를 읽어서 짝수를 삭제
+    list.forEach(integer -> System.out.println(integer + " : "));
   }
 }
