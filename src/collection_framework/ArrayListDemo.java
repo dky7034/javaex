9b2bfb0c7874cb2a1911522237b4fdf2435ca2cb
artp <dky7034@naver.com>
2025-03-07 15:23:05 +0900

    Auto commit: Fri Mar  7 15:23:05 KST 2025

--- src/collection_framework/ArrayListDemo.java
+++ src/collection_framework/ArrayListDemo.java
@@ -14,6 +14,6 @@ public class ArrayListDemo {
     System.out.println("list2 = " + list2);
     list2.forEach(s -> System.out.println(s));
     list2.forEach(System.out::println);
-    list2.replaceAll(s -> "뉴");
+    list2.replaceAll(s -> "뉴 " + s);
   }
 }
