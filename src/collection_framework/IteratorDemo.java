1247a8e1f1ae83e2c9e397cdb340b40d29bc76c6
artp <dky7034@naver.com>
2025-03-07 12:14:08 +0900

    Auto commit: Fri Mar  7 12:14:08 KST 2025

--- src/collection_framework/IteratorDemo.java
+++ src/collection_framework/IteratorDemo.java
@@ -9,7 +9,7 @@ public class IteratorDemo {
     Collection<String> list = Arrays.asList("다람쥐", "개구리", "나비");
     Iterator<String> iterator = list.iterator();
 
-    if (iterator.hasNext() == true) {
+    if (iterator.hasNext()) {
     }
     System.out.println(iterator.next());
     System.out.println(iterator.next());
