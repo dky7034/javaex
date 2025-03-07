40d8ba0c3fb0d62c6ceddc8870ed84f56c5747fa
artp <dky7034@naver.com>
2025-03-07 10:29:23 +0900

    Auto commit: Fri Mar  7 10:29:23 KST 2025

--- src/lambda/FunctionalInterfaceDemo.java
+++ src/lambda/FunctionalInterfaceDemo.java
@@ -8,7 +8,7 @@ public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
     list.forEach(i -> System.out.print(i + " : "));
-    list.removeIf(j -> j % 2 == 0); // list의 모든 요소를 읽어서 짝수를 삭제
+//    list.removeIf(j -> j % 2 == 0); // list의 모든 요소를 읽어서 짝수를 삭제
     list.removeIf(new Predicate<Integer>() {
       @Override
       public boolean test(Integer integer) {
