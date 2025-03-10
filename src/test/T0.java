556267f7ea0faeec7837c705bacb43217828c185
artp <dky7034@naver.com>
2025-03-10 22:11:55 +0900

    Auto commit: Mon Mar 10 22:11:55 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -1,11 +1,7 @@
 package test;
 
 public class T0 extends AbstractClass {
-  @Override
-  void abstractMethod1() {}
-
-  @Override
-  void abstractMethod2() {}
+  
 
   public static void main(String[] args) {
     T0class t0class = new T0class();
