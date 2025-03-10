9a49d9558837e1dfb2ee33170e2050c8bc4c93b4
artp <dky7034@naver.com>
2025-03-10 11:22:54 +0900

    Auto commit: Mon Mar 10 11:22:54 KST 2025

--- src/collection_framework/CollectionsDemo.java
+++ src/collection_framework/CollectionsDemo.java
@@ -2,6 +2,6 @@ package collection_framework;
 
 public class CollectionsDemo {
   public static void main(String[] args) {
-    String[] fruits = {};
+    String[] fruits = {"포도", "수박", "사과", "키위", "망고"};
   }
 }
