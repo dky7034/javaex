64e18bb2bf9f628dfed900a99b35dd8b44b06c75
artp <dky7034@naver.com>
2025-03-10 11:24:39 +0900

    Auto commit: Mon Mar 10 11:24:39 KST 2025

--- src/collection_framework/CollectionsDemo.java
+++ src/collection_framework/CollectionsDemo.java
@@ -1,7 +1,14 @@
 package collection_framework;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CollectionsDemo {
   public static void main(String[] args) {
     String[] fruits = {"포도", "수박", "사과", "키위", "망고"};
+    Collections.sort(fruits);
+    Map<Fruit, Integer> fruitsMap = new HashMap<>();
+    Collections.sort(fru);
   }
 }
