823ee88e2edc8597d85062641a8be74cd6e3a77f
artp <dky7034@naver.com>
2025-03-10 12:17:27 +0900

    Auto commit: Mon Mar 10 12:17:27 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -1,7 +1,11 @@
 package collection_framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class T2 {
   public static void main(String[] args) {
+    Map<String, String> map = new HashMap<>();
     
   }
 }
