cec0b743639134600e9b393d9a29a826caf4bf8b
artp <dky7034@naver.com>
2025-03-10 09:47:58 +0900

    Auto commit: Mon Mar 10 09:47:58 KST 2025

--- src/collection_framework/MapDemo.java
+++ src/collection_framework/MapDemo.java
@@ -9,6 +9,8 @@ public class MapDemo {
       fruits = Map.of("사과", 5, "바나나", 3, "포도", 10, "딸기", 1, "사과", 50);
     } catch (IllegalArgumentException e) {
       System.out.println(e.getLocalizedMessage());
+    } catch (NullPointerException e) {
+      
     }
     System.out.println(fruits.size() + "종류의 과일이 있습니다.");
   }
