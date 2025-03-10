3ac3cc8b1990387b5a74c5261cda09b59f87772a
artp <dky7034@naver.com>
2025-03-10 17:18:47 +0900

    Auto commit: Mon Mar 10 17:18:47 KST 2025

--- src/stream/LazyDemo.java
+++ src/stream/LazyDemo.java
@@ -4,11 +4,11 @@ import java.util.stream.IntStream;
 
 public class LazyDemo {
   public static void main(String[] args) {
-    IntStream intStream = IntStream.rangeClosed(1, 5);
-    IntStream filterStream = intStream.filter(i -> i % 2 == 0);
-    IntStream mapStream = filterStream.map(x -> x * x);
+//    IntStream intStream = IntStream.rangeClosed(1, 5);
+//    IntStream filterStream = intStream.filter(i -> i % 2 == 0);
+//    IntStream mapStream = filterStream.map(x -> x * x);
 
-    
+
     IntStream.rangeClosed(1, 5)
         .filter(i -> i % 2 == 0)
         .map(x -> x * x)
