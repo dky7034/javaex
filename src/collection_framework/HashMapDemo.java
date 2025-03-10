687b0b4ef1fbb97a5dff0c9f25e309beb17a1bc6
artp <dky7034@naver.com>
2025-03-10 10:35:01 +0900

    Auto commit: Mon Mar 10 10:35:01 KST 2025

--- src/collection_framework/HashMapDemo.java
+++ src/collection_framework/HashMapDemo.java
@@ -1,4 +1,10 @@
 package collection_framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
+  public static void main(String[] args) {
+    Map<Fruit, Integer> fruits = new HashMap<>();
+  }
 }
