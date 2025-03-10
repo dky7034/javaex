21891a1b3ebfb82081943b7b381152222fd5d6f0
artp <dky7034@naver.com>
2025-03-10 17:15:40 +0900

    Auto commit: Mon Mar 10 17:15:40 KST 2025

--- src/stream/LazyDemo.java
+++ src/stream/LazyDemo.java
@@ -1,12 +1,19 @@
 package stream;
 
+import java.util.function.UnaryOperator;
 import java.util.stream.IntStream;
 
 public class LazyDemo {
   public static void main(String[] args) {
     IntStream intStream = IntStream.rangeClosed(1, 5);
-    intStream.filter(i -> i % 2 == 0)
+    intStream.filter(i -> i % 2 == 0);
 
+    UnaryOperator unaryOperator = new UnaryOperator() {
+      @Override
+      public Object apply(Object o) {
+        return null;
+      }
+    };
 
   }
 }
