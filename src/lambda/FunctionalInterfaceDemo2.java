37ff85651c7e5fb1f6cb36fe38be8ab328ed4b28
artp <dky7034@naver.com>
2025-03-10 14:10:38 +0900

    Auto commit: Mon Mar 10 14:10:38 KST 2025

--- src/lambda/FunctionalInterfaceDemo2.java
+++ src/lambda/FunctionalInterfaceDemo2.java
@@ -3,7 +3,12 @@ package lambda;
 public class FunctionalInterfaceDemo2 {
   public static void main(String[] args) {
     // 두 개의 정수를 입력받아서 두 수를 더한 후 그 결과를 출력
-    
+    Calculable c = new Calculable() {
+      @Override
+      public void calculate(int x, int y) {
+        
+      }
+    }
   }
 }
 
