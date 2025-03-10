c24cb44b618ff763ea32b120053536f762c799e4
artp <dky7034@naver.com>
2025-03-10 16:20:39 +0900

    Auto commit: Mon Mar 10 16:20:39 KST 2025

--- src/stream/StreamDemo3.java
+++ src/stream/StreamDemo3.java
@@ -2,11 +2,13 @@ package stream;
 
 import java.util.Random;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamDemo3 {
   public static void main(String[] args) {
     Random r = new Random();
     IntStream intStream = r.ints();
-    intStream.forEach(System.out::println); // 무한루프
+//    intStream.forEach(System.out::println); // 무한루프
+    Stream.generate(() -> 1)
   }
 }
