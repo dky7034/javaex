79267650dc3cefe09573b3c5f14c735e1d6eb792
artp <dky7034@naver.com>
2025-03-10 09:43:36 +0900

    Auto commit: Mon Mar 10 09:43:36 KST 2025

--- src/collection_framework/MapDemo.java
+++ src/collection_framework/MapDemo.java
@@ -1,4 +1,9 @@
 package collection_framework;
 
-public class MapDemp {
+import java.util.Map;
+
+public class MapDemo {
+  public static void main(String[] args) {
+    Map.of("사과", 5, "바나나", 3, "포도", "10", "딸기", 1);
+  }
 }
