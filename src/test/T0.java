37336c2e963375b5777aeda048bf70ee262a1d62
artp <dky7034@naver.com>
2025-03-10 22:11:04 +0900

    Auto commit: Mon Mar 10 22:11:04 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -1,14 +1,11 @@
 package test;
 
-public class T0 extends T0class {
+public class T0 extends AbstractClass{
   public static void main(String[] args) {
     T0class t0class = new T0class();
     boolean b = t0class.isLongEnough("String");
     System.out.println("b = " + b);
 
-    T0 t0 = new T0();
-    boolean b1 = t0.isLongEnough("Integer");
-    System.out.println("b1 = " + b1);
 
   }
 }
