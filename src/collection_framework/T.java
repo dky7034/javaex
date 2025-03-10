31f90fe560575bbcf5a08ef3189e934fecf46150
artp <dky7034@naver.com>
2025-03-10 12:10:52 +0900

    Auto commit: Mon Mar 10 12:10:52 KST 2025

--- src/collection_framework/T.java
+++ src/collection_framework/T.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class T {
   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
     Map<String, Integer> map = new HashMap<>();
 
     map.put("김열공", 80);
@@ -13,7 +14,6 @@ public class T {
     map.put("우등생", 80);
     map.put("나자바", 80);
 
-    Scanner in = new Scanner(System.in);
     String name = in.nextLine();
     System.out.print("이름을 입력하세요 : ");
 
