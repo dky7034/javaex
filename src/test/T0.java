dc471d3b492928c2d8dface73b7e720d64c4c3f5
artp <dky7034@naver.com>
2025-03-10 21:46:19 +0900

    Auto commit: Mon Mar 10 21:46:19 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -3,8 +3,8 @@ package test;
 public class T0 {
   public static void main(String[] args) {
     T0class t0class = new T0class();
-    t0class.
-    System.out.println();
+    boolean b = t0class.isLongEnough("String");
+    System.out.println(b);
 
   }
 }
