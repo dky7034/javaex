06f752adaeeb5779a5f4d65c646c17bfc7e30409
artp <dky7034@naver.com>
2025-03-10 12:25:53 +0900

    Auto commit: Mon Mar 10 12:25:53 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -8,12 +8,14 @@ public class T2 {
     Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
     for (String value : map.values()) {
       map.replaceAll((s1, s2) -> s2.toUpperCase());
+
       BiFunction b = new BiFunction() {
         @Override
         public Object apply(Object o1, Object o2) {
           return null;
         }
       };
+
       System.out.println(value);
     }
 
