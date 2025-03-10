e9c06cf9a1169e250017cae3988886b8e359f4db
artp <dky7034@naver.com>
2025-03-10 11:43:58 +0900

    Auto commit: Mon Mar 10 11:43:58 KST 2025

--- src/collection_framework/CollectionsDemo2.java
+++ src/collection_framework/CollectionsDemo2.java
@@ -5,5 +5,6 @@ import java.util.List;
 public class CollectionsDemo2 {
   public static void main(String[] args) {
     List<String> list1 = List.of("포도", "키위", "수박", "사과", "망고", "포도", "키위", "수박", "사과", "망고");
+    
   }
 }
