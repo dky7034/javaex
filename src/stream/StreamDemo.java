62d7b4b710503732076cfcfef11fda714ff76941
artp <dky7034@naver.com>
2025-03-10 14:56:27 +0900

    Auto commit: Mon Mar 10 14:56:27 KST 2025

--- src/stream/StreamDemo.java
+++ src/stream/StreamDemo.java
@@ -31,7 +31,7 @@ public class StreamDemo {
     list.stream()
         .filter(i -> i > 10) // Predicate 함수형 인터페이스의 익명 구현체
         .sorted()
-        .forEach(i -> System.out::println); // Consumer 함수형 인터페이스의 익명 구현체
+        .forEach(System.out::println); // Consumer 함수형 인터페이스의 익명 구현체
 
   }
 }
