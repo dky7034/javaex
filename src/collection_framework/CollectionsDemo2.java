c02ca2694decd30f987edc10a7a599ad425346c5
artp <dky7034@naver.com>
2025-03-10 11:45:07 +0900

    Auto commit: Mon Mar 10 11:45:07 KST 2025

--- src/collection_framework/CollectionsDemo2.java
+++ src/collection_framework/CollectionsDemo2.java
@@ -1,10 +1,14 @@
 package collection_framework;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionsDemo2 {
   public static void main(String[] args) {
     List<String> list1 = List.of("포도", "키위", "수박", "사과", "망고", "포도", "키위", "수박", "사과", "망고");
     System.out.println(list1);
+    Set<String> set = new HashSet<>();
+    System.out.println(set);
   }
 }
