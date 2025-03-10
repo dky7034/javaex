512b7ff6a0593a71e1cfa62cc7fe1edb98d5fde1
artp <dky7034@naver.com>
2025-03-10 13:08:58 +0900

    Auto commit: Mon Mar 10 13:08:58 KST 2025

--- src/test/N9.java
+++ src/test/N9.java
@@ -1,7 +1,10 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class N9 {
   public static void main(String[] args) {
-    
+    Map<String, Integer> map = new HashMap<>();
   }
 }
