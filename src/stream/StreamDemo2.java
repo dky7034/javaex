1b92462236ba0b3a208f6be6c3f97be48b12d385
artp <dky7034@naver.com>
2025-03-10 15:29:12 +0900

    Auto commit: Mon Mar 10 15:29:12 KST 2025

--- src/stream/StreamDemo2.java
+++ src/stream/StreamDemo2.java
@@ -1,7 +1,10 @@
 package stream;
 
+import java.util.stream.Stream;
+
 public class StreamDemo2 {
   public static void main(String[] args) {
     int[] arr = new int[]{1, 2, 3, 4, 5}; // 배열, 공간적 집합체
+    Stream.of(arr);
   }
 }
