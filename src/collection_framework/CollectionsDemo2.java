3d1ef9a0f5dd015c0b1e5290fa34e07936b49d69
artp <dky7034@naver.com>
2025-03-10 11:42:31 +0900

    Auto commit: Mon Mar 10 11:42:31 KST 2025

--- src/collection_framework/CollectionsDemo2.java
+++ src/collection_framework/CollectionsDemo2.java
@@ -1,7 +1,9 @@
 package collection_framework;
 
+import java.util.List;
+
 public class CollectionsDemo2 {
   public static void main(String[] args) {
-    
+    List<String> list1 = List.of();
   }
 }
