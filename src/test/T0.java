6866a874a1335a1218bc3848460ca5f1328a074d
artp <dky7034@naver.com>
2025-03-10 22:10:17 +0900

    Auto commit: Mon Mar 10 22:10:17 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -28,5 +28,7 @@ class T0class implements T0interface {
 abstract class AbstractClass {
   abstract void abstractMethod1();
   abstract void abstractMethod2();
-  
+  default void defaultMethod1() {
+    
+  }
 }
