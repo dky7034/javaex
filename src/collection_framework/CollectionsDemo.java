a02b8dd6b948ba559585c49eb1626757eaabe17e
artp <dky7034@naver.com>
2025-03-10 11:27:05 +0900

    Auto commit: Mon Mar 10 11:27:05 KST 2025

--- src/collection_framework/CollectionsDemo.java
+++ src/collection_framework/CollectionsDemo.java
@@ -6,9 +6,9 @@ public class CollectionsDemo {
   public static void main(String[] args) {
     String[] fruits = {"포도", "수박", "사과", "키위", "망고"};
     List<String> fruitsList = Arrays.asList(fruits);
-    Collections.sort(fruitsList);
+//    Collections.sort(fruitsList); // Collections 클래스의 sort 메서드는 배열, Set 사용 불가능
     Map<Fruit, Integer> fruitsMap = new HashMap<>();
-    Collections.sort(fruitsMap);
+//    Collections.sort(fruitsMap); // Map 구현체도 사용 불가능
 
   }
 }
