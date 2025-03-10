d29a2b365e9e413573e68481ce6e79117e41f8f1
artp <dky7034@naver.com>
2025-03-10 12:05:13 +0900

    Auto commit: Mon Mar 10 12:05:13 KST 2025

--- src/collection_framework/T.java
+++ src/collection_framework/T.java
@@ -1,4 +1,16 @@
 package collection_framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class T {
+  public static void main(String[] args) {
+    Map<String, Integer> map = new HashMap<>();
+
+    map.put("김열공", 80);
+    map.put("최고봉", 80);
+    map.put("우등생", 80);
+    map.put("나자바", 80);
+
+  }
 }
