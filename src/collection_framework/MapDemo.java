5afe0390809ec1953f357bff5aba88e38148448d
artp <dky7034@naver.com>
2025-03-10 09:45:16 +0900

    Auto commit: Mon Mar 10 09:45:16 KST 2025

--- src/collection_framework/MapDemo.java
+++ src/collection_framework/MapDemo.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class MapDemo {
   public static void main(String[] args) {
-    Map<String, Integer> fruits = Map.of("사과", 5, "바나나", 3, "포도", 10, "딸기", 1);
+    Map<String, Integer> fruits = Map.of("사과", 5, "바나나", 3, "포도", 10, "딸기", 1, "사과", 50);
     System.out.println(fruits.size() + "종류의 과일이 있습니다.");
   }
 }
