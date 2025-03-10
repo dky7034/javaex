ece6de447e801fd6b1e28ceb4c0298a39b649456
artp <dky7034@naver.com>
2025-03-10 14:41:48 +0900

    Auto commit: Mon Mar 10 14:41:48 KST 2025

--- src/stream/StreamDemo.java
+++ src/stream/StreamDemo.java
@@ -1,7 +1,11 @@
 package stream;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StreamDemo {
   public static void main(String[] args) {
-
+    List<Integer> list = new ArrayList<>();
+    
   }
 }
