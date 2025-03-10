21fff33a20e1022210ee4e3b24c76ac56be51a04
artp <dky7034@naver.com>
2025-03-10 09:44:15 +0900

    Auto commit: Mon Mar 10 09:44:15 KST 2025

--- src/collection_framework/MapDemo.java
+++ src/collection_framework/MapDemo.java
@@ -4,6 +4,6 @@ import java.util.Map;
 
 public class MapDemo {
   public static void main(String[] args) {
-    Map.of("사과", 5, "바나나", 3, "포도", "10", "딸기", 1);
+    Map.of("사과", 5, "바나나", 3, "포도", 10, "딸기", 1);
   }
 }
