9b385df7725eefe7d7daabffb4675969e34f8b8a
artp <dky7034@naver.com>
2025-03-10 12:21:07 +0900

    Auto commit: Mon Mar 10 12:21:07 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -5,6 +5,10 @@ import java.util.Map;
 public class T2 {
   public static void main(String[] args) {
     Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
+    for (String value : map.values()) {
+      System.out.println(value);
+    }
+//    Collections.replaceAll();
     
   }
 }
