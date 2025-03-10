0d608e2869ca41e2aafd81722b66f99763454fb5
artp <dky7034@naver.com>
2025-03-10 15:51:13 +0900

    Auto commit: Mon Mar 10 15:51:13 KST 2025

--- src/test/N9.java
+++ src/test/N9.java
@@ -15,7 +15,7 @@ public class N9 {
     int totalScore = 0; // 점수 합계를 저장하는 변수
 
     // 여기에 코드를 작성하세요.
-    //
+    // entrySet() 사용
     for (Map.Entry<String, Integer> entry : map.entrySet()) {
       totalScore += entry.getValue();
       if (maxScore < entry.getValue()) {
