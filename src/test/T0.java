60b0b7de68f3ab7c78caa918ccf4dcaf9450ecc1
artp <dky7034@naver.com>
2025-03-10 22:11:08 +0900

    Auto commit: Mon Mar 10 22:11:08 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -1,6 +1,6 @@
 package test;
 
-public class T0 extends AbstractClass{
+public class T0 extends AbstractClass {
   public static void main(String[] args) {
     T0class t0class = new T0class();
     boolean b = t0class.isLongEnough("String");
