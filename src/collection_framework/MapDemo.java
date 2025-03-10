f453dd0753e0f1826b3ff11adac08706590a3941
artp <dky7034@naver.com>
2025-03-10 09:48:59 +0900

    Auto commit: Mon Mar 10 09:48:59 KST 2025

--- src/collection_framework/MapDemo.java
+++ src/collection_framework/MapDemo.java
@@ -6,7 +6,7 @@ public class MapDemo {
   public static void main(String[] args) {
     Map<String, Integer> fruits = null;
     try {
-      fruits = Map.of("사과", 5, "바나나", 3, "포도", 10, "딸기", 1, "사과", 50);
+      fruits = Map.of("사과", 5, "바나나", 3, "포도", 10, "딸기", 1);
     } catch (IllegalArgumentException e) {
       System.out.println(e.getLocalizedMessage());
     } catch (NullPointerException e) {
