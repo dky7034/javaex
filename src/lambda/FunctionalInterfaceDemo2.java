13eaab6663af3508aec0207bbbe8cae8c4389a1e
artp <dky7034@naver.com>
2025-03-10 14:14:01 +0900

    Auto commit: Mon Mar 10 14:14:01 KST 2025

--- src/lambda/FunctionalInterfaceDemo2.java
+++ src/lambda/FunctionalInterfaceDemo2.java
@@ -9,17 +9,18 @@ public class FunctionalInterfaceDemo2 {
 
       }
     };
-}
+  }
 
-@FunctionalInterface
-interface Calculable {
-  // 추상메서드
-  void calculate(int x, int y);
-}
+  @FunctionalInterface
+  interface Calculable {
+    // 추상메서드
+    void calculate(int x, int y);
+  }
 
-class add implements Calculable {
-  @Override
-  public void calculate(int x, int y) {
-    System.out.println(x + y);
+  class add implements Calculable {
+    @Override
+    public void calculate(int x, int y) {
+      System.out.println(x + y);
+    }
   }
 }
