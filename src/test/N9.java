09e52aa4d061566993d4dcf99ef80d847e4c9703
artp <dky7034@naver.com>
2025-03-10 15:51:04 +0900

    Auto commit: Mon Mar 10 15:51:04 KST 2025

--- src/test/N9.java
+++ src/test/N9.java
@@ -2,7 +2,6 @@ package test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class N9 {
   public static void main(String[] args) {
@@ -14,10 +13,10 @@ public class N9 {
     String name = null; // 최고 점수를 받은 아이디를 저장하는 변수
     int maxScore = 0; // 최고 점수를 저장하는 변수
     int totalScore = 0; // 점수 합계를 저장하는 변수
-    
+
     // 여기에 코드를 작성하세요.
     //
-    for (Map.Entry<String, Integer> entry : entries) {
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
       totalScore += entry.getValue();
       if (maxScore < entry.getValue()) {
         maxScore = entry.getValue();
