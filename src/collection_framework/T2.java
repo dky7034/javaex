f2d4878540e5d01b442df6772af9c493ee83945b
artp <dky7034@naver.com>
2025-03-10 12:30:46 +0900

    Auto commit: Mon Mar 10 12:30:46 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -1,10 +1,11 @@
 package collection_framework;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class T2 {
   public static void main(String[] args) {
-    Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
+    Map<String, String> map = new HashMap<>(Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion"));
 
     map.replaceAll((key, value) -> value.toUpperCase());
 
