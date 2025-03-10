a43fcdb650ee24e1ee3524f63c5a0f503173e166
artp <dky7034@naver.com>
2025-03-10 16:31:16 +0900

    Auto commit: Mon Mar 10 16:31:16 KST 2025

--- src/stream/StreamDemo3.java
+++ src/stream/StreamDemo3.java
@@ -14,10 +14,10 @@ public class StreamDemo3 {
 //        .nextInt(10))
 //        .forEach(System.out::println); // 무한루프
 
-//    IntStream range = IntStream.range(1, 10); // 1 ~ 9 까지 한 번씩 생성, end 불포함 (1, 2, 3, 4, 5, 6, 7, 8, 9)
+//    IntStream range = IntStream.range(1, 10); // 유한 생성, end 불포함 (1, 2, 3, 4, 5, 6, 7, 8, 9)
 //    range.forEach(System.out::println);
 
-    LongStream range2 = LongStream.rangeClosed(1, 10); // end 포함
+    LongStream range2 = LongStream.rangeClosed(1, 10); // 유한 생성, end 포함 (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     range2.forEach(System.out::println);
 
   } // end of main method
