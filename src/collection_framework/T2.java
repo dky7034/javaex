b095dcd023820319436f90efc4fe268a9855fec6
artp <dky7034@naver.com>
2025-03-10 12:41:00 +0900

    Auto commit: Mon Mar 10 12:41:00 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class T2 {
   public static void main(String[] args) {
+    // Map.of는 불변 객체를 생성합니다.
     Map<String, String> map = new HashMap<>(Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion"));
 
     map.replaceAll((key, value) -> value.toUpperCase());
