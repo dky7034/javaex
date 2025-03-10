df3c72192ea5e7510aa953ed35b5567a692b7a4b
artp <dky7034@naver.com>
2025-03-10 12:23:54 +0900

    Auto commit: Mon Mar 10 12:23:54 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class T2 {
   public static void main(String[] args) {
     Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
-    
+    Set<String> set = new HashSet<>(map);
+
     for (String value : map.values()) {
       Collections.replaceAll(map, value, value.toUpperCase());
       System.out.println(value);
