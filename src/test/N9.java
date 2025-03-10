b5805070b5c28d5fda07a47021198e6d89cb45dc
artp <dky7034@naver.com>
2025-03-10 16:45:41 +0900

    Auto commit: Mon Mar 10 16:45:41 KST 2025

--- src/test/N9.java
+++ src/test/N9.java
@@ -11,8 +11,6 @@ public class N9 {
     map.put("white", 92);
 
     String name = null; // 최고 점수를 받은 아이디를 저장하는 변수
-    int max = Integer.MIN_VALUE;
-    System.out.println("max = " + max);
     int maxScore = 0; // 최고 점수를 저장하는 변수
     int totalScore = 0; // 점수 합계를 저장하는 변수
 
