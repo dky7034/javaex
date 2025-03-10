3778e3dd19d75f6bf31939aeeecd38cb9ff7a499
artp <dky7034@naver.com>
2025-03-10 12:29:31 +0900

    Auto commit: Mon Mar 10 12:29:31 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -1,24 +1,12 @@
 package collection_framework;
 
 import java.util.Map;
-import java.util.function.BiFunction;
 
 public class T2 {
   public static void main(String[] args) {
     Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
-    for (String value : map.values()) {
-      map.replaceAll((key, value) -> value.toUpperCase());
 
-
-      System.out.println(value);
-    }
-
-    BiFunction b = new BiFunction() {
-      @Override
-      public Object apply(Object o1, Object o2) {
-        return null;
-      }
-    };
-
+    map.replaceAll((key, value) -> value.toUpperCase());
   }
+
 }
