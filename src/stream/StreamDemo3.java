5e6bd715bf1ae8e0d1ff15c7680d0f4be7d342bd
artp <dky7034@naver.com>
2025-03-10 16:15:01 +0900

    Auto commit: Mon Mar 10 16:15:01 KST 2025

--- src/stream/StreamDemo3.java
+++ src/stream/StreamDemo3.java
@@ -7,7 +7,7 @@ public class StreamDemo3 {
   public static void main(String[] args) {
     Random r = new Random();
     IntStream intStream = r.ints();
-    intStream.forEach(System.out::println);
+    intStream.forEach(i -> System.out.println(x);
 
   }
 }
