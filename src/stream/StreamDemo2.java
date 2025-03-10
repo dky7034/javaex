85c218ba25c71dbbf8cd9caf9186d1dea599abea
artp <dky7034@naver.com>
2025-03-10 15:30:50 +0900

    Auto commit: Mon Mar 10 15:30:50 KST 2025

--- src/stream/StreamDemo2.java
+++ src/stream/StreamDemo2.java
@@ -10,6 +10,6 @@ public class StreamDemo2 {
     int[] arr = new int[]{1, 2, 3, 4, 5}; // 배열, 공간적 집합체
     IntStream intStream = Arrays.stream(arr);
 
-    List<Integer> ilist = new ArrayList<>(arr);
+    List<Integer> ilist = new ArrayList<>();
   }
 }
