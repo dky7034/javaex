f07a47a1e13dc580180ace2e35717a16ece5e416
artp <dky7034@naver.com>
2025-03-10 12:25:13 +0900

    Auto commit: Mon Mar 10 12:25:13 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -6,8 +6,9 @@ public class T2 {
   public static void main(String[] args) {
     Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
     for (String value : map.values()) {
-      map.replaceAll((s1, s2) -> s2.toUpperCase() );
+      map.replaceAll((s1, s2) -> s2.toUpperCase());
       System.out.println(value);
     }
+
   }
 }
