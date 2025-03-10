2e7ffdd13cb986092b20610fef6f0007dd846118
artp <dky7034@naver.com>
2025-03-10 09:48:12 +0900

    Auto commit: Mon Mar 10 09:48:12 KST 2025

--- src/collection_framework/MapDemo.java
+++ src/collection_framework/MapDemo.java
@@ -10,7 +10,7 @@ public class MapDemo {
     } catch (IllegalArgumentException e) {
       System.out.println(e.getLocalizedMessage());
     } catch (NullPointerException e) {
-      
+      System.out.println("자료 생성 과정에서 오류가 발생했습니다.");
     }
     System.out.println(fruits.size() + "종류의 과일이 있습니다.");
   }
