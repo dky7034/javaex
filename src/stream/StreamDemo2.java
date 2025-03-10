8d207f01d8d844a057b9e4111251f8a45a4dccf0
artp <dky7034@naver.com>
2025-03-10 15:36:17 +0900

    Auto commit: Mon Mar 10 15:36:17 KST 2025

--- src/stream/StreamDemo2.java
+++ src/stream/StreamDemo2.java
@@ -8,11 +8,13 @@ import java.util.stream.Stream;
 
 public class StreamDemo2 {
   public static void main(String[] args) {
-    int[] arr = new int[]{1, 2, 3, 4, 5}; // 배열, 공간적 집합체
+    int[] iarr = new int[]{1, 2, 3, 4, 5}; // 배열, 공간적 집합체
     IntStream stream = Arrays.stream(arr); // 스트림이라는 시간적 집합체
 
     double[] darr = {1.0, 2.0, 3.0, 4.0};
     DoubleStream stream3 = Arrays.stream(darr);
+
+    long[] larr
 
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5); // 리스트, 공간적 집합체
     Stream<Integer> stream1 = list.stream(); // 스트림이라는 시간적 집합체
