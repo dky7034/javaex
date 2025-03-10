ae3a53c11e6654344c703ae221f91ba199a0d120
artp <dky7034@naver.com>
2025-03-10 21:51:40 +0900

    Auto commit: Mon Mar 10 21:51:40 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -4,11 +4,12 @@ public class T0 extends T0class {
   public static void main(String[] args) {
     T0class t0class = new T0class();
     boolean b = t0class.isLongEnough("String");
+    System.out.println("b = " + b);
 
     T0 t0 = new T0();
     boolean b1 = t0.isLongEnough("Integer");
-    System.out.println(b);
-
+    System.out.println("b1 = " + b1);
+    
   }
 }
 
