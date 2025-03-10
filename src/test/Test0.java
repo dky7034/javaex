49b1543bac0353e7e6f5e96603b626b9d4452e11
artp <dky7034@naver.com>
2025-03-10 22:19:46 +0900

    Auto commit: Mon Mar 10 22:19:46 KST 2025

--- src/test/Test0.java
+++ src/test/Test0.java
@@ -1,4 +1,55 @@
 package test;
 
 public class Test0 {
+  public static void main(String[] args) {
+    Test0class Test0class = new Test0class();
+    boolean b = Test0class.isLongEnough("String");
+    System.out.println("b = " + b); // b = true
+
+    AbstractClass abstractClass = new AbstractClass() {
+      @Override
+      void abstractMethod1() {
+        System.out.println("abstractMethod1");
+      }
+      @Override
+      void abstractMethod2() {
+        System.out.println("abstractMethod2");
+      }
+    };
+    abstractClass.abstractMethod1(); // abstractMethod1
+    abstractClass.abstractMethod2(); // abstractMethod2
+
+    Test0Class test0Class = new Test0Class() {
+      @Override
+      void test0Method1() {
+        System.out.println("test0Method1Override");
+      }
+    };
+    test0Class.test0Method1(); // test0Method1Override
+    test0Class.test0Method2(); // ""
+  }
+}
+
+@FunctionalInterface
+interface Test0interface {
+  boolean isLongEnough(String str);
+}
+
+class Test0class implements Test0interface {
+  @Override
+  public boolean isLongEnough(String str) {
+    return str.length() >= 5;
+  }
+}
+
+abstract class AbstractClass {
+  abstract void abstractMethod1();
+  abstract void abstractMethod2();
+  void method1() {}
+}
+
+class Test0Class {
+  static void staticTest0Method() {}
+  void test0Method1() {}
+  void test0Method2() {}
 }
