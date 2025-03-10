6e5a573a3a220e4020c1cbbf45dc3e8d92bc2bf8
artp <dky7034@naver.com>
2025-03-10 12:21:44 +0900

    Auto commit: Mon Mar 10 12:21:44 KST 2025

--- src/collection_framework/T2.java
+++ src/collection_framework/T2.java
@@ -1,14 +1,16 @@
 package collection_framework;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class T2 {
   public static void main(String[] args) {
     Map<String, String> map = Map.of("호랑이", "tiger", "표범", "leopard", "사자", "lion");
     for (String value : map.values()) {
+      Collections.replaceAll(map, value, value.toUpperCase());
       System.out.println(value);
     }
 //    Collections.replaceAll();
-    
+
   }
 }
