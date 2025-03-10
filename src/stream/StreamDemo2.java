37dfa4e627862eebabaaa6ebde8729276f5ca7b0
artp <dky7034@naver.com>
2025-03-10 15:31:11 +0900

    Auto commit: Mon Mar 10 15:31:11 KST 2025

--- src/stream/StreamDemo2.java
+++ src/stream/StreamDemo2.java
@@ -1,7 +1,6 @@
 package stream;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class StreamDemo2 {
@@ -9,7 +8,6 @@ public class StreamDemo2 {
     int[] arr = new int[]{1, 2, 3, 4, 5}; // 배열, 공간적 집합체
     IntStream intStream = Arrays.stream(arr);
 
-    List<Integer> ilist = Arrays.asList();
-
+    
   }
 }
