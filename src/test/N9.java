7dc9a53698d7c7a5f80b78f755fc5095fcbc5157
artp <dky7034@naver.com>
2025-03-10 15:44:50 +0900

    Auto commit: Mon Mar 10 15:44:50 KST 2025

--- src/test/N9.java
+++ src/test/N9.java
@@ -16,6 +16,7 @@ public class N9 {
     int totalScore = 0; // 점수 합계를 저장하는 변수
 
     Set<Map.Entry<String, Integer>> entries = map.entrySet();
+    System.out.println(entries);
 
     // 여기에 코드를 작성하세요.
     for (Integer i : map.values()) {
