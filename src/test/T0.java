f2d513a01f9ab86bd66626f702724ee677f03b42
artp <dky7034@naver.com>
2025-03-10 21:42:51 +0900

    Auto commit: Mon Mar 10 21:42:51 KST 2025

--- src/test/T0.java
+++ src/test/T0.java
@@ -2,7 +2,12 @@ package test;
 
 public class T0 {
   public static void main(String[] args) {
-    isLong
+    T0interface t0interface = new T0interface() {
+      @Override
+      public boolean isLongEnough(String str) {
+        return false;
+      }
+    };
     System.out.println();
 
   }
