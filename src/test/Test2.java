e31e1e6c0411665edb11cdb0513e7af29fd17955
artp <dky7034@naver.com>
2025-03-11 17:04:22 +0900

    Auto commit: Tue Mar 11 17:04:22 KST 2025

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class Test2 {
   public static void main(String[] args) {
     Stream.of("a1", "a2", "a3")
-        .map(s -> s.substring(1)) // 모든 요소를 인덱스 1번부터 출력 (1, 2, 3)
+        .map(s -> s.substring(1)) // 모든 요소를 인덱스 1번부터 출력 - 1, 2, 3
         .mapToInt(Integer::parseInt) // Integer 로 형변환
         .max() // 최댓값 거르기
         .ifPresent(System.out::println); // 3
