3017a7c142bfc4056b9d15f17d6dda7ba1b4404f
artp <dky7034@naver.com>
2025-03-11 10:47:18 +0900

    Auto commit: Tue Mar 11 10:47:18 KST 2025

--- src/stream/OptionalDemo.java
+++ src/stream/OptionalDemo.java
@@ -15,6 +15,7 @@ public class OptionalDemo {
       System.out.println(divide);
     }
 
+    System.out.println("ifPresentOrElse 실행");
     divide.ifPresentOrElse(
         n -> System.out.println(divide),
         () -> System.out.println("0으로 나눌 수 없습니다.")
