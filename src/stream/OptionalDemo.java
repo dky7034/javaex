3b29b982216c7dace159c8219020fe437d940db7
artp <dky7034@naver.com>
2025-03-11 10:26:18 +0900

    Auto commit: Tue Mar 11 10:26:18 KST 2025

--- src/stream/OptionalDemo.java
+++ src/stream/OptionalDemo.java
@@ -4,11 +4,12 @@ import java.util.OptionalDouble;
 
 public class OptionalDemo {
   public static void main(String[] args) {
-
+    System.out.println(divide(3.0, 0.0));
   }
 
   public static OptionalDouble divide(double x, double y) {
-    OptionalDouble od = double x /double y;
+    OptionalDouble od =
+
     return
   }
 
