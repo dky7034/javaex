1f83c1b1e51a01829270041a423a784458ba5a6b
artp <dky7034@naver.com>
2025-03-11 12:03:32 +0900

    Auto commit: Tue Mar 11 12:03:32 KST 2025

--- src/stream/DistinctDemo2.java
+++ src/stream/DistinctDemo2.java
@@ -5,7 +5,12 @@ import java.util.stream.Stream;
 
 public class DistinctDemo2 {
   public static void main(String[] args) {
-    Stream<Circle> cs = Stream.of(new Circle(3), new Circle(5), new Circle(3));
+    Stream<Circle> cs = Stream.of(
+        new Circle(3),
+        new Circle(5),
+        new Circle(3),
+        new Circle(4),
+        new Circle(8));
     cs.distinct().forEach(circle -> System.out.println(circle));
   }
 }
