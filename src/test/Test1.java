37a76788f17e3abbbcd2c9239c08418789639222
artp <dky7034@naver.com>
2025-03-11 11:00:03 +0900

    Auto commit: Tue Mar 11 11:00:03 KST 2025

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -1,9 +1,11 @@
 package test;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Test1 {
   public static void main(String[] args) {
     List<Integer> scores = List.of(45, 76, 38, 27, 50);
+    Stream<Integer> si = scores.stream();
   }
 }
