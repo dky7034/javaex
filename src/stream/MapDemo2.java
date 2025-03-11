579866930d3f252ab99357669dc467920732db7a
artp <dky7034@naver.com>
2025-03-11 14:30:03 +0900

    Auto commit: Tue Mar 11 14:30:03 KST 2025

--- src/stream/MapDemo2.java
+++ src/stream/MapDemo2.java
@@ -20,8 +20,7 @@ public class MapDemo2 {
     System.out.println();
 
     // 문제 2.
-    // 국가정보를 리스트로 얻어오기
-    // 리스트를 스트림으로 변환하기
+    // 국가정보 리스트를 스트림으로 변환하기
     // GDP 순위만을 number 스트림으로 map 시키기
     // 전 국가의 GDP 순위를 출력하기
     Nation.nations.stream()
