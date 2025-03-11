fe837c6fce4ff082638913ba01bcbc1021ac49c7
artp <dky7034@naver.com>
2025-03-11 10:14:02 +0900

    Auto commit: Tue Mar 11 10:14:02 KST 2025

--- src/stream/LazyDemo3.java
+++ src/stream/LazyDemo3.java
@@ -8,12 +8,13 @@ public class LazyDemo3 {
     IntStream intStream = IntStream.generate(() -> new Random().nextInt(10)); // Supplier: 받는 것 없이(매개변수 없이) 주기만 함(리턴만 함) - 제공자
 
     intStream.filter(i -> {
-      System.out.println("filter: " + i);
-      return i % 2 == 0;
-    }).map(x -> {
-      System.out.println("map: " + x);
-      return x * x;
-    }).forEach(j -> System.out.println("forEach: " + j));
+          System.out.println("filter: " + i);
+          return i % 2 == 0;
+        }).limit(3)
+        .map(x -> {
+          System.out.println("map: " + x);
+          return x * x;
+        }).forEach(j -> System.out.println("forEach: " + j));
 
   }
 }
