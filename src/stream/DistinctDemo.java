9f8071fe6076462b203e7406b8efc996d70bae06
artp <dky7034@naver.com>
2025-03-11 11:30:09 +0900

    Auto commit: Tue Mar 11 11:30:09 KST 2025

--- src/stream/DistinctDemo.java
+++ src/stream/DistinctDemo.java
@@ -1,7 +1,9 @@
 package stream;
 
+import java.util.stream.IntStream;
+
 public class DistinctDemo {
   public static void main(String[] args) {
-    
+    IntStream is = IntStream.of(1, 2, 1, 3, 3, 2, 2, 4, 4);
   }
 }
