f76d3fba211cf61841ae0149993a30fc8fa7bb27
artp <dky7034@naver.com>
2025-03-11 16:09:58 +0900

    Auto commit: Tue Mar 11 16:09:58 KST 2025

--- src/stream/MatchDemo.java
+++ src/stream/MatchDemo.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class MatchDemo {
   public static void main(String[] args) {
     boolean b1 = Stream.of("a1", "b1", "c2").anyMatch(x -> x.startsWith("c"));
-    System.out.println("b1 = " + b1);
+    System.out.println("b1 = " + b1); // false
 
     System.out.println(Stream.of(2, 4, 6, 8, 10).allMatch(i -> i % 2 == 0)); // true
 
