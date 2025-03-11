090807d9f8255d3ab1debe5ec39932fc03c5a289
artp <dky7034@naver.com>
2025-03-11 16:40:33 +0900

    Auto commit: Tue Mar 11 16:40:33 KST 2025

--- src/stream/LoopDemo.java
+++ src/stream/LoopDemo.java
@@ -1,11 +1,14 @@
 package stream;
 
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class LoopDemo {
   public static void main(String[] args) {
     Stream<Nation> peek = Nation.nations.stream()
         .peek(Util::printWithParenthesis);
+
+    peek.max(Comparator.comparing(nation -> nation.getPopulation()))
 
   }
 }
