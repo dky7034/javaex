3023b7f893f72ba1b0f25ef7dc8cc0a484ddd7c5
artp <dky7034@naver.com>
2025-03-11 12:40:45 +0900

    Auto commit: Tue Mar 11 12:40:45 KST 2025

--- src/stream/MapDemo.java
+++ src/stream/MapDemo.java
@@ -9,7 +9,7 @@ public class MapDemo {
     // 길이를 구해서 숫자로 변환
     s2.forEach(Util::print);
     // 중간을 잘라내서 문자열로 변환 (앞자리 하나만 substring 을 이용해서 잘라내기)
-    s1.map(s -> s.substring(0))
+    s1.map(s -> s.substring(0)).forEach(Util::print);
 
   }
 }
