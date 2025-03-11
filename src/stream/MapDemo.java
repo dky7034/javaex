a84e2d1fa9075009ef74d8eaebd0323566e87e99
artp <dky7034@naver.com>
2025-03-11 12:29:49 +0900

    Auto commit: Tue Mar 11 12:29:49 KST 2025

--- src/stream/MapDemo.java
+++ src/stream/MapDemo.java
@@ -6,6 +6,7 @@ public class MapDemo {
   public static void main(String[] args) {
     Stream<String> s1 = Stream.of("abc", "b", "cd", "d", "e");
     Stream<Integer> s2 = s1.map(s -> s.length());
+    s2.distinct().forEach(Util.print(););
 
   }
 }
