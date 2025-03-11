7382b46037a2a27c935543c07259a2c99c48da4c
artp <dky7034@naver.com>
2025-03-11 15:48:04 +0900

    Auto commit: Tue Mar 11 15:48:04 KST 2025

--- src/stream/SortedDemo.java
+++ src/stream/SortedDemo.java
@@ -36,7 +36,7 @@ public class SortedDemo {
     System.out.println();
     System.out.println("인구수 정렬: 내림차순");
     list3.stream()
-        .sorted(Comparator.comparing(n -> n.getPopulation).reversed())
+        .sorted(Comparator.comparing(n -> n.getPopulation))
         .forEach(Util::printWithParenthesis);
 
     list3.stream()
