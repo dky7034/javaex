7e974aa73ed76742f2a2e398e54bf80896db40c8
artp <dky7034@naver.com>
2025-03-11 16:18:44 +0900

    Auto commit: Tue Mar 11 16:18:44 KST 2025

--- src/stream/MatchDemo.java
+++ src/stream/MatchDemo.java
@@ -16,5 +16,6 @@ public class MatchDemo {
     System.out.println(nations.allMatch(nation -> nation.getPopulation() > 100)); // false
 
     Optional<Nation> first = nations.findFirst();
+    first.ifPresentOrElse(nation -> , () -> {});
   }
 }
