5f3fa9f63731cafa95d81c4e1d07d655888385f8
artp <dky7034@naver.com>
2025-03-11 16:16:26 +0900

    Auto commit: Tue Mar 11 16:16:26 KST 2025

--- src/stream/MatchDemo.java
+++ src/stream/MatchDemo.java
@@ -1,5 +1,6 @@
 package stream;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class MatchDemo {
@@ -14,6 +15,6 @@ public class MatchDemo {
     Stream<Nation> nations = Nation.nations.stream();
     System.out.println(nations.allMatch(nation -> nation.getPopulation() > 100)); // false
 
-    nations.map(n -> n.getPopulation()).findFirst();
+    Optional<Double> optional = nations.map(n -> n.getPopulation()).findFirst();
   }
 }
