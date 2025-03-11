3ce58f1a8fc81a98f102d8b966e50f6b8f336ef3
artp <dky7034@naver.com>
2025-03-11 14:39:52 +0900

    Auto commit: Tue Mar 11 14:39:52 KST 2025

--- src/stream/MapDemo3.java
+++ src/stream/MapDemo3.java
@@ -3,6 +3,7 @@ package stream;
 public class MapDemo3 {
   public static void main(String[] args) {
     String s = "hello java";
-    String s2 = "hi, my name is"
+    String s2 = "hi, my name is";
+    s.split("");
   }
 }
