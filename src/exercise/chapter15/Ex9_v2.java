380b0bbecdbc6c42bae87676b619177b7952d69d
artp <dky7034@naver.com>
2025-03-11 09:11:22 +0900

    Auto commit: Tue Mar 11 09:11:22 KST 2025

--- src/exercise/chapter15/Ex9_v2.java
+++ src/exercise/chapter15/Ex9_v2.java
@@ -3,7 +3,7 @@ package exercise.chapter15;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Ex9 {
+public class Ex9_v2 {
   public static void main(String[] args) {
     Map<String, Integer> map = new HashMap<>();
     map.put("blue", 96);
@@ -17,12 +17,12 @@ public class Ex9 {
     // 에러 발생
     // entrySet() 은 Set 타입 컬렉션을 반환하는데, 이를 Map.Entry<> 타입으로 변환하는
     // 타입 캐스팅이 불가능하기 때문에 에러가 발생합니다.
-    System.out.println("--- 출력 Test ---");
-    Map.Entry<String, Integer> entries = (Map.Entry<String, Integer>)map.entrySet();
-    String key = entries.getKey();
-    Integer value = entries.getValue();
-    System.out.println("key = " + key);
-    System.out.println("value = " + value);
+//    System.out.println("--- 출력 Test ---");
+//    Map.Entry<String, Integer> entries = (Map.Entry<String, Integer>)map.entrySet();
+//    String key = entries.getKey();
+//    Integer value = entries.getValue();
+//    System.out.println("key = " + key);
+//    System.out.println("value = " + value);
 
     // 여기에 코드를 작성하세요.
     // entrySet() 사용
