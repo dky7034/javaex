eca0d78ceaf7f716a5009bc9fb196db2e3c7ff4e
artp <dky7034@naver.com>
2025-03-11 12:25:53 +0900

    Auto commit: Tue Mar 11 12:25:53 KST 2025

--- src/stream/MapDemo.java
+++ src/stream/MapDemo.java
@@ -1,7 +1,9 @@
 package stream;
 
+import java.util.stream.Stream;
+
 public class MapDemo {
   public static void main(String[] args) {
-    
+    Stream<String> s1 = Stream.of("a", "b", "c", "d", "e");
   }
 }
