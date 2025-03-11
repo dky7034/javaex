946078253fc41cbedc3c4169280855582ad1d010
artp <dky7034@naver.com>
2025-03-11 14:41:25 +0900

    Auto commit: Tue Mar 11 14:41:25 KST 2025

--- src/stream/MapDemo3.java
+++ src/stream/MapDemo3.java
@@ -6,7 +6,7 @@ public class MapDemo3 {
     String s2 = "hi, my name is";
     String[] s1 = s.split("");
     String[] s3 = s2.split("");
-    System.out.println(s);
+    System.out.println(s1.length);
 
   }
 }
