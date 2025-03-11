fb08bb5109efcf1654eb121c2003b3eefb2dd901
artp <dky7034@naver.com>
2025-03-11 11:21:33 +0900

    Auto commit: Tue Mar 11 11:21:33 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -5,6 +5,6 @@ import java.util.stream.Stream;
 public class FilterDemo {
   public static void main(String[] args) {
     Stream<String> s1 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
-    s1.filter(s -> s.startsWith("c")).forEach(System.out::println);
+    s1.filter(s -> s.startsWith("c")).forEach();
   }
 }
