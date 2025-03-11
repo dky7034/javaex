40ed290db15c6bcdb8b8e63d461ad6b3744d1da1
artp <dky7034@naver.com>
2025-03-11 14:22:33 +0900

    Auto commit: Tue Mar 11 14:22:33 KST 2025

--- src/stream/MapDemo2.java
+++ src/stream/MapDemo2.java
@@ -1,13 +1,14 @@
 package stream;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class MapDemo2 {
   public static void main(String[] args) {
     // 문제 1.
     // 국가정보를 리스트로 얻어오기
-    List<Nation> nations = Nation.nations;
-    
+    Stream<List<Nation>> nations = Nation.nations;
+
     // 리스트를 스트림으로 변환하기
     // 국가의 이름만을 스트림으로 map 시키기
     // 4개의 국가의 이름만 출력하기
