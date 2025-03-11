58fa2a02160fa0b9e0b5f31f919d92b7bd6b199e
artp <dky7034@naver.com>
2025-03-11 11:55:20 +0900

    Auto commit: Tue Mar 11 11:55:20 KST 2025

--- src/stream/DistinctDemo2.java
+++ src/stream/DistinctDemo2.java
@@ -25,10 +25,9 @@ class Circle {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Circle circle = (Circle) o;
-    return this.r == circle.r;
+    return r == circle.r;
   }
 
   @Override
