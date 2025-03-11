cc7b4ea2046303b95609082a0f3e351bb68c1834
artp <dky7034@naver.com>
2025-03-11 11:45:55 +0900

    Auto commit: Tue Mar 11 11:45:55 KST 2025

--- src/stream/DistinctDemo2.java
+++ src/stream/DistinctDemo2.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class DistinctDemo2 {
   public static void main(String[] args) {
-    Stream<Circle> cs = Stream.of(new Cir)
+    Stream<Circle> cs = Stream.of(new Circle(3), new Circle(5), new Circle(3));
   }
 }
 
