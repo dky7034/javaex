c4aa05f7cfc6ba246db24497b2d7fcd5aa9a0526
artp <dky7034@naver.com>
2025-03-11 10:41:05 +0900

    Auto commit: Tue Mar 11 10:41:05 KST 2025

--- src/stream/OptionalDemo.java
+++ src/stream/OptionalDemo.java
@@ -17,7 +17,9 @@ public class OptionalDemo {
 
     divide.ifPresentOrElse(
         n -> System.out.println(divide),
-        () -> System.out.println("0으로 나눌 수 없습니다."));
+        () -> System.out.println("0으로 나눌 수 없습니다.")
+    );
+    
     }
 
     public static OptionalDouble divide ( double x, double y){
