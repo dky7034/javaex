025e5b4dae0ba0cf8dd7f2069356dc80071545c3
artp <dky7034@naver.com>
2025-03-11 11:17:25 +0900

    Auto commit: Tue Mar 11 11:17:25 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -4,6 +4,6 @@ import java.util.stream.Stream;
 
 public class FilterDemo {
   public static void main(String[] args) {
-    Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
+    Stream<String> s1 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
   }
 }
