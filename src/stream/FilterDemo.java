68071551f664ccba1ce65c0a81f4aea9cbff656b
artp <dky7034@naver.com>
2025-03-11 11:17:15 +0900

    Auto commit: Tue Mar 11 11:17:15 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -1,4 +1,9 @@
 package stream;
 
+import java.util.stream.Stream;
+
 public class FilterDemo {
+  public static void main(String[] args) {
+    Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
+  }
 }
