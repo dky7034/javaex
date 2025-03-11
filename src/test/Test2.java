89487fa74c4eb1437acdf6ebb304f3b67b772212
artp <dky7034@naver.com>
2025-03-11 17:02:27 +0900

    Auto commit: Tue Mar 11 17:02:27 KST 2025

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -1,4 +1,11 @@
 package test;
 
+import java.util.stream.Stream;
+
 public class Test2 {
+  public static void main(String[] args) {
+    Stream.of("a1", "a2", "a3")
+        .map(s -> s.substring(1))
+        .mapToInt(Integer::parseInt)
+  }
 }
