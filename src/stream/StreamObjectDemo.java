125e37b78434a9d0b23d31dc4682265ca0fa7741
artp <dky7034@naver.com>
2025-03-11 12:10:39 +0900

    Auto commit: Tue Mar 11 12:10:39 KST 2025

--- src/stream/StreamObjectDemo.java
+++ src/stream/StreamObjectDemo.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class StreamObjectDemo {
   public static void main(String[] args) {
     List<Nation> nations = Nation.nations;
+    nations.stream();
     System.out.println(Nation.nations);
   }
 }
