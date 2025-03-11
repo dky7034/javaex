84d91934bd29a6fde42edd65bc2f8b46500e90b7
artp <dky7034@naver.com>
2025-03-11 11:18:21 +0900

    Auto commit: Tue Mar 11 11:18:21 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -5,5 +5,6 @@ import java.util.stream.Stream;
 public class FilterDemo {
   public static void main(String[] args) {
     Stream<String> s1 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
+    s1.filter().skip().forEach();
   }
 }
