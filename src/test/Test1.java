6588110632293ecb5ada0fe3900426d1b5811f0d
artp <dky7034@naver.com>
2025-03-11 11:02:41 +0900

    Auto commit: Tue Mar 11 11:02:41 KST 2025

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -7,7 +7,6 @@ public class Test1 {
   public static void main(String[] args) {
     List<Integer> scores = List.of(45, 76, 38, 27, 50);
     Stream<Integer> si = scores.stream();
-    si.map((x) -> x + 10)
-        .forEach(System.out::println);
+    si.map((x) -> x + 10).forEach(System.out::println);
   }
 }
