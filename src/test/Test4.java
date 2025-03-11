fffe7f07e0933b391de961ffb553d523c1a20b11
artp <dky7034@naver.com>
2025-03-11 17:14:37 +0900

    Auto commit: Tue Mar 11 17:14:37 KST 2025

--- src/test/Test4.java
+++ src/test/Test4.java
@@ -6,7 +6,9 @@ import java.util.stream.Stream;
 
 public class Test4 {
   public static void main(String[] args) {
-    List<String> list = new ArrayList<>()
+    List<String> list = new ArrayList<>();
+
+
     Stream.of("서울", "워싱턴", "베이징", "파리", "마드리드", "런던")
 
   }
