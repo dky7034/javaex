7dd246837130dd5906131434850cbe25ae1800c3
artp <dky7034@naver.com>
2025-03-11 11:24:44 +0900

    Auto commit: Tue Mar 11 11:24:44 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -7,6 +7,8 @@ public class FilterDemo {
     Stream<String> s1 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
     s1.filter(s -> s.startsWith("c")).forEach(s -> Util.print(s));
 
+    System.out.println("--- 한 줄 띄우기 ---");
+
     Stream<String> s2 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
     s2.filter(s -> s.startsWith("c")).forEach(Util::printWithParenthesis);
   }
