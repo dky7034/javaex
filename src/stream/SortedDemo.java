0708bbdc082ecdc89b3d3866f89a09cf77fb85fd
artp <dky7034@naver.com>
2025-03-11 15:14:49 +0900

    Auto commit: Tue Mar 11 15:14:49 KST 2025

--- src/stream/SortedDemo.java
+++ src/stream/SortedDemo.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class SortedDemo {
   public static void main(String[] args) {
-    List<String>
+    List<String> list1 = List.of("a1", "c2", "b3", "b2", "a4");
   }
 }
