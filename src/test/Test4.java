ad259e6033b0943ed39ebfae5892174526b3581a
artp <dky7034@naver.com>
2025-03-11 17:18:45 +0900

    Auto commit: Tue Mar 11 17:18:45 KST 2025

--- src/test/Test4.java
+++ src/test/Test4.java
@@ -6,13 +6,7 @@ import java.util.stream.Stream;
 
 public class Test4 {
   public static void main(String[] args) {
-    List<String> list = new ArrayList<>();
-    list.add("서울");
-    list.add("워싱턴");
-    list.add("베이징");
-    list.add("파리");
-    list.add("마드리드");
-    list.add("런던");
+    List<String> capitals = List.of("서울", "워싱턴", "베이징", "파리", "마드리드", "런던");
     System.out.println("list = " + list);
     Stream<String> listStream = list.stream();
     listStream.sorted().findFirst().ifPresentOrElse(s -> System.out.println(s), () -> System.out.println("찾는 값이 없습니다."));
