378e937315800809aa18afd58d53522daa06df48
artp <dky7034@naver.com>
2025-03-11 16:36:47 +0900

    Auto commit: Tue Mar 11 16:36:47 KST 2025

--- src/stream/LoopDemo.java
+++ src/stream/LoopDemo.java
@@ -1,7 +1,11 @@
 package stream;
 
+import java.util.stream.Stream;
+
 public class LoopDemo {
   public static void main(String[] args) {
-    
+    Stream<Nation> peek = Nation.nations.stream().
+        peek(nation -> System.out.println(nation.getName()))
+        .forEach(Util::printWithParenthesis);
   }
 }
