315f03e5af180a357301c6b5b833cc49c0a2c9e7
artp <dky7034@naver.com>
2025-03-11 17:10:51 +0900

    Auto commit: Tue Mar 11 17:10:51 KST 2025

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -5,5 +5,7 @@ import java.util.stream.Stream;
 public class Test3 {
   public static void main(String[] args) {
     Stream.of("갈매기", "나비", "다람쥐", "라마")
+        .filter(s -> s.length() == 2)
+        .forEach(System.out::println);
   }
 }
