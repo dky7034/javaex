19f0235be7741cd6de59b80f16e52a956bf76bd2
artp <dky7034@naver.com>
2025-03-11 11:23:20 +0900

    Auto commit: Tue Mar 11 11:23:20 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -6,5 +6,8 @@ public class FilterDemo {
   public static void main(String[] args) {
     Stream<String> s1 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
     s1.filter(s -> s.startsWith("c")).forEach(s -> Util.print(s));
+
+    Stream<String> s2 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
+    s2.filter(s -> )
   }
 }
