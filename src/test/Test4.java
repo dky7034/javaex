1a607889a51207cfed1b5c83077bd4f60f395d35
artp <dky7034@naver.com>
2025-03-11 17:14:03 +0900

    Auto commit: Tue Mar 11 17:14:03 KST 2025

--- src/test/Test4.java
+++ src/test/Test4.java
@@ -1,9 +1,13 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Test4 {
   public static void main(String[] args) {
+    List<String> list = new ArrayList<>()
     Stream.of("서울", "워싱턴", "베이징", "파리", "마드리드", "런던")
+
   }
 }
