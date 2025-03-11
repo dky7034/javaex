aa0d8be643e8c94f37190b3533de328a164e84c2
artp <dky7034@naver.com>
2025-03-11 12:41:44 +0900

    Auto commit: Tue Mar 11 12:41:44 KST 2025

--- src/stream/MapDemo.java
+++ src/stream/MapDemo.java
@@ -8,6 +8,10 @@ public class MapDemo {
     // 길이를 구해서 숫자로 변환
     Stream<Integer> s2 = s1.map(s -> s.length());
     s2.forEach(Util::print);
+
+    // 한 줄 띄우기
+    System.out.println();
+
     // 중간을 잘라내서 문자열로 변환 (앞자리 하나만 substring 을 이용해서 잘라내기)
     Stream<String> s3 = Stream.of("abc", "b", "cd", "d", "e");
     s3.map(s -> s.substring(0)).forEach(Util::print);
