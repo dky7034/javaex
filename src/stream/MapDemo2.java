6382e2e73a9f06262635dc868b32e6915b68efac
artp <dky7034@naver.com>
2025-03-11 14:24:35 +0900

    Auto commit: Tue Mar 11 14:24:35 KST 2025

--- src/stream/MapDemo2.java
+++ src/stream/MapDemo2.java
@@ -11,9 +11,9 @@ public class MapDemo2 {
     // 리스트를 스트림으로 변환하기
     Stream<Nation> s1 = nations.stream();
     // 국가의 이름만을 스트림으로 map 시키기
+    // 4개의 국가의 이름만 출력하기
     s1.map(n -> n.getName())
 
-    // 4개의 국가의 이름만 출력하기
 
     // 문제 2.
     // 국가정보를 리스트로 얻어오기
