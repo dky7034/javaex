9428d26a963ec116e6ba2ca6f72653ac87d0c38a
artp <dky7034@naver.com>
2025-03-11 17:19:01 +0900

    Auto commit: Tue Mar 11 17:19:01 KST 2025

--- src/test/Test4.java
+++ src/test/Test4.java
@@ -8,7 +8,7 @@ public class Test4 {
   public static void main(String[] args) {
     List<String> capitals = List.of("서울", "워싱턴", "베이징", "파리", "마드리드", "런던");
     Stream<String> stream = list.stream();
-    listStream.sorted().findFirst().ifPresentOrElse(s -> System.out.println(s), () -> System.out.println("찾는 값이 없습니다."));
+    stream.sorted().findFirst().ifPresentOrElse(s -> System.out.println(s), () -> System.out.println("찾는 값이 없습니다."));
 
   }
 }
