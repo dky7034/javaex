65d0207549f1b5be03ec6d865d0362a8312c6a11
artp <dky7034@naver.com>
2025-03-11 14:20:55 +0900

    Auto commit: Tue Mar 11 14:20:55 KST 2025

--- src/stream/MapDemo2.java
+++ src/stream/MapDemo2.java
@@ -2,6 +2,11 @@ package stream;
 
 public class MapDemo2 {
   public static void main(String[] args) {
-    
+    // 문제 1.
+    // 국가정보를 리스트로 얻어오기
+
+    // 리스트를 스트림으로 변환하기
+    // 국가의 이름만을 스트림으로 map 시키기
+    // 4개의 국가의 이름만 출력하기
   }
 }
