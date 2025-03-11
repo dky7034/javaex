68b6f65c62bc503110c9ce3307db8fdeb3302ae5
artp <dky7034@naver.com>
2025-03-11 17:13:40 +0900

    Auto commit: Tue Mar 11 17:13:40 KST 2025

--- src/test/Test4.java
+++ src/test/Test4.java
@@ -1,7 +1,9 @@
 package test;
 
+import java.util.stream.Stream;
+
 public class Test4 {
   public static void main(String[] args) {
-    
+    Stream.of("서울", "워싱턴", "베이징", "파리", "마드리드", "런던")
   }
 }
