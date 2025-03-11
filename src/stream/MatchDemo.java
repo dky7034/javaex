0b8fbd77ed0acf6643ed9acdf8e185571a18009c
artp <dky7034@naver.com>
2025-03-11 16:08:58 +0900

    Auto commit: Tue Mar 11 16:08:58 KST 2025

--- src/stream/MatchDemo.java
+++ src/stream/MatchDemo.java
@@ -5,6 +5,8 @@ import java.util.stream.Stream;
 public class MatchDemo {
   public static void main(String[] args) {
     boolean b1 = Stream.of("a1", "b1", "c2").anyMatch(x -> x.startsWith("c"));
+    System.out.println("b1 = " + b1);
+    
 
   }
 }
