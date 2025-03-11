b05f122be28d1d5b98315a12bb328082920e0d50
artp <dky7034@naver.com>
2025-03-11 11:19:14 +0900

    Auto commit: Tue Mar 11 11:19:14 KST 2025

--- src/stream/FilterDemo.java
+++ src/stream/FilterDemo.java
@@ -5,6 +5,6 @@ import java.util.stream.Stream;
 public class FilterDemo {
   public static void main(String[] args) {
     Stream<String> s1 = Stream.of("a1", "b1", "b2", "c1", "c2", "c3");
-    s1.filter(s -> s.startsWith("c")).skip().forEach();
+    s1.filter(s -> s.startsWith("c")).skip(1).forEach();
   }
 }
