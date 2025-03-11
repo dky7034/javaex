016f4abb78eb671f27e166289f864b030a7c3c24
artp <dky7034@naver.com>
2025-03-11 15:12:46 +0900

    Auto commit: Tue Mar 11 15:12:46 KST 2025

--- src/stream/MapDemo4.java
+++ src/stream/MapDemo4.java
@@ -4,6 +4,6 @@ import java.util.stream.Stream;
 
 public class MapDemo4 {
   public static void main(String[] args) {
-    Stream<Nation> nations = Nation.nations;
+    Stream<Nation> nation = Nation.nations;
   }
 }
