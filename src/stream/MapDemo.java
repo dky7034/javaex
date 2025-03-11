30888f093f3a9089c46aba173ba88c8476f5c903
artp <dky7034@naver.com>
2025-03-11 12:29:04 +0900

    Auto commit: Tue Mar 11 12:29:04 KST 2025

--- src/stream/MapDemo.java
+++ src/stream/MapDemo.java
@@ -1,10 +1,11 @@
 package stream;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MapDemo {
   public static void main(String[] args) {
     Stream<String> s1 = Stream.of("abc", "b", "cd", "d", "e");
-    s1
+    IntStream is = s1.map(s -> s.length());
   }
 }
