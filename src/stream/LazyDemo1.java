5beab4767fd1b5d2d8a4d0fcb8120ed79c7198d6
artp <dky7034@naver.com>
2025-03-11 10:03:09 +0900

    Auto commit: Tue Mar 11 10:03:09 KST 2025

--- src/stream/LazyDemo1.java
+++ src/stream/LazyDemo1.java
@@ -1,7 +1,9 @@
 package stream;
 
+import java.util.stream.IntStream;
+
 public class LazyDemo1 {
   public static void main(String[] args) {
-    
+    IntStream intStream = IntStream.rangeClosed(1, 5);
   }
 }
