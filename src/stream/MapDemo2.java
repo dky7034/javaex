ba1f582b76fe4d8717d44ff026ed0d2bcbf159f9
artp <dky7034@naver.com>
2025-03-11 14:22:19 +0900

    Auto commit: Tue Mar 11 14:22:19 KST 2025

--- src/stream/MapDemo2.java
+++ src/stream/MapDemo2.java
@@ -7,6 +7,7 @@ public class MapDemo2 {
     // 문제 1.
     // 국가정보를 리스트로 얻어오기
     List<Nation> nations = Nation.nations;
+    
     // 리스트를 스트림으로 변환하기
     // 국가의 이름만을 스트림으로 map 시키기
     // 4개의 국가의 이름만 출력하기
