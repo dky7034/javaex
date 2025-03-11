fec484063c82c35635f3d8ebf44e50120e64ad7d
artp <dky7034@naver.com>
2025-03-11 16:16:57 +0900

    Auto commit: Tue Mar 11 16:16:57 KST 2025

--- src/stream/MatchDemo.java
+++ src/stream/MatchDemo.java
@@ -14,6 +14,6 @@ public class MatchDemo {
     Stream<Nation> nations = Nation.nations.stream();
     System.out.println(nations.allMatch(nation -> nation.getPopulation() > 100)); // false
 
-    nations.map(n -> n.getPopulation()).findFirst();
+    nations.findFirst();
   }
 }
