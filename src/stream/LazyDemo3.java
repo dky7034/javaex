a4ecc43eb3bbb92dfae0a559ae80fd25b3a9dccd
artp <dky7034@naver.com>
2025-03-11 10:22:54 +0900

    Auto commit: Tue Mar 11 10:22:53 KST 2025

--- src/stream/LazyDemo3.java
+++ src/stream/LazyDemo3.java
@@ -7,7 +7,7 @@ public class LazyDemo3 {
   public static void main(String[] args) {
     IntStream intStream = IntStream.generate(() -> new Random().nextInt(10)); // Supplier: 받는 것 없이(매개변수 없이) 주기만 함(리턴만 함) - 제공자
 
-    intStream.filter(i -> { // Predicate: 
+    intStream.filter(i -> { // Predicate: 받은 것을(매개변수 있음) 조건으로 판단함(참 또는 거짓을 리턴) - 판단자
           System.out.println("filter: " + i);
           return i % 2 == 0;
         }).limit(3)
