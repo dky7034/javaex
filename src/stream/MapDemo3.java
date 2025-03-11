a9bbd8b0a27dc60014fda7cc4e5ef5ed6d7c521f
artp <dky7034@naver.com>
2025-03-11 14:45:47 +0900

    Auto commit: Tue Mar 11 14:45:47 KST 2025

--- src/stream/MapDemo3.java
+++ src/stream/MapDemo3.java
@@ -2,6 +2,7 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class MapDemo3 {
   public static void main(String[] args) {
@@ -16,8 +17,8 @@ public class MapDemo3 {
     System.out.println("s1.length = " + s1.length);
     System.out.println("s2.length() = " + s2.length());
     
-    List.of("안녕, 자바!", "잘 가", "C++!");
-    
+    List<String> list1 = List.of("안녕, 자바!", "잘 가", "C++!");
+    Stream<String> list2 = list
 
   }
 }
