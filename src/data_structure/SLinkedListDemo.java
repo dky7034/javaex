b6f70c045951cbfaf8224eed0ea8a2ed58534b87
artp <dky7034@naver.com>
2025-03-14 12:26:31 +0900

    Auto commit: Fri Mar 14 12:26:31 KST 2025

--- src/data_structure/SLinkedListDemo.java
+++ src/data_structure/SLinkedListDemo.java
@@ -1,8 +1,10 @@
 package data_structure;
 
+import java.util.LinkedList;
+
 public class SLinkedListDemo {
   public static void main(String[] args) {
-
-
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    
   }
 }
