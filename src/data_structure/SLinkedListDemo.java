2bab4524849b2ad126de086bbad81bcccb50fdf2
artp <dky7034@naver.com>
2025-03-14 12:41:08 +0900

    Auto commit: Fri Mar 14 12:41:08 KST 2025

--- src/data_structure/SLinkedListDemo.java
+++ src/data_structure/SLinkedListDemo.java
@@ -6,5 +6,8 @@ public class SLinkedListDemo {
   public static void main(String[] args) {
     LinkedList<Integer> linkedList = new LinkedList<>();
     System.out.println(linkedList.size());
+
+    SLinkedList<Integer> sLinkedList = new SLinkedList<>();
+    System.out.println(sLinkedList.size());
   }
 }
