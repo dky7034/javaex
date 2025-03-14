d3bf0f2ff29b38d723116a730fd7858f84787492
artp <dky7034@naver.com>
2025-03-14 09:55:36 +0900

    Auto commit: Fri Mar 14 09:55:36 KST 2025

--- src/data_structure/ArrListTest.java
+++ src/data_structure/ArrListTest.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class ArrListTest {
   public static void main(String[] args) {
-    int[] arr = new int[] {1,2,3,4,5};
-
+    int[] arr = new int[]{1, 2, 3, 4, 5};
+    
 
 
     List<Integer> arrList = new ArrayList<>();
