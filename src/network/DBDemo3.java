33f2f700f4216d79b1a6f09e347097073359387c
artp <dky7034@naver.com>
2025-03-14 09:15:53 +0900

    Auto commit: Fri Mar 14 09:15:53 KST 2025

--- src/network/DBDemo3.java
+++ src/network/DBDemo3.java
@@ -1,7 +1,12 @@
 package network;
 
+import java.sql.Connection;
+
 public class DBDemo3 {
   public static void main(String[] args) {
+
+  }
+  private static Connection makeConnection() {
     
   }
 }
