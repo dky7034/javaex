e05b2fa7d14afda17f5980681027aa805e8ba70c
artp <dky7034@naver.com>
2025-03-14 09:08:13 +0900

    Auto commit: Fri Mar 14 09:08:13 KST 2025

--- src/network/InetAddressDemo.java
+++ src/network/InetAddressDemo.java
@@ -2,23 +2,35 @@ package network;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InetAddressDemo {
   public static void main(String[] args) {
-    InetAddress address = null;
+    InetAddress addr = null;
+    InetAddress addr2 = null;
+    InetAddress[] addresses = null;
 
     Scanner in = new Scanner(System.in);
-    System.out.print("호스트 이름을 입력하세요: ");
+    System.out.print("호스트 이름을 입력하세요 : ");
     String url = in.nextLine();
 
     try {
-      address = InetAddress.getByName(url);
+      addr = InetAddress.getByName(url);
+      addr2 = InetAddress.getLocalHost();
+      addresses = InetAddress.getAllByName(url);
     } catch (UnknownHostException e) {
       System.out.println(e.getMessage());
     }
 
-    System.out.println(url + "의 ip 주소는 " + address);
+    System.out.println(url + "의 ip 주소는 " + Arrays.toString(addr.getAddress())); // (X)
+    System.out.println(url + "의 ip 주소는 " + addr.getHostAddress()); // (O)
+    System.out.println(url + "의 ip 주소는 " + addr.getHostName()); // (X)
+
+    System.out.println("로컬 IP 주소는 " + addr2.getHostAddress());
+
+    System.out.println(url + "의 ip 주소들은 총 " + addresses.length + " 개이며 ");
+    Arrays.stream(addresses)
+        .forEach(address -> System.out.println(address.getHostAddress()));
   }
 }
-
