90ae03194abaa4b287697cebe8b41b5310514ff0
artp <dky7034@naver.com>
2025-03-14 14:33:22 +0900

    Auto commit: Fri Mar 14 14:33:22 KST 2025

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -91,8 +91,8 @@ public class SLinkedList<E> {
     Node<E> find = head;
     // 헤드 노드의 아이템(값)을 출력합니다.
     System.out.println(find.item);
-    // for 반복문을 통해,
-    //
+    // for 반복문:
+    // i = 0 부터 시작하여, i < index 조건이 참인 동안 실행됩니다.
     for (int i = 0; i < index; i++) {
       System.out.println(find.item);
       find = find.next;
