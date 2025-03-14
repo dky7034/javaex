6f4a084b54db87284b3d70d84814d068bbfa6705
artp <dky7034@naver.com>
2025-03-14 14:10:21 +0900

    Auto commit: Fri Mar 14 14:10:21 KST 2025

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -61,7 +61,7 @@ public class SLinkedList<E> {
     // head 와 tail 모두 newNode 가 되도록 처리한다.
     // 리스트에 데이터가 있을 경우에는 head 만 새로 추가된 노드로 변경해준다.
     // < === 2) 여기를 구현한다.
-    if (size == 0) { // head == tail && head == null
+    if (size == 0) {
       tail = newNode;
     }
     head = newNode;
