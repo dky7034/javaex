de2d8e02d2f283e55dbfc5b35d622d5ebc25fd75
artp <dky7034@naver.com>
2025-03-14 09:16:25 +0900

    Auto commit: Fri Mar 14 09:16:25 KST 2025

--- src/network/DBDemo3.java
+++ src/network/DBDemo3.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 
 public class DBDemo3 {
   public static void main(String[] args) {
-
+    Connection conn = makeConnection();
+    String url = "SELECT * FROM person";
   }
   private static Connection makeConnection() {
     Strubg
