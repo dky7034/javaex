8a5c48ea69777af031ae4dc107ecc8e7e672c3c4
artp <dky7034@naver.com>
2025-03-14 11:51:18 +0900

    Auto commit: Fri Mar 14 11:51:18 KST 2025

--- src/data_structure/ArrListDemo.java
+++ src/data_structure/ArrListDemo.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 public class ArrListDemo {
   public static void main(String[] args) {
+
     ArrayList<Integer> numbers = new ArrayList<>();
-//    numbers.get(0); // IndexOutOfBoundsException
-//    numbers.remove(0); // IndexOutOfBoundsException
+    //numbers.get(0); // IndexOutOfBoundsException
+    //numbers.remove(0); // IndexOutOfBoundsException
     numbers.add(10);
     numbers.add(20);
     numbers.add(30);
@@ -19,21 +20,43 @@ public class ArrListDemo {
     System.out.println(numbers);
     numbers.remove(2);
     System.out.println(numbers);
-    System.out.println("get(인덱스)");
+    System.out.println("get(인덱스 2)");
     System.out.println(numbers.get(2));
 
     ArrList<Integer> arr = new ArrList<>();
-//    arr.peek(0);
+    //arr.peek(0);
     arr.add(10);
-    System.out.println("list의 크기: " + arr.size() + ", " + "내부 배열의 크기: " + arr.length());
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
     arr.add(20);
-    System.out.println("list의 크기: " + arr.size() + ", " + "내부 배열의 크기: " + arr.length());
-    arr.add(20);
-    System.out.println("list의 크기: " + arr.size() + ", " + "내부 배열의 크기: " + arr.length());
-    arr.add(20);
-    arr.add(20);
-    System.out.println("list의 크기: " + arr.size() + ", " + "내부 배열의 크기: " + arr.length());
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    arr.add(30);
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    arr.add(40);
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    arr.add(50);
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    System.out.println(arr);
+    System.out.println("========삭제=======");
+    arr.remove();
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    System.out.println(arr);
+    arr.remove();
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    System.out.println(arr);
+    arr.remove();
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
+    System.out.println(arr);
+    arr.remove();
+    System.out.println("list의 크기 : " + arr.size() + "," +
+        "내부 배열의 크기 : " + arr.length());
     System.out.println(arr);
   }
-
 }
