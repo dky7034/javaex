fb7ff93a40590a7aac998aafeb116e2447f0cee7
artp <dky7034@naver.com>
2025-03-14 14:39:19 +0900

    Auto commit: Fri Mar 14 14:39:19 KST 2025

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -88,7 +88,7 @@ public class SLinkedList<E> {
   Node<E> getNode(int index) {
     // 노드의 헤드를 찾습니다.
     // find 참조변수에 헤드 노드(시작을 가리키는 포인터)를 저장합니다.
-    Node<E> find = head;
+    Node<E> find = head; // 첫 번재
     // 헤드 노드의 아이템(값)을 출력합니다.
     System.out.println(find.item);
     // for 반복문:
