1f713a70c9e9a43648698a50b3e07b61521adb3e
artp <dky7034@naver.com>
2025-03-14 09:54:01 +0900

    Auto commit: Fri Mar 14 09:54:01 KST 2025

--- src/data_structure/ArrListTest.java
+++ src/data_structure/ArrListTest.java
@@ -5,7 +5,13 @@ import java.util.List;
 
 public class ArrListTest {
   public static void main(String[] args) {
-    List<String> arrList = new ArrayList<>();
+    List<Integer> arrList = new ArrayList<>();
+    arrList.add(1);
+    arrList.add(2);
+    arrList.add(3);
+    arrList.add(4);
+    arrList.add(5);
+
   }
 
 
