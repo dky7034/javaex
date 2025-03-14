e82134b42a904e6a8adb8a32e9edbd235be5a0e3
artp <dky7034@naver.com>
2025-03-14 11:04:15 +0900

    Auto commit: Fri Mar 14 11:04:15 KST 2025

--- src/data_structure/ArrListDemo.java
+++ src/data_structure/ArrListDemo.java
@@ -6,7 +6,6 @@ public class ArrListDemo {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>();
 
-    numbers.get(0);
     numbers.add(10);
     numbers.add(20);
     numbers.add(30);
