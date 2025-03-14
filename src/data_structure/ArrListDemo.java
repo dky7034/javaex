d27fae812e4420562a95d603fcff7405adc487b7
artp <dky7034@naver.com>
2025-03-14 10:46:10 +0900

    Auto commit: Fri Mar 14 10:46:10 KST 2025

--- src/data_structure/ArrListDemo.java
+++ src/data_structure/ArrListDemo.java
@@ -1,7 +1,9 @@
 package data_structure;
 
+import java.util.ArrayList;
+
 public class ArrListDemo {
   public static void main(String[] args) {
-
+    ArrayList<Integer> numbers = new ArrayList<>();
   }
 }
