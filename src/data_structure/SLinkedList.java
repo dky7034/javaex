11ba2082ec149a196267ae047a4a8b24d7d7dffe
artp <dky7034@naver.com>
2025-03-14 12:40:25 +0900

    Auto commit: Fri Mar 14 12:40:25 KST 2025

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -45,7 +45,8 @@ public class SLinkedList<E> {
     if (size == 0) { // head == tail && head == null
       tail = newNode;
     }
-    
+    head = newNode;
+
     // 3단계
     // 데이터가 하나 추가되었기 때문에 리스트의 사이즈를 하나 증가시킨다.
     // < === 3) 여기를 구현한다.
