98b2dfefb291a29c98d213dfc5fff51b036e272d
artp <dky7034@naver.com>
2025-03-15 19:32:41 +0900

    Auto commit: Sat Mar 15 19:32:41 KST 2025

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -70,8 +70,9 @@ public class SLinkedList<E> {
     if (index == 0) {
       newNode.next = head; // 새로운 노드에 다음 노드를 참조시킴
       head = newNode; // 헤드가 새로운 노드를 참조하도록 함
+    // 인덱스가 0이 아닌 경우
     } else {
-      
+
     }
 
   }
