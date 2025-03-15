ce41d1ddf5db1825c35faddcd9782c4159362886
artp <dky7034@naver.com>
2025-03-15 19:35:46 +0900

    Auto commit: Sat Mar 15 19:35:46 KST 2025

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -73,8 +73,9 @@ public class SLinkedList<E> {
     // 인덱스가 0이 아닌 경우 (새로운 노드를 중간에 삽입하려는 경우)
     } else {
       Node<E> prevNode = getNode(index - 1);
+      // 겹치는 코드 아닌가?
       newNode.next = prevNode;
-      
+
     }
 
   }
