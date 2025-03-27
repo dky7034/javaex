4cc0567d0cb05768e4a4e753dd6ad8a035d2c170
artp <dky7034@naver.com>
2025-03-27 12:16:58 +0900

    Auto commit: Thu Mar 27 12:16:58 KST 2025

--- src/test/Calculator.java
+++ src/test/Calculator.java
@@ -1,4 +1,12 @@
 package test;
 
 public class Calculator {
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  int multiply(int a, int b) {
+    return a + b;
+  }
 }
+
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,4 +1,11 @@
 package test;
 
 public class Main {
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    int res1 = calculator.add(5, 3);
+    int res2 = calculator.multiply(4, 6);
+    System.out.println("res1 = " + res1);
+    System.out.println("res2 = " + res2);
+  }
 }
